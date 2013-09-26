ea43d00410d0236e43c8d6be5bfff846b800b442
ankurankan <ankurankan@gmail.com>
2013-09-27 04:22:27 +0530

    added tests

--- tests/test_BayesianModel.py
+++ tests/test_BayesianModel.py
@@ -33,7 +33,36 @@ class TestModel(unittest.TestCase):
         self.G.add_nodes('a', 'b', 'c', 'd')
         self.G.add_edges(('a', 'b'), 'c')
         self.assertItemsEqual(self.G.edges(), [('a', 'c'), ('b', 'c')])
+    
+    def test_add_edges_both_string(self):
+        self.G.add_nodes('a', 'b')
+        self.G.add_edges('a', 'b')
+        self.assertItemsEqual(self.G.edges(), [('a', 'b')])
 
+    def test_add_edges_multiple_times(self):
+        self.G.add_nodes('a', 'b', 'c', 'd')
+        self.G.add_edges('a', ('c', 'd'))
+        self.G.add_edges('b', ('c', 'd'))
+        self.assertItemsEqual(self.G.edges(), [('a', 'c'), ('a', 'd'),
+            ('b', 'c'), ('b', 'd')])
+
+    def tearDown(self):
+        del self.G
+
+
+class TestNodeProperties(unittest.TestCase):
+
+    def setUp(self):
+        G = bm.BayesianModel()
+        G.add_nodes('a', 'b', 'c', 'd', 'e')
+        G.add_edges(('a', 'b'), ('c', 'd'))
+
+    def test_parents(self):
+        self.assertItemsEqual(self.G.node['c']['_parents'], ['a', 'b'])
+        self.assertItemsEqual(self.G.node['d']['_parents'], ['a', 'b'])
+        self.assertItemsEqual(self.G.node['a']['_parents'], [])
+        self.assertItemsEqal(self.G.nodes['b']['_paresnts'], [])
+            
     def tearDown(self):
         del self.G
 
