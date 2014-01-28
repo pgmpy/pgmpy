7668d27657506806577bec74c251a71a3854b67a
Abinash Panda <abinash.panda.ece10@itbhu.ac.in>
2014-01-28 23:08:46 +0530

    added install_requires dependencies in setup.py

--- setup.py
+++ setup.py
@@ -2,12 +2,13 @@
 
 from distutils.core import setup
 
-setup(name="pgmpy",
-      packages=["pgmpy"],
-      version="0.1.0",
-      author=open("AUTHORS.rst").read(),
-      url="https://github.com/pgmpy/pgmpy",
-      classifiers = [
+setup(
+    name="pgmpy",
+    packages=["pgmpy"],
+    version="0.1.0",
+    author=open("AUTHORS.rst").read(),
+    url="https://github.com/pgmpy/pgmpy",
+    classifiers=[
         "Programming Language :: Python :: 3",
         "Intended Audience :: Developers",
         "Intended Audience :: Researchers",
@@ -16,6 +17,11 @@ setup(name="pgmpy",
         "Operating System :: Microsoft :: Windows",
         "Operating System :: MacOS",
         "Topic :: Scientific/Engineering"
-        ],
-      long_description=open("README.md").read()
-     )
+    ],
+    long_description=open("README.md").read(),
+    install_requires=[
+        "networkx >= 1.8.1",
+        "scipy >= 0.12.1",
+        "numpy >= 1.7.1"
+    ]
+)
