5031865183fa585a5ff7bcff2eaa3dc2dc3eead2
Abinash Panda <abinash.panda.ece10@itbhu.ac.in>
2014-01-28 22:45:14 +0530

    added setup.py --version=0.1.0

---
+++ setup.py
@@ -0,0 +1,21 @@
+#/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name="pgmpy",
+      packages=["pgmpy"],
+      version="0.1.0",
+      author=open("AUTHORS.rst").read(),
+      url="https://github.com/pgmpy/pgmpy",
+      classifiers = [
+        "Programming Language :: Python :: 3",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Researchers",
+        "Operating System :: Unix",
+        "Operating System :: POSIX",
+        "Operating System :: Microsoft :: Windows",
+        "Operating System :: MacOS",
+        "Topic :: Scientific/Engineering"
+        ],
+      long_description=open("README.md").read()
+     )
