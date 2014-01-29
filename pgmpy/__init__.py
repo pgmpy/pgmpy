30497e3a461446641cd39b15d648efc0272395c4
Abinash Panda <abinash.panda.ece10@itbhu.ac.in>
2014-01-29 09:49:07 +0530

    changed __init__.py to import BayesianModel, Factor and Exceptions subpackage

--- pgmpy/__init__.py
+++ pgmpy/__init__.py
@@ -1,6 +1,8 @@
 from . import BayesianModel
 from . import Exceptions
+from . import Factor
 
 __all__ = []
 __all__.extend(BayesianModel.__all__)
 __all__.extend(Exceptions.__all__)
+__all__.extend(Factor.__all__)
