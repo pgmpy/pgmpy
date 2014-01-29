44c7dfb9a383c046c8b6636352bbb4d5f742d1b8
Abinash Panda <abinash.panda.ece10@itbhu.ac.in>
2014-01-29 10:36:41 +0530

    modified __init__.py to not to extend BayesianModel.__all__, Factor.__all__, Exceptions.__all__

--- pgmpy/__init__.py
+++ pgmpy/__init__.py
@@ -2,7 +2,6 @@ from . import BayesianModel
 from . import Exceptions
 from . import Factor
 
-__all__ = []
-__all__.extend(BayesianModel.__all__)
-__all__.extend(Exceptions.__all__)
-__all__.extend(Factor.__all__)
+__all__ = ['BayesianModel',
+           'Exceptions',
+           'Factor']
