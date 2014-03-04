5159b76e0c727879354710e4464ff1900168db7c
Abinash Panda <abinash.panda.ece10@itbhu.ac.in>
2014-03-04 09:09:18 +0530

    changed __init__ file of BayesianModel to remove IMap

--- pgmpy/BayesianModel/__init__.py
+++ pgmpy/BayesianModel/__init__.py
@@ -1,7 +1,4 @@
 from .BayesianModel import *
-from .Independencies import *
-from .MarkovModel import *
 
 __all__ = ['BayesianModel',
-           'Independencies',
-           'MarkovModel']
+           ]
