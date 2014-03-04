1501eca4945bd25829a6d40a821843ff464d07eb
Abinash Panda <abinash.panda.ece10@itbhu.ac.in>
2014-03-04 07:04:01 +0530

    initial commit of api

--- docs/conf.py
+++ docs/conf.py
@@ -19,8 +19,8 @@ import os
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-#sys.path.insert(0, os.path.abspath('.'))
 
+sys.path.append(os.path.abspath('../pgmpy/'))
 # -- General configuration ------------------------------------------------
 
 # If your documentation needs a minimal Sphinx version, state it here.
