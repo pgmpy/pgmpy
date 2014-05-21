ef070a2541e210bf33a4829aa8b0b70de118c879
Ankur Ankan <ankurankan@gmail.com>
2014-05-21 11:09:35 +0530

    Base class for Joint Probability Distribution

--- pgmpy/Factor/JointProbabilityDistribution.py
+++ pgmpy/Factor/JointProbabilityDistribution.py
@@ -1,1 +1,62 @@
-__author__ = 'ankur'
+from pgmpy.Factor import Factor
+import numpy as np
+
+
+class JointProbabilityDistribution(Factor.Factor):
+    """
+    Base class for Joint Probability Distribution
+
+    Public Methods
+    --------------
+    get_independencies()
+    pmap()
+    minimal_imap()
+    """
+    def __init__(self, variables, cardinality, values):
+        """
+        Initialize a Joint Probability Distribution class.
+
+        Defined above, we have the following mapping from variable
+        assignments to the index of the row vector in the value field:
+
+        +-----+-----+-----+-------------------------+
+        |  x1 |  x2 |  x3 |    P(x1, x2, x2)        |
+        +-----+-----+-----+-------------------------+
+        | x1_0| x2_0| x3_0|    P(x1_0, x2_0, x3_0)  |
+        +-----+-----+-----+-------------------------+
+        | x1_1| x2_0| x3_0|    P(x1_1, x2_0, x3_0)  |
+        +-----+-----+-----+-------------------------+
+        | x1_0| x2_1| x3_0|    P(x1_0, x2_1, x3_0)  |
+        +-----+-----+-----+-------------------------+
+        | x1_1| x2_1| x3_0|    P(x1_1, x2_1, x3_0)  |
+        +-----+-----+-----+-------------------------+
+        | x1_0| x2_0| x3_1|    P(x1_0, x2_0, x3_1)  |
+        +-----+-----+-----+-------------------------+
+        | x1_1| x2_0| x3_1|    P(x1_1, x2_0, x3_1)  |
+        +-----+-----+-----+-------------------------+
+        | x1_0| x2_1| x3_1|    P(x1_0, x2_1, x3_1)  |
+        +-----+-----+-----+-------------------------+
+        | x1_1| x2_1| x3_1|    P(x1_1, x2_1, x3_1)  |
+        +-----+-----+-----+-------------------------+
+
+        Parameters
+        ----------
+        variables: list
+            List of scope of Joint Probability Distribution.
+        cardinality: list, array_like
+            List of cardinality of each variable
+        value: list, array_like
+            List or array of values of factor.
+            A Joint Probability Distribution's values are stored in a row
+            vector in the value using an ordering such that the left-most
+            variables as defined in the variable field cycle through their
+            values the fastest.
+
+        Examples
+        --------
+        >>> prob = JointProbabilityDistribution(['x1', 'x2', 'x3'], [2, 2, 2], np.ones(8)/8)
+        """
+        if np.isclose(np.sum(values), 1):
+            Factor.Factor.__init__(self, variables, cardinality, values)
+        else:
+            raise ValueError("The probability values doesn't sum to 1.")
