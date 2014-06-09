2604b98c97ba47b443843d6ed784fdc88e5c3982
navin <navinchandak92@gmail.com>
2014-06-09 19:52:16 +0530

    Added the classes for UndirectedGraphs (added general graph functionalities on top of nx.Graph) and JunctionTree

---
+++ pgmpy/MarkovModel/JunctionTree.py
@@ -0,0 +1,20 @@
+import networkx as nx
+from pgmpy.MarkovModel.UndirectedGraph import UndirectedGraph
+
+class JunctionTree(UndirectedGraph):
+
+    def addJTEdges(self):
+        nodes = self.nodes()
+        num_nodes = self.number_of_nodes()
+        for i in range(num_nodes):
+            for j in range(i + 1, num_nodes):
+                print(self.node[nodes[i]]["clique_nodes"])
+                set1 = set(self.node[nodes[i]]["clique_nodes"])
+                set2 = set(self.node[nodes[j]]["clique_nodes"])
+                set3 = set1.intersection(set2)
+                self.add_edge(nodes[i], nodes[j], weight = -len(set3))
+        self.print_graph("before the MST, after adding all edges")
+        T = nx.minimum_spanning_edges(self)
+        self.remove_edges_from(self.edges())
+        self.add_edges_from(T)
+        return self
