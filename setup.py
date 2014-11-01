49f13e9ee3e24a3d4ddf48ce70d2a60daa92b09d
Abinash Panda <abinash.panda.ece10@itbhu.ac.in>
2014-11-01 08:24:26 +0530

    Removed and cleaned Cython dependencies from setup.py

--- setup.py
+++ setup.py
@@ -1,39 +1,12 @@
 #/usr/bin/env python3
-import ez_setup
-ez_setup.use_setuptools()
-
-USE_CYTHON = True
 
 from setuptools import setup, find_packages
-from distutils.extension import Extension
-import numpy as np
-
-try:
-    from Cython.Distutils import build_ext
-except ImportError:
-    USE_CYTHON = False
-
-ext_modules = []
-cmdclass = {}
-
-if USE_CYTHON:
-    ext_modules.extend([
-        Extension("pgmpy.Factor._factor_product",
-                  ["pgmpy/Factor/_factor_product.pyx"])
-    ])
-    cmdclass.update({'build_ext': build_ext})
-else:
-    ext_modules.extend([
-        Extension("pgmpy.Factor._factor_product",
-                  ["pgmpy/Factor/_factor_product.c"])
-    ])
 
 setup(
     name="pgmpy",
-    packages=find_packages(exclude=['tests']),
-    cmdclass=cmdclass,
-    ext_modules=ext_modules,
     version="0.1.0",
+    description="A library for Probabilistic Graphical Models",
+    packages=find_packages(exclude=['tests']),
     author=open("AUTHORS.rst").read(),
     url="https://github.com/pgmpy/pgmpy",
     classifiers=[
@@ -54,5 +27,4 @@ setup(
         "nose >= 1.3.0",
         "coveralls >= 0.4"
     ],
-    include_dirs = [np.get_include()]
 )
