0eda007fd420c518ee24586648cb7f04a12c1a3e
Ankur Ankan <ankurankan@gmail.com>
2014-11-02 13:54:15 +0530

    added docstrings to Inference class

--- pgmpy/Inference/base.py
+++ pgmpy/Inference/base.py
@@ -8,8 +8,34 @@ from pgmpy.factors import Factor
 class Inference:
     """
     Base class for all Inference algorithms.
+
+    Converts BayesianModel and MarkovModel to a uniform representation so that inference
+    algorithms can be applied. Also it checks if all the associated CPDs / Factors are
+    consistent with the model.
     """
     def __init__(self, model):
+        """
+        Initialize Inference for a model.
+
+        Parameters
+        ----------
+        model: pgmpy.models.BayesianModel or pgmpy.models.MarkovModel or pgmpy.models.NoisyOrModel
+            model for which to initialize the Inference object.
+
+        Examples
+        --------
+        >>> from pgmpy.Inference import VariableElimination
+        >>> from pgmpy.models import BayesianModel
+        >>> from pgmpy.factors import TabularCPD
+        >>> student = BayesianModel([('diff', 'grade'), ('intel', 'grade')])
+        >>> diff_cpd = TabularCPD('diff', 2, [[0.2, 0.8]])
+        >>> intel_cpd = TabularCPD('intel', 2, [[0.3, 0.7]])
+        >>> grade_cpd = TabularCPD('grade', 3, [[0.1, 0.1, 0.1, 0.1],
+        ...                                     [0.1, 0.1, 0.1, 0.1],
+        ...                                     [0.8, 0.8, 0.8, 0.8]])
+        >>> student.add_cpd([diff_cpd, intel_cpd, grade_cpd])
+        >>> model = Inference(student)
+        """
         self.variables = model.nodes()
         if isinstance(model, BayesianModel):
             self.factors = {}
