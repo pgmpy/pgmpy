d16bd7ff1fe336bfc7284dc3565a861f106a9911
Ankur Ankan <ankurankan@gmail.com>
2014-11-02 15:36:58 +0530

    corrected tests according to the new structure of Models

--- pgmpy/tests/test_inference/test_inference.py
+++ pgmpy/tests/test_inference/test_inference.py
@@ -4,32 +4,39 @@ import numpy as np
 from pgmpy.models import BayesianModel
 from pgmpy.models import MarkovModel
 from pgmpy.factors import Factor
+from pgmpy.factors import TabularCPD
 from pgmpy.Inference import Inference
 
 
 class TestInferenceBase(unittest.TestCase):
     def setUp(self):
         self.bayesian = BayesianModel([('a', 'b'), ('b', 'c'), ('c', 'd'), ('d', 'e')])
-        self.bayesian.set_cpd('a', np.array([[0.4, 0.6]]))
-        self.bayesian.set_cpd('b', np.array([[0.2, 0.4], [0.3, 0.4]]))
-        self.bayesian.set_cpd('c', np.array([[0.1, 0.2], [0.3, 0.4]]))
-        self.bayesian.set_cpd('d', np.array([[0.4, 0.3], [0.2, 0.1]]))
-        self.bayesian.set_cpd('e', np.array([[0.3, 0.2], [0.4, 0.1]]))
+        a_cpd = TabularCPD('a', 2, [[0.4, 0.6]])
+        b_cpd = TabularCPD('b', 2, [[0.2, 0.4], [0.3, 0.4]], evidence='a', evidence_card=[2])
+        c_cpd = TabularCPD('c', 2, [[0.1, 0.2], [0.3, 0.4]], evidence='b', evidence_card=[2])
+        d_cpd = TabularCPD('d', 2, [[0.4, 0.3], [0.2, 0.1]], evidence='c', evidence_card=[2])
+        e_cpd = TabularCPD('e', 2, [[0.3, 0.2], [0.4, 0.1]], evidence='d', evidence_card=[2])
+        self.bayesian.add_cpd([a_cpd, b_cpd, c_cpd, d_cpd, e_cpd])
 
         self.markov = MarkovModel([('a', 'b'), ('b', 'd'), ('a', 'c'), ('c', 'd')])
-        self.markov.add_factors(Factor(['a', 'b'], [2, 2], np.array([100, 1, 1, 100])))
-        self.markov.add_factors(Factor(['a', 'c'], [2, 2], np.array([40, 30, 100, 20])))
-        self.markov.add_factors(Factor(['b', 'd'], [2, 2], np.array([1, 100, 100, 1])))
-        self.markov.add_factors(Factor(['c', 'd'], [2, 2], np.array([60, 60, 40, 40])))
+        factor_1 = Factor(['a', 'b'], [2, 2], np.array([100, 1, 1, 100]))
+        factor_2 = Factor(['a', 'c'], [2, 2], np.array([40, 30, 100, 20]))
+        factor_3 = Factor(['b', 'd'], [2, 2], np.array([1, 100, 100, 1]))
+        factor_4 = Factor(['c', 'd'], [2, 2], np.array([60, 60, 40, 40]))
+        self.markov.add_factors([factor_1, factor_2, factor_3, factor_4])
 
     def test_inference_init(self):
         infer_bayesian = Inference(self.bayesian)
         self.assertEqual(infer_bayesian.variables, ['a', 'b', 'c', 'd', 'e'])
-        self.assertEqual(infer_bayesian.factors, {'a': [self.bayesian.get_cpd('a'), self.bayesian.get_cpd('b')],
-                                                  'b': [self.bayesian.get_cpd('b'), self.bayesian.get_cpd('c')],
-                                                  'c': [self.bayesian.get_cpd('c'), self.bayesian.get_cpd('d')],
-                                                  'd': [self.bayesian.get_cpd('d'), self.bayesian.get_cpd('e')],
-                                                  'e': [self.bayesian.get_cpd('e')]})
+        self.assertEqual(infer_bayesian.factors, {'a': [self.bayesian.get_cpd('a').to_factor(),
+                                                        self.bayesian.get_cpd('b').to_factor()],
+                                                  'b': [self.bayesian.get_cpd('b').to_factor(),
+                                                        self.bayesian.get_cpd('c').to_factor()],
+                                                  'c': [self.bayesian.get_cpd('c').to_factor(),
+                                                        self.bayesian.get_cpd('d').to_factor()],
+                                                  'd': [self.bayesian.get_cpd('d').to_factor(),
+                                                        self.bayesian.get_cpd('e').to_factor()],
+                                                  'e': [self.bayesian.get_cpd('e').to_factor()]})
 
         infer_markov = Inference(self.markov)
         self.assertEqual(infer_markov.variables, ['a', 'b', 'c', 'd'])
