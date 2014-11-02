3e3cac0c2c9eaa3fc2c4d31a80555ae474c838d7
Ankur Ankan <ankurankan@gmail.com>
2014-11-02 12:51:45 +0530

    fixed moral_graph method

--- pgmpy/models/BayesianModel.py
+++ pgmpy/models/BayesianModel.py
@@ -1075,7 +1075,7 @@ class BayesianModel(nx.DiGraph):
 
         Examples
         --------
-        >>> import pgmpy.models as bm
+        >>> from pgmpy.models import BayesianModel
         >>> G = BayesianModel([('diff', 'grade'), ('intel', 'grade'),
         ...                    ('intel', 'SAT'), ('grade', 'letter')])
         >>> moral_graph = G.moral_graph()
@@ -1087,13 +1087,13 @@ class BayesianModel(nx.DiGraph):
         [('diff', 'intel'), ('diff', 'grade'), ('intel', 'grade'),
         ('intel', 'SAT'), ('grade', 'letter')]
         """
-        moral_graph = self.copy()
+        moral_graph = self.to_undirected()
         parents_dict = {}
         for node in self.nodes():
             parents_dict[node] = self.get_parents(node)
         for node, parents in parents_dict.items():
             moral_graph.add_edges_from(list(itertools.combinations(parents, 2)))
-        return moral_graph.to_undirected()
+        return moral_graph
 
     def get_factorized_product(self, latex=False):
         #TODO: refer to IMap class for explanation why this is not implemented.
