b8c943d9f1e511757706fa468dfb6c7abadba652
Ankur Ankan <ankurankan@gmail.com>
2014-11-04 13:27:33 +0530

    doctest in Variable Elimination

--- pgmpy/Inference/ExactInference.py
+++ pgmpy/Inference/ExactInference.py
@@ -9,9 +9,20 @@ class VariableElimination(Inference):
         """
         Examples
         --------
-        >>> from pgmpy.BayesianModel import BayesianModel
+        For a Bayesian Model
+        >>> from pgmpy.models import BayesianModel
+        >>> from pgmpy.factors import TabularCPD
         >>> bm = BayesianModel([('a', 'b'), ('b', 'c'), ('c', 'd'), ('d', 'e')])
-        >>> # add cpds
+        >>> cpd_a = TabularCPD('a', 2, [[0.4, 0.6]])
+        >>> cpd_b = TabularCPD('b', 2, [[0.4, 0.8], [0.6, 0.2]],
+        ...                    evidence=['a'], evidence_card=[2])
+        >>> cpd_c = TabularCPD('c', 2, [[0.2, 0.2], [0.8, 0.8]],
+        ...                    evidence='b', evidence_card=[2])
+        >>> cpd_d = TabularCPD('d', 2, [[0.5, 0.4], [0.5, 0.6]],
+        ...                    evidence='c', evidence_card=[2])
+        >>> cpd_e = TabularCPD('e', 2, [[0.1, 0.8], [0.9, 0.2]],
+        ...                    evidence='d', evidence_card=[2])
+        >>> bm.add_cpd([cpd_a, cpd_b, cpd_c, cpd_d, cpd_e])
         >>> VariableElimination(bm).query(variables={'c':{}})
         """
         if not elimination_order:
