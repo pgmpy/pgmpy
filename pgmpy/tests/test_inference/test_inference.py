1aa6c092d8a0f70e0a386382a4cd7693824fdb67
Ankur Ankan <ankurankan@gmail.com>
2014-11-04 11:56:10 +0530

    tests for inference base class

--- pgmpy/tests/test_inference/test_inference.py
+++ pgmpy/tests/test_inference/test_inference.py
@@ -23,28 +23,31 @@ class TestInferenceBase(unittest.TestCase):
         factor_2 = Factor(['a', 'c'], [2, 2], np.array([40, 30, 100, 20]))
         factor_3 = Factor(['b', 'd'], [2, 2], np.array([1, 100, 100, 1]))
         factor_4 = Factor(['c', 'd'], [2, 2], np.array([60, 60, 40, 40]))
-        self.markov.add_factors([factor_1, factor_2, factor_3, factor_4])
+        self.markov.add_factors(factor_1, factor_2, factor_3, factor_4)
 
-    def test_inference_init(self):
+    def test_bayesian_inference_init(self):
         infer_bayesian = Inference(self.bayesian)
-        self.assertEqual(infer_bayesian.variables, ['a', 'b', 'c', 'd', 'e'])
-        self.assertEqual(infer_bayesian.factors, {'a': [self.bayesian.get_cpd('a').to_factor(),
-                                                        self.bayesian.get_cpd('b').to_factor()],
-                                                  'b': [self.bayesian.get_cpd('b').to_factor(),
-                                                        self.bayesian.get_cpd('c').to_factor()],
-                                                  'c': [self.bayesian.get_cpd('c').to_factor(),
-                                                        self.bayesian.get_cpd('d').to_factor()],
-                                                  'd': [self.bayesian.get_cpd('d').to_factor(),
-                                                        self.bayesian.get_cpd('e').to_factor()],
-                                                  'e': [self.bayesian.get_cpd('e').to_factor()]})
+        self.assertEqual(set(infer_bayesian.variables), {'a', 'b', 'c', 'd', 'e'})
+        self.assertEqual(infer_bayesian.cardinality, {'a': 2, 'b': 2, 'c': 2, 'd': 2, 'e': 2})
+        # self.assertEqual(infer_bayesian.factors, {'a': [self.bayesian.get_cpd('a').to_factor(),
+        #                                                 self.bayesian.get_cpd('b').to_factor()],
+        #                                           'b': [self.bayesian.get_cpd('b').to_factor(),
+        #                                                 self.bayesian.get_cpd('c').to_factor()],
+        #                                           'c': [self.bayesian.get_cpd('c').to_factor(),
+        #                                                 self.bayesian.get_cpd('d').to_factor()],
+        #                                           'd': [self.bayesian.get_cpd('d').to_factor(),
+        #                                                 self.bayesian.get_cpd('e').to_factor()],
+        #                                           'e': [self.bayesian.get_cpd('e').to_factor()]})
 
+    def test_markov_inference_init(self):
         infer_markov = Inference(self.markov)
-        self.assertEqual(infer_markov.variables, ['a', 'b', 'c', 'd'])
-        self.assertEqual(infer_markov.factors, {'a': [Factor(['a', 'b'], [2, 2], np.array([100, 1, 1, 100])),
-                                                      Factor(['a', 'c'], [2, 2], np.array([40, 30, 100, 20]))],
-                                                'b': [Factor(['a', 'b'], [2, 2], np.array([100, 1, 1, 100])),
-                                                      Factor(['b', 'd'], [2, 2], np.array([1, 100, 100, 1]))],
-                                                'c': [Factor(['a', 'c'], [2, 2], np.array([40, 30, 100, 20])),
-                                                      Factor(['c', 'd'], [2, 2], np.array([60, 60, 40, 40]))],
-                                                'd': [Factor(['b', 'd'], [2, 2], np.array([1, 100, 100, 1])),
-                                                      Factor(['c', 'd'], [2, 2], np.array([60, 60, 40, 40]))]})
+        self.assertEqual(set(infer_markov.variables), {'a', 'b', 'c', 'd'})
+        self.assertEqual(infer_markov.cardinality, {'a': 2, 'b': 2, 'c': 2, 'd': 2})
+        # self.assertEqual(infer_markov.factors, {'a': [Factor(['a', 'b'], [2, 2], np.array([100, 1, 1, 100])),
+        #                                               Factor(['a', 'c'], [2, 2], np.array([40, 30, 100, 20]))],
+        #                                         'b': [Factor(['a', 'b'], [2, 2], np.array([100, 1, 1, 100])),
+        #                                               Factor(['b', 'd'], [2, 2], np.array([1, 100, 100, 1]))],
+        #                                         'c': [Factor(['a', 'c'], [2, 2], np.array([40, 30, 100, 20])),
+        #                                               Factor(['c', 'd'], [2, 2], np.array([60, 60, 40, 40]))],
+        #                                         'd': [Factor(['b', 'd'], [2, 2], np.array([1, 100, 100, 1])),
+        #                                               Factor(['c', 'd'], [2, 2], np.array([60, 60, 40, 40]))]})
