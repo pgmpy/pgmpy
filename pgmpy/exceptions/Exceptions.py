1b61f4eac3a0976d55f65061bfe13d56746fbd96
Ankur Ankan <ankurankan@gmail.com>
2014-12-24 07:53:50 +0530

    adds a new ModelError exception

--- pgmpy/exceptions/Exceptions.py
+++ pgmpy/exceptions/Exceptions.py
@@ -96,3 +96,11 @@ class RequiredError(Exception):
 
     def __str__(self):
         return repr(str(self.extra))
+
+
+class ModelError(Exception):
+    def __init__(self, extra):
+        self.extra = extra
+
+    def __str__(self):
+        return repr(str(self.extra))
