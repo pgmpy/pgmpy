50db87f0ae3439c9be4f4febf0dbe714d53e47e9
Ankur Ankan <ankurankan@gmail.com>
2014-12-24 10:27:00 +0530

    predict method for Bayesian Model [fixes #224]

--- pgmpy/models/BayesianModel.py
+++ pgmpy/models/BayesianModel.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python3
 
-from collections import defaultdict
-from pgmpy.base import DirectedGraph
-from pgmpy.factors import TabularCPD, TreeCPD, RuleCPD
 import itertools
+from collections import defaultdict
 import networkx as nx
 import numpy as np
+import pandas as pd
 
+from pgmpy.base import DirectedGraph
+from pgmpy.factors import TabularCPD, TreeCPD, RuleCPD
+from pgmpy.inference import VariableElimination
 
 class BayesianModel(DirectedGraph):
     """
@@ -511,6 +513,51 @@ class BayesianModel(DirectedGraph):
                 cpds_list.append(cpd)
 
         self.add_cpds(*cpds_list)
+
+    def predict(self, data):
+        """
+        Predicts states of all the missing variables.
+
+        Parameters
+        ----------
+        data : pandas DataFrame object
+            A DataFrame object with column names same as the variables in the model.
+
+        Examples
+        --------
+        >>> import numpy as np
+        >>> import pandas as pd
+        >>> from pgmpy.models import BayesianModel
+        >>> values = pd.DataFrame(np.random.randint(low=0, high=2, size=(1000, 5)),
+        ...                       columns=['A', 'B', 'C', 'D', 'E'])
+        >>> train_data = values[:800]
+        >>> predict_data = values[800:]
+        >>> model = BayesianModel([('A', 'B'), ('C', 'B'), ('C', 'D'), ('B', 'E')])
+        >>> model.fit(values)
+        >>> predict_data.drop('E', axis=1)
+        >>> y_pred = model.predict(predict_data)
+        >>> y_pred
+        array([0, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 1, 1, 0, 1, 1, 0, 1, 0, 1, 1, 0, 1,
+               1, 0, 0, 0, 1, 0, 0, 1, 0, 1, 1, 1, 1, 0, 1, 0, 0, 1, 0, 1, 1, 0, 1,
+               1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 1,
+               1, 0, 1, 1, 0, 0, 1, 0, 0, 1, 1, 0, 0, 0, 1, 0, 1, 0, 1, 1, 0, 0, 1,
+               1, 1, 1, 0, 0, 0, 1, 0])
+        """
+        if set(data.columns) == set(self.nodes()):
+            raise ValueError("No variable missing in data. Nothing to predict")
+
+        elif set(data.columns) - set(self.nodes()):
+            raise ValueError("data has variables which are not in the model")
+
+        missing_variables = set(self.nodes()) - set(data.columns)
+        pred_values = defaultdict(list)
+
+        model_inference = VariableElimination(self)
+        for index, data_point in data.iterrows():
+            states_dict = model_inference.map_query(variables=missing_variables, evidence=data_point.to_dict())
+            for k, v in states_dict:
+                pred_values[k].append(v)
+        return pd.DataFrame(pred_values, index=data.index)
 
     def get_factorized_product(self, latex=False):
         # TODO: refer to IMap class for explanation why this is not implemented.
