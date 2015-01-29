252a19866609619f53854a9c679764ff8a27391f
Ankur Ankan <ankurankan@gmail.com>
2015-01-29 18:39:57 +0530

    fix tests

--- pgmpy/tests/test_inference/test_ExactInference.py
+++ pgmpy/tests/test_inference/test_ExactInference.py
@@ -67,15 +67,15 @@ class TestVariableElimination(unittest.TestCase):
         np_test.assert_almost_equal(self.bayesian_inference.max_marginal(), 0.5714)
 
     def test_max_marginal_var(self):
-        np_test.assert_almost_equal(self.bayesian_inference.max_marginal(['G']), 0.5714)
+        np_test.assert_almost_equal(self.bayesian_inference.max_marginal(['G']), 0.5714, decimal=4)
 
     def test_max_marginal_var1(self):
         np_test.assert_almost_equal(self.bayesian_inference.max_marginal(['G', 'R']),
-                                    0.3176)
+                                    0.4055, decimal=4)
 
     def test_max_marginal_var2(self):
         np_test.assert_almost_equal(self.bayesian_inference.max_marginal(['G', 'R', 'A']),
-                                    0.2466)
+                                    0.3260, decimal=4)
 
 
 class TestBeliefPropagation(unittest.TestCase):
