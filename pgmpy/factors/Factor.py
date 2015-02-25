029dc2f2d8fa6fc5cb63e6c019b4744506c5e5d5
palashahuja <abhor902@gmail.com>
2015-02-25 14:53:54 +0530

    removing pep8 errors for fast marginalize bug#267
    changes for fast marginalize method

--- pgmpy/factors/Factor.py
+++ pgmpy/factors/Factor.py
@@ -18,7 +18,6 @@ class Factor:
     product(*Factor)
     reduce([variable_values_list])
     """
-    
     def __init__(self, variables, cardinality, value):
         """
         Initialize a factors class.
@@ -191,15 +190,15 @@ class Factor:
             factor = self
         else:
             factor = Factor(self.scope(), self.cardinality, self.values)
-        marginalize_index = np.array(np.where(np.in1d(factor.scope(),variables)))
+        marginalize_index = np.array(np.where(np.in1d(factor.scope(), variables)))
         assign = np.array(factor.cardinality)
         assign[marginalize_index] = -1
         sum_variable = np.zeros(len(factor.cardinality))
         sum_variable[marginalize_index] = -1
         marg_factor = []
-        for i in itertools.product(*[range(index) for index in factor.cardinality[np.where(assign!=-1)[0]]]):
+        for i in itertools.product(*[range(index) for index in factor.cardinality[np.where(assign != -1)[0]]]):
             temp_sum = 0
-            sum_variable[sum_variable!=-1] = i
+            sum_variable[sum_variable != -1] = i
             for index in factor._index_for_assignment(sum_variable):
                 temp_sum += factor.values[index]
             marg_factor.append(temp_sum)
