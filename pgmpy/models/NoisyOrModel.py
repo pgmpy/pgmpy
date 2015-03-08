2b263cda0595e7e5f4a78ab83d996c159b1e9a3e
gokul-uf <gokul.uf@gmail.com>
2015-03-08 09:49:28 +0530

    modified:   pgmpy/models/NoisyOrModel.py Reduced heap usage in add_variables

--- pgmpy/models/NoisyOrModel.py
+++ pgmpy/models/NoisyOrModel.py
@@ -73,12 +73,11 @@ class NoisyOrModel(DirectedGraph):
         # Converting the inhibitor_probability to a uniform 2D array
         # because else numpy treats it as a 1D array with dtype object.
         inhibitor_probability_list = []
+        max_cardinality = max(cardinality)
         for prob_array in inhibitor_probability:
-            if len(prob_array) < max(cardinality):
-                prob_array.extend([0]*(max(cardinality)-len(prob_array)))
+                prob_array.extend([0]*(max_cardinality-len(prob_array)))  #If len(prob_array) == max_cardinality, prob_array is untouched
                 inhibitor_probability_list.append(prob_array)
-            else:
-                inhibitor_probability_list.append(prob_array)
+
         inhibitor_probability_uni = np.array(inhibitor_probability_list)
 
         if inhibitor_probability_uni[inhibitor_probability_uni > 1]:
