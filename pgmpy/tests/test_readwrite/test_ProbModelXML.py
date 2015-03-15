6493ccc48b52d5d1dab07c20d49c98d44d5dc57f
Vivek Jain <vivek425ster@gmail.com>
2015-03-15 16:51:52 +0530

    Added tests for ProbModelXMLWriter class.

--- pgmpy/tests/test_readwrite/test_ProbModelXML.py
+++ pgmpy/tests/test_readwrite/test_ProbModelXML.py
@@ -2,7 +2,7 @@
 import unittest
 from io import StringIO
 import networkx as nx
-from pgmpy.readwrite import ProbModelXMLReader
+from pgmpy.readwrite import ProbModelXMLReader, ProbModelXMLWriter
 import warnings
 try:
     from lxml import etree
@@ -22,9 +22,7 @@ class TestProbModelXMLReaderString(unittest.TestCase):
       <ProbNet type="BayesianNetwork">
         <AdditionalConstraints />
         <Comment>Student example model from Probabilistic Graphical Models: Principles and Techniques by Daphne Koller</Comment>
-        <Language>
-            English
-        </Language>
+        <Language>English</Language>
         <AdditionalProperties />
         <Variables>
             <Variable name="intelligence" type="FiniteState" role="Chance">
@@ -83,8 +81,8 @@ class TestProbModelXMLReaderString(unittest.TestCase):
     def test_comment(self):
         comment_expected = "Student example model from Probabilistic Graphical Models: Principles and Techniques by Daphne Koller"
         self.maxDiff = None
-        self.assertEqual(self.reader_string.probnet['comment'], comment_expected)
-        self.assertEqual(self.reader_file.probnet['comment'], comment_expected)
+        self.assertEqual(self.reader_string.probnet['Comment'], comment_expected)
+        self.assertEqual(self.reader_file.probnet['Comment'], comment_expected)
 
     def test_variables(self):
         variables = {'difficulty':
@@ -123,3 +121,101 @@ class TestProbModelXMLReaderString(unittest.TestCase):
         self.maxDiff = None
         self.assertDictEqual(self.reader_string.probnet['edges'], edge)
         self.assertDictEqual(self.reader_file.probnet['edges'], edge)
+
+class TestProbModelXMLWriter(unittest.TestCase):
+    def setUp(self):
+        self.model_data = {'probnet':
+                           {'type': 'BayesianNetwork', 
+                            'Language': 'English', 
+                            'Variables': {'difficulty': 
+                                          {'type': 'FiniteState', 
+                                           'role': 'Chance', 
+                                           'States': {'difficult': {}, 'easy': {}}, 
+                                           'Comment': None, 
+                                           'Coordinates': {}}, 
+                                          'intelligence': 
+                                          {'type': 'FiniteState', 
+                                           'role': 'Chance', 
+                                           'States': {'smart': {}, 'dumb': {}}, 
+                                           'Comment': None, 
+                                           'Coordinates': {}}}, 
+                            'Comment': 'Student example model from Probabilistic Graphical Models: Principles and Techniques by Daphne Koller', 
+                            'edges': {('difficulty', 'grade'): 
+                                      {'directed': '1', 
+                                       'Label': 'diff_to_grad', 
+                                       'Comment': 'Directed Edge from difficulty to grade'}, 
+                                      ('intelligence', 'grade'): 
+                                      {'directed': '1', 
+                                       'Label': 'intel_to_grad', 
+                                       'Comment': 'Directed Edge from intelligence to grade'}, 
+                                      ('intelligence', 'SAT'): 
+                                      {'directed': '1', 
+                                       'Label': 'intel_to_sat', 
+                                       'Comment': 'Directed Edge from intelligence to SAT'}, 
+                                      ('grade', 'recommendation_letter'): 
+                                      {'directed': '1', 
+                                       'Label': 'grad_to_reco', 
+                                       'Comment': 'Directed Edge from grade to recommendation_letter'}}}}
+        self.writer = ProbModelXMLWriter(model_data=self.model_data)
+    def test_file(self):
+        self.expected_xml = etree.XML("""<ProbModelXML formatVersion="1.0">
+  <ProbNet type="BayesianNetwork">
+    <Variables>
+      <Variable name="intelligence" role="Chance" type="FiniteState">
+        <Comment/>
+        <Coordinates/>
+        <AdditionalProperties/>
+        <States>
+          <State name="smart">
+            <AdditionalProperties/>
+          </State>
+          <State name="dumb">
+            <AdditionalProperties/>
+          </State>
+        </States>
+      </Variable>
+      <Variable name="difficulty" role="Chance" type="FiniteState">
+        <Comment/>
+        <Coordinates/>
+        <AdditionalProperties/>
+        <States>
+          <State name="difficult">
+            <AdditionalProperties/>
+          </State>
+          <State name="easy">
+            <AdditionalProperties/>
+          </State>
+        </States>
+      </Variable>
+    </Variables>
+    <Links>
+      <Link directed="1" var1="intelligence" var2="grade">
+        <Comment>Directed Edge from intelligence to grade</Comment>
+        <Label>intel_to_grad</Label>
+        <AdditionalProperties/>
+      </Link>
+      <Link directed="1" var1="difficulty" var2="grade">
+        <Comment>Directed Edge from difficulty to grade</Comment>
+        <Label>diff_to_grad</Label>
+        <AdditionalProperties/>
+      </Link>
+      <Link directed="1" var1="grade" var2="recommendation_letter">
+        <Comment>Directed Edge from grade to recommendation_letter</Comment>
+        <Label>grad_to_reco</Label>
+        <AdditionalProperties/>
+      </Link>
+      <Link directed="1" var1="intelligence" var2="SAT">
+        <Comment>Directed Edge from intelligence to SAT</Comment>
+        <Label>intel_to_sat</Label>
+        <AdditionalProperties/>
+      </Link>
+    </Links>
+    <Potential/>
+    <Language>English</Language>
+    <Comment>Student example model from Probabilistic Graphical Models: Principles and Techniques by Daphne Koller</Comment>
+    <AdditionalProperties/>
+  </ProbNet>
+</ProbModelXML>
+""")
+        self.maxDiff = None
+        self.assertEqual(self.writer.__str__(), etree.tostring(self.expected_xml))
