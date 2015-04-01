03372d151fcc4d4f8e2355d30e9ce0c6de759421
palashahuja <abhor902@gmail.com>
2015-04-01 13:39:53 +0530

    to_tabular_cpd method of TreeCPD [refs #200]

--- pgmpy/tests/test_factors/test_Factor.py
+++ pgmpy/tests/test_factors/test_Factor.py
@@ -487,12 +487,12 @@ class TestTreeCPDInit(unittest.TestCase):
 
 class TestTreeCPD(unittest.TestCase):
     def setUp(self):
-        self.tree1 = TreeCPD([('B', Factor(['A'], [2], [0.8, 0.2]), 0),
-                              ('B', 'C', 1),
-                              ('C', Factor(['A'], [2], [0.1, 0.9]), 0),
-                              ('C', 'D', 1),
-                              ('D', Factor(['A'], [2], [0.9, 0.1]), 0),
-                              ('D', Factor(['A'], [2], [0.4, 0.6]), 1)])
+        self.tree1 = TreeCPD([('B', Factor(['A'], [2], [0.8, 0.2]), '0'),
+                              ('B', 'C', '1'),
+                              ('C', Factor(['A'], [2], [0.1, 0.9]), '0'),
+                              ('C', 'D', '1'),
+                              ('D', Factor(['A'], [2], [0.9, 0.1]), '0'),
+                              ('D', Factor(['A'], [2], [0.4, 0.6]), '1')])
 
         self.tree2 = TreeCPD([('C','A','0'), ('C','B','1'), 
                               ('A', Factor(['J'], [2], [0.9, 0.1]), '0'),
