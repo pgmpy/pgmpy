9377cbad8fda88705b39b8f130bed5ae47515c2e
Abinash Panda <abinash.panda.ece10@itbhu.ac.in>
2015-04-07 22:52:30 +0530

    Added imports and re-ordered them for both JunctionTree as well as ClusterGraph class.

--- pgmpy/models/__init__.py
+++ pgmpy/models/__init__.py
@@ -2,8 +2,8 @@ from .BayesianModel import *
 from .NoisyOrModel import *
 from .MarkovModel import *
 from .FactorGraph import *
-from .JunctionTree import *
 from .ClusterGraph import *
+from .JunctionTree import *
 
 __all__ = ['BayesianModel',
            'NoisyOrModel',
