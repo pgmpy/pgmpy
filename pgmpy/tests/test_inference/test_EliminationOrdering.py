ebcdaf643838f71454c6e201359756d4370e332c
Ankur Ankan <ankurankan@gmail.com>
2015-04-22 12:38:31 +0530

    updates tests for elimination order classes

--- pgmpy/tests/test_inference/test_EliminationOrdering.py
+++ pgmpy/tests/test_inference/test_EliminationOrdering.py
@@ -1,6 +1,8 @@
 import unittest
-from pgmpy.inference import EliminationOrdering
+
 import numpy as np
+
+from pgmpy.inference import WeightedMinFill, MinNeighbours, MinWeight, MinFill
 from pgmpy.models import BayesianModel
 from pgmpy.factors import TabularCPD
 
@@ -8,7 +10,10 @@ from pgmpy.factors import TabularCPD
 class TestEliminationOrdering(unittest.TestCase):
 
     def setUp(self):
-        graph = BayesianModel()
+        self.model = BayesianModel()
+        self.model.add_edges_from([('c', 'd'), ('d', 'g'), ('i', 'g'),
+                              ('i', 's'), ('s', 'j'), ('g', 'l'),
+                              ('l', 'j'), ('j', 'h'), ('g', 'h')])
         cpd_c = TabularCPD('c', 2, np.random.rand(2, 1))
         cpd_d = TabularCPD('d', 2, np.random.rand(2, 2),
                            ['c'], [2])
@@ -23,52 +28,42 @@ class TestEliminationOrdering(unittest.TestCase):
                            ['g'], [3])
         cpd_h = TabularCPD('h', 2, np.random.rand(2, 6),
                            ['g', 'j'], [3, 2])
-        graph.add_edges_from([('c', 'd'), ('d', 'g'), ('i', 'g'),
-                              ('i', 's'), ('s', 'j'), ('g', 'l'),
-                              ('l', 'j'), ('j', 'h'), ('g', 'h')])
-        graph.add_cpds(cpd_c, cpd_d, cpd_g, cpd_i, cpd_s, cpd_j,
-                       cpd_l, cpd_h)
-
-        self.elim_ord = EliminationOrdering(graph)
+        self.model.add_cpds(cpd_c, cpd_d, cpd_g, cpd_i, cpd_s, cpd_j,
+                            cpd_l, cpd_h)
 
     def test_weighted_min_fill(self):
-        self.assertEqual(self.elim_ord.weighted_min_fill('c'), 0)
-        self.assertEqual(self.elim_ord.weighted_min_fill('d'), 10)
+        elim_order = WeightedMinFill(self.model)
+        self.assertEqual(elim_order.cost('c'), 0)
+        self.assertEqual(elim_order.cost('d'), 10)
 
     def test_min_neighbors(self):
-        self.assertEqual(self.elim_ord.min_neighbors('c'), 1)
-        self.assertEqual(self.elim_ord.min_neighbors('g'), 5)
+        elim_order = MinNeighbours(self.model)
+        self.assertEqual(elim_order.cost('c'), 1)
+        self.assertEqual(elim_order.cost('g'), 5)
 
     def test_min_weight(self):
-        self.assertEqual(self.elim_ord.min_weight('l'), 12)
-        self.assertEqual(self.elim_ord.min_weight('s'), 8)
+        elim_order = MinWeight(self.model)
+        self.assertEqual(elim_order.cost('l'), 12)
+        self.assertEqual(elim_order.cost('s'), 8)
 
     def test_min_fill(self):
-        self.assertEqual(self.elim_ord.min_fill('i'), 2)
-        self.assertEqual(self.elim_ord.min_fill('h'), 0)
+        elim_order = MinFill(self.model)
+        self.assertEqual(elim_order.cost('i'), 2)
+        self.assertEqual(elim_order.cost('h'), 0)
 
     def test_find_elimination_ordering(self):
-        self.assertListEqual(self.elim_ord.
-                             find_elimination_ordering(['c', 'd',
-                                                        'g', 'l', 's'],
-                                                       self.elim_ord.
-                                                       weighted_min_fill),
-                             ['c', 'l', 's', 'd', 'g'])
-        self.assertListEqual(self.elim_ord.
-                             find_elimination_ordering(['c', 'd',
-                                                        'g', 'l', 's'],
-                                                       self.elim_ord.
-                                                       min_neighbors),
-                             ['c', 'd', 'l', 's', 'g'])
-        self.assertListEqual(self.elim_ord.
-                             find_elimination_ordering(['c', 'd',
-                                                        'g', 'l', 's'],
-                                                       self.elim_ord.
-                                                       min_weight),
-                             ['c', 's', 'd', 'l', 'g'])
-        self.assertListEqual(self.elim_ord.
-                             find_elimination_ordering(['c', 'd',
-                                                        'g', 'l', 's'],
-                                                       self.elim_ord.
-                                                       min_fill),
-                             ['c', 'l', 'd', 's', 'g'])
+        weighted_min_fill = WeightedMinFill(self.model)
+        self.assertListEqual(weighted_min_fill.get_elimination_order(['c', 'd', 'g', 'l', 's']),
+                                                                     ['c', 'l', 's', 'd', 'g'])
+
+        min_neighbors = MinNeighbours(self.model)
+        self.assertListEqual(min_neighbors.get_elimination_order(['c', 'd', 'g', 'l', 's']),
+                                                                 ['c', 'd', 'l', 's', 'g'])
+
+        min_weight = MinWeight(self.model)
+        self.assertListEqual(min_weight.get_elimination_order(['c', 'd', 'g', 'l', 's']),
+                                                              ['c', 's', 'd', 'l', 'g'])
+
+        min_fill = MinFill(self.model)
+        self.assertListEqual(min_fill.get_elimination_order(['c', 'd', 'g', 'l', 's']),
+                                                            ['c', 'l', 'd', 's', 'g'])
