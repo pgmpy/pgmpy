f3e272869f8be6b63b6133778118a9113a658121
Pratyaksh Sharma <pratyaksh@me.com>
2015-06-01 14:36:39 +0200

    implemented forward_sampling

--- pgmpy/inference/__init__.py
+++ pgmpy/inference/__init__.py
@@ -1,7 +1,9 @@
 from .base import Inference
 from .ExactInference import VariableElimination
 from .ExactInference import BeliefPropagation
+from .approximate_inference.Sampling import BayesianModelSampling
 
 __all__ = ['Inference',
            'VariableElimination',
-           'BeliefPropagation']
+           'BeliefPropagation',
+           'BayesianModelSampling']
