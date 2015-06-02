edd2b382023dce9be9adfec4ff47801ca865d5ea
palashahuja <abhor902@gmail.com>
2015-06-02 17:43:45 +0530

    removing np.where and np.in1d

--- pgmpy/factors/Factor.py
+++ pgmpy/factors/Factor.py
@@ -194,7 +194,7 @@ class Factor:
             factor = self
         else:
             factor = Factor(self.scope(), self.cardinality, self.values)
-        marginalize_index = np.array(np.where(np.array([i in variables for i in factor.scope()])))
+        marginalize_index = np.array([i in variables for i in factor.scope()])
         assign = np.array(factor.cardinality)
         assign[marginalize_index] = -1
         marginalized_values = []
