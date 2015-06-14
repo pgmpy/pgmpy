df5142f7af03060a161f765be36572cd9fe0c91f
Pratyaksh Sharma <pratyaksh@me.com>
2015-06-14 19:32:10 -0400

    added class for markov chain

--- pgmpy/inference/__init__.py
+++ pgmpy/inference/__init__.py
@@ -1,9 +1,10 @@
 from .base import Inference
 from .ExactInference import VariableElimination
 from .ExactInference import BeliefPropagation
-from .Sampling import BayesianModelSampling
+from .Sampling import BayesianModelSampling, MarkovChainMonteCarlo
 
 __all__ = ['Inference',
            'VariableElimination',
            'BeliefPropagation',
-           'BayesianModelSampling']
+           'BayesianModelSampling',
+           'MarkovChainMonteCarlo']
