53bb79702b387d47046e016b68ecdeea92e105fc
Pratyaksh Sharma <pratyaksh@me.com>
2015-06-20 19:14:01 +0200

    Added docstrings and moved the class MarkovChainMonteCarlo to models.
    MarkovChainMonteCarlo moved to pgmpy/models/MarkovChain.py

--- pgmpy/inference/__init__.py
+++ pgmpy/inference/__init__.py
@@ -1,10 +1,9 @@
 from .base import Inference
 from .ExactInference import VariableElimination
 from .ExactInference import BeliefPropagation
-from .Sampling import BayesianModelSampling, MarkovChainMonteCarlo
+from .Sampling import BayesianModelSampling
 
 __all__ = ['Inference',
            'VariableElimination',
            'BeliefPropagation',
-           'BayesianModelSampling',
-           'MarkovChainMonteCarlo']
+           'BayesianModelSampling']
