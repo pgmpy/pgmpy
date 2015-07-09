9fe0431c42b965beb2011a0ec6c58817b5ce8aa6
Vivek Jain <vivek425ster@gmail.com>
2015-07-09 09:41:36 +0530

    Add get_edges and get_tables method.

--- pgmpy/readwrite/UAI.py
+++ pgmpy/readwrite/UAI.py
@@ -1,4 +1,5 @@
 from pyparsing import *
+from itertools import combinations
 
 
 class UAIReader:
@@ -37,8 +38,8 @@ class UAIReader:
         self.network_type = self.get_network_type()
         self.variables = self.get_variables()
         self.domain = self.get_domain()
-        # TODO : Add function to get edges
-        self.edges = None
+        self.edges = self.get_edges()
+        self.tables = self.get_tables()
 
     def get_grammar(self):
         """
@@ -54,11 +55,19 @@ class UAIReader:
         grammar += no_functions
         self.no_functions = int(grammar.parseString(self.network)['no_functions'])
         for function in range(0, self.no_functions):
-            scope_grammar = Word(nums, exact=1).setResultsName('fun_scope_'+str(function))
+            scope_grammar = Word(nums).setResultsName('fun_scope_'+str(function))
             grammar += scope_grammar
             function_scope = grammar.parseString(self.network)['fun_scope_'+str(function)]
             function_grammar = ((Word(nums))*int(function_scope)).setResultsName('fun_'+str(function))
             grammar += function_grammar
+
+        floatnumber = Combine(Word(nums) + Optional(Literal(".") + Optional(Word(nums))))
+        for function in range(0, self.no_functions):
+            no_values_grammar = Word(nums).setResultsName('fun_no_values_' + str(function))
+            grammar += no_values_grammar
+            no_values = grammar.parseString(self.network)['fun_no_values_' + str(function)]
+            values_grammar = ((floatnumber)*int(no_values)).setResultsName('fun_values_'+str(function))
+            grammar += values_grammar
         return grammar
 
     def get_network_type(self):
@@ -122,3 +131,61 @@ class UAIReader:
         for var in range(0, len(var_domain)):
             domain["var_"+str(var)] = var_domain[var]
         return domain
+
+    def get_edges(self):
+        """
+        Returns the edges of the network.
+
+        Returns
+        -------
+        set: set containing the edges of the network
+
+        Example
+        -------
+        >>> reader = UAIReader('TestUAI.uai')
+        >>> reader.get_edges()
+        {('var_0', 'var_1'), ('var_0', 'var_2'), ('var_1', 'var_2')}
+        """
+        edges = []
+        for function in range(0, self.no_functions):
+            function_variables = self.grammar.parseString(self.network)['fun_'+str(function)]
+            if self.network_type == 'BAYES':
+                child_var = "var_" + str(function_variables[-1])
+                function_variables = function_variables[:-1]
+                for var in function_variables:
+                    edges.append((child_var, "var_"+str(var)))
+            elif self.network_type == "MARKOV":
+                function_variables = ["var_" + var for var in function_variables]
+                edges.extend(list(combinations(function_variables, 2)))
+        return set(edges)
+
+    def get_tables(self):
+        """
+        Returns list of tuple of child variable and CPD in case of Bayesian
+        and list of tuple of scope of variables and values in case of Markov.
+
+        Returns
+        -------
+        list : list of tuples of child variable and values in Bayesian
+            list of tuples of scope of variables and values in case of Markov.
+
+        Example
+        -------
+        >>> reader = UAIReader('TestUAI.uai')
+        >>> reader.get_tables()
+        [(['var_0', 'var_1'], ['4.000', '2.400', '1.000', '0.000']),
+         (['var_0', 'var_1', 'var_2'],
+          ['2.2500', '3.2500', '3.7500', '0.0000', '0.0000', '10.0000', '1.8750', '4.0000', '3.3330', '2.0000', '2.0000', '3.4000'])]
+        """
+        tables = []
+        for function in range(0, self.no_functions):
+            function_variables = self.grammar.parseString(self.network)['fun_'+str(function)]
+            if self.network_type == 'BAYES':
+                child_var = "var_" + str(function_variables[-1])
+                values = self.grammar.parseString(self.network)['fun_values_'+str(function)]
+                tables.append((child_var, list(values)))
+            elif self.network_type == "MARKOV":
+                function_variables = ["var_" + var for var in function_variables]
+                values = self.grammar.parseString(self.network)['fun_values_'+str(function)]
+                tables.append((function_variables, list(values)))
+        return tables
--- pgmpy/tests/test_readwrite/test_UAI.py
+++ pgmpy/tests/test_readwrite/test_UAI.py
@@ -34,3 +34,14 @@ class TestUAIReader(unittest.TestCase):
     def test_get_domain(self):
         domain_expected = {'var_1': '2', 'var_2': '3', 'var_0': '2'}
         self.assertDictEqual(self.reader_string.domain, domain_expected)
+
+    def test_get_edges(self):
+        edges_expected = {('var_0', 'var_1'), ('var_0', 'var_2'), ('var_1', 'var_2')}
+        self.assertSetEqual(self.reader_string.edges, edges_expected)
+
+    def test_get_tables(self):
+        tables_expected = [(['var_0', 'var_1'],
+                            ['4.000', '2.400', '1.000', '0.000']),
+                           (['var_0', 'var_1', 'var_2'],
+                            ['2.2500', '3.2500', '3.7500', '0.0000', '0.0000', '10.0000', '1.8750', '4.0000', '3.3330', '2.0000', '2.0000', '3.4000'])]
+        self.assertListEqual(self.reader_string.tables, tables_expected)
