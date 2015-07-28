ae74f0137a2c7742762c3bc39fe4359d9d1efdc1
Pratyaksh Sharma <pratyaksh@me.com>
2015-07-29 04:33:10 +0530

    Added class for Gibbs sampling

--- pgmpy/inference/__init__.py
+++ pgmpy/inference/__init__.py
@@ -1,11 +1,12 @@
 from .base import Inference
 from .ExactInference import VariableElimination
 from .ExactInference import BeliefPropagation
-from .Sampling import BayesianModelSampling
+from .Sampling import BayesianModelSampling, GibbsSampling
 from .mplp import Mplp
 
 __all__ = ['Inference',
            'VariableElimination',
            'BeliefPropagation',
            'BayesianModelSampling',
+           'GibbsSampling',
            'Mplp']
