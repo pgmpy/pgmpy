c86ce27f319a60460a4ee03f14f2008b6bca31fc
palashahuja <abhor902@gmail.com>
2015-08-17 19:18:30 +0530

    adding mplp

--- pgmpy/inference/__init__.py
+++ pgmpy/inference/__init__.py
@@ -3,9 +3,11 @@ from .ExactInference import VariableElimination
 from .ExactInference import BeliefPropagation
 from .Sampling import BayesianModelSampling
 from .dbn_inference import DBNInference
+from .mplp import Mplp
 
 __all__ = ['Inference',
            'VariableElimination',
            'DBNInference',
            'BeliefPropagation',
-           'BayesianModelSampling']
+           'BayesianModelSampling'
+           'Mplp']
