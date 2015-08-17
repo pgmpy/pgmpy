957bcd2b2fa9fb76d27b59d1f0c7f8701ba732f7
palashahuja <abhor902@gmail.com>
2015-08-17 19:40:01 +0530

    adding all the changes.

--- pgmpy/inference/dbn_inference.py
+++ pgmpy/inference/dbn_inference.py
@@ -1,3 +1,4 @@
+import copy
 import itertools
 from collections import defaultdict
 
@@ -13,10 +14,34 @@ class DBNInference(Inference):
         """
         Class for performing inference using Belief Propagation method
         for the input Dynamic Bayesian Network.
+
         Parameters
         ----------
         model: Dynamic Bayesian Network
         model for which inference is to performed
+
+        Examples
+        --------
+        >>> from pgmpy.models import BayesianModel as bm
+        >>> from pgmpy.models import DynamicBayesianNetwork as dbn
+        >>> dbnet = dbn()
+        >>> grade_cpd = TabularCPD(('G',0),3, [[0.3, 0.05, 0.9, 0.5],
+                                               [0.4, 0.25, 0.08, 0.3],
+                                               [0.3, 0.7, 0.2, 0.2]],[('D',0),('I',0)],[2,2])
+        >>> d_i_cpd = TabularCPD(('D',1),2,[[0.6,0.3],[0.4,0.7]],[('D',0)],2)
+        >>> diff_cpd = TabularCPD(('D',0),2,[[0.6,0.4]])
+        >>> intel_cpd = TabularCPD(('I',0),2,[[0.7,0.3]])
+        >>> i_i_cpd = TabularCPD(('I',1),2,[[0.5,0.4],[0.5,0.6]],[('I',0)],2)
+        >>> dbnet.add_edges_from([(('D', 0), ('D', 1)), (('I', 0), ('I', 1)), (('D',1),('G',1)), (('I',1), ('G',1))])
+        >>> dbnet.add_cpds(grade_cpd, d_i_cpd, diff_cpd, intel_cpd, i_i_cpd)
+        >>> dbnet.initialize_initial_state()
+        >>> dbn_inf = DBNInference(dbnet)
+        >>> dbn_inf.start_junction_tree.nodes()
+        [(('I', 0), ('G', 0), ('D', 0))]
+        >>> dbn_inf.one_and_half_junction_tree.nodes()
+        [(('I', 1), ('D', 1), ('D', 0)),
+        (('I', 1), ('D', 1), ('G', 1)),
+        (('I', 1), ('I', 0), ('D', 0))]
         """
         super().__init__(model)
         self.interface_nodes_0 = model.get_interface_nodes(0)
@@ -25,11 +50,11 @@ class DBNInference(Inference):
         start_markov_model = self.start_bayesian_model.to_markov_model()
         one_and_half_markov_model = self.one_and_half_model.to_markov_model()
 
-        combinations_slice_0 = list(itertools.combinations(model.get_interface_nodes(0), 2))
-        combinations_slice_1 = list(itertools.combinations(model.get_interface_nodes(1), 2))
+        combinations_slice_0 = itertools.combinations(model.get_interface_nodes(0), 2)
+        combinations_slice_1 = itertools.combinations(model.get_interface_nodes(1), 2)
 
         start_markov_model.add_edges_from(combinations_slice_0)
-        one_and_half_markov_model.add_edges_from(combinations_slice_0 + combinations_slice_1)
+        one_and_half_markov_model.add_edges_from(itertools.chain(combinations_slice_0, combinations_slice_1))
 
         self.one_and_half_junction_tree = one_and_half_markov_model.to_junction_tree()
         self.start_junction_tree = start_markov_model.to_junction_tree()
@@ -40,7 +65,8 @@ class DBNInference(Inference):
 
     def _shift_nodes(self, nodes, time):
         """
-        shifting the nodes to a certain required timeslice.
+        Shifting the nodes to a certain required timeslice.
+
         Parameters
         ----------
         shift: int
@@ -50,8 +76,9 @@ class DBNInference(Inference):
 
     def _get_clique(self, junction_tree, nodes):
         """
-        extracting the cliques from the junction tree which are a subset of
+        Extracting the cliques from the junction tree which are a subset of
         the given nodes.
+
         Parameters
         ----------
         junction_tree: Junction tree
@@ -63,7 +90,8 @@ class DBNInference(Inference):
 
     def _get_evidence(self, evidence_dict, time, shift):
         """
-        getting the evidence belonging to a particular timeslice.
+        Getting the evidence belonging to a particular timeslice.
+
         Parameters
         ----------
         evidence: dict
@@ -81,7 +109,8 @@ class DBNInference(Inference):
 
     def _marginalize_factor(self, nodes, factor):
         """
-        marginalizing the factor selectively for a set of variables.
+        Marginalizing the factor selectively for a set of variables.
+
         Parameters
         ----------
         nodes: iterable container
@@ -95,7 +124,8 @@ class DBNInference(Inference):
 
     def _update_belief(self, belief_prop, in_clique, out_clique_potential):
         """
-        method for updating the belief.
+        Method for updating the belief.
+
         Parameters
         ----------
         belief_prop: Belief Propagation
@@ -113,7 +143,8 @@ class DBNInference(Inference):
 
     def _get_factor(self, belief_prop, evidence):
         """
-        extracts the required factor from the junction tree.
+        Extracts the required factor from the junction tree.
+
         Parameters
         ----------
         belief_prop: Belief Propagation
@@ -121,7 +152,7 @@ class DBNInference(Inference):
         evidence: dict
             a dict key, value pair as {var: state_of_var_observed}
         """
-        final_factor = factor_product(*belief_prop.junction_tree.get_factors())
+        final_factor = copy.deepcopy(factor_product(*belief_prop.junction_tree.get_factors()))
         if evidence:
             for var in evidence:
                 if var in final_factor.scope():
@@ -130,7 +161,8 @@ class DBNInference(Inference):
 
     def forward_inference(self, variables, evidence=None):
         """
-        forward inference method using belief propagation.
+        Forward inference method using belief propagation.
+
         Parameters
         ----------
         variables: list
@@ -163,29 +195,27 @@ class DBNInference(Inference):
         time_range = max(variable_dict)
         start_bp = BeliefPropagation(self.start_junction_tree)
         mid_bp = BeliefPropagation(self.one_and_half_junction_tree)
-        start_bp.calibrate()
-        mid_bp.calibrate()
         evidence_0 = self._get_evidence(evidence, 0, 0)
         interface_nodes_dict = {}
-        if evidence is not None:
+        if evidence:
             interface_nodes_dict = {k: v for k, v in evidence_0.items() if k in self.interface_nodes_0}
         initial_factor = self._get_factor(start_bp, evidence_0)
         marginalized_factor = self._marginalize_factor(self.interface_nodes_0, initial_factor)
         self._update_belief(mid_bp, self.in_clique, marginalized_factor)
-        if variable_dict[0] != []:
+        if variable_dict[0]:
             factor_values = start_bp.query(variable_dict[0], evidence=evidence_0)
         else:
             factor_values = {}
 
         for time in range(1, time_range + 1):
             evidence_time = self._get_evidence(evidence, time, 1)
-            if interface_nodes_dict != {}:
+            if interface_nodes_dict:
                 if evidence_time is not None:
                     evidence_time.update(interface_nodes_dict)
                 else:
                     evidence_time = interface_nodes_dict
 
-            if variable_dict[time] != []:
+            if variable_dict[time]:
                 variable_time = self._shift_nodes(variable_dict[time], 1)
                 new_values = mid_bp.query(variable_time, evidence=evidence_time)
                 changed_values = {}
@@ -200,7 +230,7 @@ class DBNInference(Inference):
             new_scope = self._shift_nodes(out_clique_phi.scope(), 0)
             new_factor = Factor(new_scope, out_clique_phi.cardinality, out_clique_phi.values)
             self._update_belief(mid_bp, self.in_clique, new_factor)
-            if evidence_time is not None:
+            if evidence_time:
                 interface_nodes_dict = {(k[0], 0): v for k, v in evidence_time.items() if k in self.interface_nodes_1}
             else:
                 interface_nodes_dict = {}
