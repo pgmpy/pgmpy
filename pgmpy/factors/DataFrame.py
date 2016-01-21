a01d5423577138a4bc7e9215fcaceefa8f3b8418
Yashu Seth <yashuseth2503@gmail.com>
2016-01-21 19:00:34 +0530

    adds get_variables, get_values methods in DataFrame

--- pgmpy/factors/DataFrame.py
+++ pgmpy/factors/DataFrame.py
@@ -22,7 +22,7 @@ class DataFrame(object):
         if isinstance(variables, six.string_types):
             raise TypeError("Variables: Expected type list or array like, got string")
         
-        self.values = np.asarray(values)
+        self.values = values
 
         if values.dtype != int:
             raise TypeError("Values: Expected type int, got ", values.dtype)
@@ -32,3 +32,42 @@ class DataFrame(object):
 
         self.variables = list(variables)
         self.values = values.reshape(-1, len(variables))
+
+    def get_variables(self):
+        """
+        Returns the list of variables whose data is given in the samples.
+
+        Returns
+        -------
+        list: List of variable names.
+
+        Examples
+        --------
+        >>> from pgmpy.factors import DataFrame
+        >>> df = DataFrame(['A', 'B', 'C'], [1,1,1,0,0,1,1,1,0,1,0,1])
+        >>> df.get_variables()
+        ['A', 'B', 'C']
+        """
+        return self.variables
+
+    def get_values(self):
+        """
+        Returns the list of values in the given samples.
+
+        Returns
+        -------
+        ndarray: 2-D Array of values with number of columns equal to the
+        number of variables and number of rows equal to the number of 
+        samples.
+
+        Examples
+        --------
+        >>> from pgmpy.factors import DataFrame
+        >>> df = DataFrame(['A', 'B', 'C'], [1,1,1,0,0,1,1,1,0,1,0,1])
+        >>> df.get_values()
+        array([[1, 1, 1],
+               [0, 0, 1],
+               [1, 1, 0],
+               [1, 0, 1]])
+        """
+        return self.values
