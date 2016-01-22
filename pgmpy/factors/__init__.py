eda908138f367412b0e45430ae8710fe493d26b7
Yashu Seth <yashuseth2503@gmail.com>
2016-01-22 21:57:44 +0530

    adds DataFrame to factors __init__.py

--- pgmpy/factors/__init__.py
+++ pgmpy/factors/__init__.py
@@ -2,11 +2,13 @@ from .Factor import Factor, factor_product, factor_divide, State
 from .FactorSet import FactorSet, factorset_product, factorset_divide
 from .CPD import TabularCPD
 from .JointProbabilityDistribution import JointProbabilityDistribution
+from .DataFrame import DataFrame
 
 __all__ = ['Factor',
            'State',
            'factor_product',
            'factor_divide',
+           'DataFrame',
            'TabularCPD',
            'JointProbabilityDistribution',
            'FactorSet',
