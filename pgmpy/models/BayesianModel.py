ea992aef1aa281faf603c44a529c5d7970fc2cc6
Utkarsh Gupta <utkarsh.gupta550@gmail.com>
2016-01-23 17:51:44 +0530

    Added method for I-equivalence in BayesianModel
    I have added two methods , get_immoralities and is_iequivalent
    for same skeleton I'm using networkx's algorithm for isomorphism

--- pgmpy/models/BayesianModel.py
+++ pgmpy/models/BayesianModel.py
@@ -622,8 +622,68 @@ class BayesianModel(DirectedGraph):
         # TODO: refer to IMap class for explanation why this is not implemented.
         pass
 
+    def get_immoralities(self):
+        """
+        Finds all the immoralities in the model
+        A v-structure X -> Z <- Y is an immorality if there is no direct edge between X and Y .
+
+        Returns
+        -------
+        set: A set of all the immoralities in the model
+
+        Examples
+        ---------
+        >>> from pgmpy.models import BayesianModel
+        >>> student = BayesianModel()
+        >>> student.add_edges_from([('diff', 'grade'), ('intel', 'grade'),
+        ...                         ('intel', 'SAT'), ('grade', 'letter')])
+        >>> student.get_immoralities()
+        {('diff','intel')}
+        """
+        immoralities = set()
+        for node in self.nodes():
+            for parents in itertools.combinations(self.predecessors(node),2):
+                if not self.has_edge(parents[0], parents[1]) and not self.has_edge(parents[1], parents[0]):
+                    immoralities.add(tuple(sorted(parents)))
+        return immoralities
+
     def is_iequivalent(self, model):
-        pass
+        """
+        Checks wether the given model is I-equivalent
+
+        Two graphs G1 and G2 are said to be I-equivalent if they have same skeleton
+        and have same set of immoralities.
+
+        Note: For same skeleton different names of nodes can work but for immoralities
+        names of nodes must be same
+
+        Parameters
+        ----------
+        model : A Bayesian model object, for which you want to check I-equivalence
+
+        Returns
+        --------
+        boolean : True if both are I-equivalent, False otherwise
+
+        Examples
+        --------
+        >>> from pgmpy.models import BayesianModel
+        >>> G = BayesianModel()
+        >>> G.add_edges_from([('V', 'W'), ('W', 'X'),
+        ...                  ('X', 'Y'), ('Z', 'Y')])
+        >>> G1 = BayesianModel()
+        >>> G1.add_edges_from([('W', 'V'), ('X', 'W'),
+        ...                    ('X', 'Y'), ('Z', 'Y')])
+        >>> G.is_iequivalent(G1)
+        True
+
+        """
+        if not isinstance(model, BayesianModel):
+            raise TypeError('model must be an instance of Bayesian Model')
+        skeleton = nx.algorithms.isomorphism.GraphMatcher(self.to_undirected(), model.to_undirected())
+        if skeleton.is_isomorphic() and self.get_immoralities() == model.get_immoralities():
+            return True
+        return False
 
     def is_imap(self, JPD):
         """
