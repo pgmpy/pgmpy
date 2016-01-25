44abbff2dc69958903c5272266aa017a1a6b1373
Yashu Seth <yashuseth2503@gmail.com>
2016-01-25 19:13:35 +0530

    adds TestDataFrameMethods1

--- pgmpy/tests/test_factors/test_DataFrame.py
+++ pgmpy/tests/test_factors/test_DataFrame.py
@@ -6,7 +6,7 @@ import numpy.testing as np_test
 from pgmpy.factors import DataFrame
 
 
-class TestFactorInit(unittest.TestCase):
+class TestDataFrameInit(unittest.TestCase):
 	def test_class_init(self):
 		phi1 = DataFrame(['x1', 'x2', 'x3'], list(range(12)))
 		self.assertListEqual(phi1.variables, ['x1', 'x2', 'x3'])
@@ -28,3 +28,27 @@ class TestFactorInit(unittest.TestCase):
 		self.assertRaises(TypeError, DataFrame, ['x1', 'x2', 'x3'], 'A')
 		self.assertRaises(ValueError, DataFrame, ['x1', 'x2', 'x3'], np.arange(10))
 		self.assertRaises(ValueError, DataFrame, ['x1', 2], np.arange(13))
+
+
+class TestDataFrameMethods1(unittest.TestCase):
+	def setUp(self):
+		self.phi1 = DataFrame(['x1', 'x2', 'x3'], list(range(12)))
+		self.phi2 = DataFrame(['x1', 'x2', 5, (1,2)],
+						 np.array([[ 0,  1,  2,  3],
+   							       [ 4,  5,  6,  7],
+						           [ 8,  9, 10, 11],
+						           [12, 13, 14, 15],
+						           [16, 17, 18, 19]]))
+
+	def test_get_variables(self):
+		self.assertListEqual(self.phi1.get_variables(), ['x1', 'x2', 'x3'])
+		self.assertListEqual(self.phi2.get_variables(), ['x1', 'x2', 5, (1,2)])
+
+	def test_get_values(self):
+		np_test.assert_array_equal(self.phi1.get_values(), np.arange(12).reshape(-1, 3))
+		np_test.assert_array_equal(self.phi2.get_values(), np.arange(20).reshape(-1, 4))
+
+	def test_get_num_of_samples(self):
+		self.assertEqual(self.phi1.get_num_of_samples(), 4)
+		self.assertEqual(self.phi2.get_num_of_samples(), 5)
+
