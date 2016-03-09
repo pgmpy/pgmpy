e75fa84c378bdcb1b26995cfc4ce7e9a22abc795
abhi95 <abhi.r.j95@gmail.com>
2016-03-09 17:30:26 +0530

    Fixes bug in hash method of Factor

--- pgmpy/factors/Factor.py
+++ pgmpy/factors/Factor.py
@@ -780,7 +780,9 @@ class Factor(object):
         return not self.__eq__(other)
 
     def __hash__(self):
-        variables = sorted(self.variables)
+        var_with_hashes = [(var, hash(var)) for var in self.variables]
+        var_with_hashes = sorted(var_with_hashes, key=lambda element: element[1])
+        variables = [var_with_hash[0] for var_with_hash in var_with_hashes]
         phi = self.copy()
         for axis in range(phi.values.ndim):
             exchange_index = phi.variables.index(variables[axis])
