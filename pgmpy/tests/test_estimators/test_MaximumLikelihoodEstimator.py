6d8caea5a5823ed010aa353099b8657c13189ac7
dan <dmsedra@gmail.com>
2016-03-22 13:44:48 -0400

    Added MLE tests

--- pgmpy/tests/test_estimators/test_MaximumLikelihoodEstimator.py
+++ pgmpy/tests/test_estimators/test_MaximumLikelihoodEstimator.py
@@ -8,8 +8,38 @@ from pgmpy.factors import TabularCPD
 
 class TestMLE(unittest.TestCase):
     def setUp(self):
-        self.m1 = BayesianModel([('A', 'C'), ('B', 'C')])
-        self.d1 = pd.DataFrame(data={'A': [0, 0, 1], 'B': [0, 1, 0], 'C': [1, 1, 0]})
+        self.m1 = BayesianModel([('A','C'),('B','C')])
+        self.m2 = BayesianModel([('A','B'),('B','C')])
+        self.m3 = BayesianModel([('B','A'),('B','C')])
+        self.d1 = pd.DataFrame(data={'A':[0,0,1], 'B':[0,1,0], 'C':[1,1,0]})
+        self.d2 = pd.DataFrame(data={'A':[0,0,1,1], 'B':[0,1,0,1], 'C':[1,1,0,0]})
+
+
+    def test_get_parameters_causal(self):
+        mle = MaximumLikelihoodEstimator(self.m2, self.d2)
+        cpds = [ TabularCPD('A', 2, [[0.5],[0.5]]),
+                 TabularCPD('B', 2, [[0.5, 0.5],
+                                     [0.5, 0.5]],
+                                     evidence=['A'], evidence_card=[2]),
+                 TabularCPD('C', 2, [[0.5, 0.5],
+                                     [0.5, 0.5]],
+                                     evidence=['B'], evidence_card=[2])]
+
+        self.assertEqual(set(mle.get_parameters()), set(cpds))
+
+
+    def test_get_parameters_common_cause(self):
+        mle = MaximumLikelihoodEstimator(self.m3, self.d2)
+        cpds = [ TabularCPD('B', 2, [[0.5],[0.5]]),
+                 TabularCPD('C', 2, [[0.5, 0.5],
+                                     [0.5, 0.5]],
+                                     evidence=['B'], evidence_card=[2]),
+                 TabularCPD('A', 2, [[0.5, 0.5],
+                                     [0.5, 0.5]],
+                                     evidence=['B'], evidence_card=[2])]
+
+        self.assertEqual(set(mle.get_parameters()), set(cpds))
+
 
     def test_get_parameters_missing_data(self):
         mle = MaximumLikelihoodEstimator(self.m1, self.d1)
@@ -21,6 +51,11 @@ class TestMLE(unittest.TestCase):
 
         self.assertSetEqual(set(mle.get_parameters()), set(cpds))
 
+
     def tearDown(self):
         del self.m1
+        del self.m2
+        del self.m3
+
         del self.d1
+        del self.d2
