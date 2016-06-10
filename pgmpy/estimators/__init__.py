433b1ce20bd89b3628669c2df4d95fa81ae847db
chrisittner <mail@chrisittner.de>
2016-06-10 23:26:35 +0200

    Bayesian Parameter Estimation for Bayesian Networks added

--- pgmpy/estimators/__init__.py
+++ pgmpy/estimators/__init__.py
@@ -1,5 +1,7 @@
 from pgmpy.estimators.base import BaseEstimator
 from pgmpy.estimators.MLE import MaximumLikelihoodEstimator
+from pgmpy.estimators.BayesianEstimator import BayesianEstimator
 
 __all__ = ['BaseEstimator',
-           'MaximumLikelihoodEstimator']
+           'MaximumLikelihoodEstimator',
+           'BayesianEstimator']
