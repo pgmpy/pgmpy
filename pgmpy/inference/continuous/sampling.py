a57220bcda63a85653f4c5c6ca6c96126ec1d53b
Utkarsh <utkarsh.gupta550@gmail.com>
2016-06-30 23:21:02 +0530

    Added generate sample method for NUTS, _build_tree method for NUTSda

--- pgmpy/inference/continuous/sampling.py
+++ pgmpy/inference/continuous/sampling.py
@@ -594,7 +594,15 @@ class HamiltonianMCda(HamiltonianMC):
         self.acceptance_rate = self.accepted_proposals / num_samples
 
 
-class NoUTurnSampler(HamiltonianMCda):
+class NoUTurnSampler(HamiltonianMC):
+    """
+    References
+    ----------
+    Matthew D. Hoffman, Andrew Gelman, The No-U-Turn Sampler: Adaptively
+    Setting Path Lengths in Hamiltonian Monte Carlo. Journal of
+    Machine Learning Research 15 (2014) 1351-1381
+    Algorithm 3 : Efficient No-U-Turn Sampler
+    """
 
     def __init__(self, model, grad_log_pdf=None, simulate_dynamics=LeapFrog):
 
@@ -605,39 +613,51 @@ class NoUTurnSampler(HamiltonianMCda):
         """
         Recursively builds a tree for proposing new position and momentum
         """
+
         # Parameter names in algorithm (here -> representation in algorithm)
         # position -> theta, momentum -> r, slice_var -> u, direction -> v, depth ->j, stepsize -> epsilon
         # candidate_set_size -> n, accept_set_bool -> s
         if depth == 0:
+            # Take single leapfrog step in the given direction
             position_bar, momentum_bar, grad_bar = self.simulate_dynamics(self.model, position, momentum, stepsize,
                                                                           self.grad_log_pdf).get_proposed_values()
 
             _, logp_bar = self.model.get_gradient_log_pdf(position_bar, self.grad_log_pdf)
-            log_hamiltonian = logp_bar - 0.5 * np.dot(momentum_bar, momentum_bar)
-            candidate_set_size = slice_var < np.exp(log_hamiltonian)
-            accept_set_bool = log_hamiltonian > np.log(slice_var) - 10000 # delta_max = 10000
+
+            hamiltonian = logp_bar - 0.5 * np.dot(momentum_bar, momentum_bar)
+
+            candidate_set_size = slice_var < np.exp(hamiltonian)
+            accept_set_bool = hamiltonian > np.log(slice_var) - 10000 # delta_max = 10000
 
             return (position_bar, momentum_bar, position_bar, momentum_bar, position_bar,
                     candidate_set_size, accept_set_bool)
 
         else:
+            # Build left and right subtrees
             (position_backward, momentum_backward, position_forward, momentum_forward, position_bar,
              candidate_set_size, accept_set_bool) = self._build_tree(position, momentum,
                                                                      slice_var, direction, depth-1, stepsize)
             if accept_set_bool == 1:
                 if direction == -1:
+                    # Build tree in backward direction
                     (position_backward, momentum_backward, _, _, position_bar2,
                      candidate_set_size2, accept_set_bool2) = self._build_tree(position_backward, momentum_backward,
                                                                                slice_var, direction, depth-1, stepsize)
                 else:
+                    # Build tree in forward direction
                     (_, _, position_forward, momentum_forward, position_bar2,
                      candidate_set_size2, accept_set_bool2) = self._build_tree(position_forward, momentum_forward,
                                                                                slice_var, direction, depth-1, stepsize)
 
                 if (np.random.rand() < candidate_set_size2 / (candidate_set_size2 + candidate_set_size)):
                     position_bar = position_bar2
+
+                # criteria1 = I[(θ+ − θ−)·r− ≥ 0]
                 criteria1 = np.dot((position_forward - position_backward), momentum_backward)>=0
+
+                # criteira2 = I[(θ+ − θ− )·r+ ≥ 0]
                 criteria2 = np.dot((position_forward - position_backward), momentum_forward)>=0
+
                 accept_set_bool = accept_set_bool2 and criteria1 and criteria2
                 candidate_set_size = candidate_set_size + candidate_set_size2
 
@@ -648,12 +668,18 @@ class NoUTurnSampler(HamiltonianMCda):
         """
         Returns a sample using a single iteration of NUTS
         """
+
+        # Re-sampling momentum
         momentum = np.random.normal(0, 1, len(position))
+
+        # Initializations
         depth = 0
         position_backward, position_forward = position, position
         momentum_backward, momentum_forward = momentum, momentum
         candidate_set_size = accept_set_bool = 1
         _, log_pdf = self.model.get_gradient_log_pdf(position, self.grad_log_pdf)
+
+        # Resample slice variable `u`
         slice_var = np.random.uniform(0, np.exp(log_pdf - 0.5 * np.dot(momentum, momentum)))
 
         while accept_set_bool == 1:
@@ -673,6 +699,7 @@ class NoUTurnSampler(HamiltonianMCda):
                     position = position_bar
 
             candidate_set_size += candidate_set_size2
+
             criteria1 = np.dot((position_forward - position_backward), momentum_backward)>=0
             criteria2 = np.dot((position_forward - position_backward), momentum_forward)>=0
             accept_set_bool = accept_set_bool2 and criteria1 and criteria2
@@ -692,8 +719,6 @@ class NoUTurnSampler(HamiltonianMCda):
         types = [(var_name, 'float') for var_name in self.model.variables]
         samples = np.zeros(num_samples, dtype=types).view(np.recarray)
 
-        # Assigning after converting into tuple because value was being changed after assignment
-        # Reason for this is unknown
         samples[0] = tuple(initial_pos)
         position_m = initial_pos
 
@@ -706,3 +731,92 @@ class NoUTurnSampler(HamiltonianMCda):
             return pd.DataFrame.from_records(samples)
 
         return samples
+
+    def generate_sample(self, initial_pos, stepsize=None):
+        """
+        """
+        initial_pos = _check_1d_array_object(initial_pos, 'initial_pos')
+        _check_length_equal(initial_pos, self.model.variables, 'initial_pos', 'model.variables')
+
+        if stepsize is None:
+            stepsize = self._find_reasonable_stepsize(initial_pos)
+
+        position_m = initial_pos
+
+        for i in range(0, num_samples):
+
+            position_m = self._sample(position_m, stepsize)
+
+            yield position_m
+
+class NoUTurnSamplerDA(NoUTurnSampler):
+    """
+
+    References
+    ----------
+    Matthew D. Hoffman, Andrew Gelman, The No-U-Turn Sampler: Adaptively
+    Setting Path Lengths in Hamiltonian Monte Carlo. Journal of
+    Machine Learning Research 15 (2014) 1351-1381
+    Algorithm 6 : No-U-Turn Sampler with Dual Averaging
+    """
+    def __init__(self, model, grad_log_pdf=None, simulate_dynamics=LeapFrog, delta=0.65):
+
+        if not isinstance(delta, float) or delta > 1.0 or delta < 0.0:
+            raise AttributeError(
+                "delta should be a floating value in between 0 and 1")
+
+        self.delta = delta
+
+        super(NoUTurnSamplerDA, self).__init__(model=model, grad_log_pdf=grad_log_pdf,
+                                              simulate_dynamics=simulate_dynamics)
+
+    def _build_tree(self, position, momentum, slice_var, direction, depth, stepsize, position0, momentum0):
+        """
+        """
+        if depth == 0:
+            # Take single leapfrog step in the given direction
+            position_bar, momentum_bar, grad_bar = self.simulate_dynamics(self.model, position, momentum, stepsize,
+                                                                          self.grad_log_pdf).get_proposed_values()
+
+            _, logp_bar = self.model.get_gradient_log_pdf(position_bar, self.grad_log_pdf)
+
+            hamiltonian = logp_bar - 0.5 * np.dot(momentum_bar, momentum_bar)
+
+            candidate_set_size = slice_var < np.exp(hamiltonian)
+            accept_set_bool = hamiltonian > np.log(slice_var) - 10000 # delta_max = 10000
+
+            alpha = min(1, self._acceptance_prob(position, position_bar, momentum, momentum_bar))
+
+            return (position_bar, momentum_bar, position_bar, momentum_bar, position_bar,
+                    candidate_set_size, accept_set_bool, alpha, 1)
+
+        else:
+            (position_backward, momentum_backward, position_forward, momentum_forward, candidate_set_size,
+             accept_set_bool, alpha, n_aplha) = self._build_tree(position, momentum, slice_var, direction, depth-1,
+                                                                 stepsize, position0, momentum0)
+
+             if accept_set_bool == 1:
+                 if direction == -1:
+                    # Build tree in backward direction
+                    (position_backward, momentum_backward, _, _, position_bar2, candidate_set_size2, accept_set_bool2,
+                     alpha2, n_alpha2) = self._build_tree(position_backward, momentum_backward, slice_var, direction,
+                                                          depth-1, stepsize, position0, momentum0)
+                else:
+                    # Build tree in forward direction
+                    (_, _, position_forward, momentum_forward, position_bar2, candidate_set_size2, accept_set_bool2,
+                     alpha2, n_alpha2) = self._build_tree(position_forward, momentum_forward, slice_var, direction,
+                                                          depth-1, stepsize, position0, momentum0)
+
+                if (np.random.rand() < candidate_set_size2 / (candidate_set_size2 + candidate_set_size)):
+                    position_bar = position_bar2
+
+                alpha += alpha2
+                n_aplha += n_alpha2
+                criteria1 = np.dot((position_forward - position_backward), momentum_backward)>=0
+                criteria2 = np.dot((position_forward - position_backward), momentum_forward)>=0
+
+                accept_set_bool = accept_set_bool2 and criteria1 and criteria2
+                candidate_set_size = candidate_set_size + candidate_set_size2
+
+            return (position_backward, momentum_backward, position_forward, momentum_forward, position_bar,
+                    candidate_set_size, accept_set_bool, aplha, n_alpha)
