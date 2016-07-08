89fd6459ea8196edf1e472d33d5359228e3d473c
Yashu Seth <yashuseth2503@gmail.com>
2016-07-08 17:05:54 +0530

    added tests for CanonicalFactor assignment and to_joint_gaussian

--- pgmpy/tests/test_factors/test_continuous/test_Canonical_Factor.py
+++ pgmpy/tests/test_factors/test_continuous/test_Canonical_Factor.py
@@ -47,3 +47,35 @@ class TestCanonicalFactor(unittest.TestCase):
         self.assertRaises(ValueError, CanonicalFactor, ['x1', 'x3'],
                          np.array([[1.1, -1, 0], [-1, 4, -2], [0, -2, 4]]),
                          np.array([1, 2, 3]), 7)
+
+
+class TestJGDMethods(unittest.TestCase):
+    def setUp(self):
+        self.phi1 = CanonicalFactor(['x1', 'x2', 'x3'],
+                                    np.array([[1.1, -1, 0], [-1, 4, -2], [0, -2, 4]]),
+                                    np.array([[1], [4.7], [-1]]), -2)
+        self.phi2 = CanonicalFactor(['x'], [[1]], [0], 1)
+        self.phi3 = self.phi1.copy()
+
+        self.gauss_phi1 = JGD(['x1', 'x2', 'x3'],
+                              np.array([[3.13043478], [2.44347826], [0.97173913]]),
+                              np.array([[ 1.30434783,  0.43478261,  0.2173913 ],
+                                       [ 0.43478261,  0.47826087,  0.23913043],
+                                       [ 0.2173913 ,  0.23913043,  0.36956522]], dtype=float))
+        self.gauss_phi2 = JGD(['x'], np.array([0]), np.array([[1]]))
+
+    def test_assignment(self):
+        np_test.assert_almost_equal(self.phi1.assignment(*[1, 2, 3]), 1.217470031e-06)
+        np_test.assert_almost_equal(self.phi2.assignment(1.234), 0.186314991823)
+
+    def test_to_joint_gaussian(self):
+        jgd1 = self.phi1.to_joint_gaussian()
+        jgd2 = self.phi2.to_joint_gaussian()
+
+        self.assertEqual(jgd1.variables, self.gauss_phi1.variables)
+        np_test.assert_almost_equal(jgd1.covariance, self.gauss_phi1.covariance)
+        np_test.assert_almost_equal(jgd1.mean, self.gauss_phi1.mean)
+
+        self.assertEqual(jgd2.variables, self.gauss_phi2.variables)
+        np_test.assert_almost_equal(jgd2.covariance, self.gauss_phi2.covariance)
+        np_test.assert_almost_equal(jgd2.mean, self.gauss_phi2.mean)
