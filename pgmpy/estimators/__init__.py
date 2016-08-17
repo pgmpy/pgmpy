a85ea11c8571b67abf15a36af66665b0dee145a3
chrisittner <mail@chrisittner.de>
2016-08-17 15:10:16 +0200

    Added HillClimbSearch structure estimator for discrete BNs

--- pgmpy/estimators/__init__.py
+++ pgmpy/estimators/__init__.py
@@ -6,8 +6,9 @@ from pgmpy.estimators.K2Score import K2Score
 from pgmpy.estimators.BdeuScore import BdeuScore
 from pgmpy.estimators.BicScore import BicScore
 from pgmpy.estimators.ExhaustiveSearch import ExhaustiveSearch
+from pgmpy.estimators.HillClimbSearch import HillClimbSearch
 
 __all__ = ['BaseEstimator',
            'ParameterEstimator', 'MaximumLikelihoodEstimator', 'BayesianEstimator',
-           'StructureEstimator', 'ExhaustiveSearch',
+           'StructureEstimator', 'ExhaustiveSearch', 'HillClimbSearch',
            'StructureScore', 'K2Score', 'BdeuScore', 'BicScore']
