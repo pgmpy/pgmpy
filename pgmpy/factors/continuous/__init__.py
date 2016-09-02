48541c1720b08ee889b631906032c3c1cf7598e2
Ankur Ankan <ankurankan@gmail.com>
2016-09-02 08:12:49 +0200

    deleted ContinousNode

--- pgmpy/factors/continuous/__init__.py
+++ pgmpy/factors/continuous/__init__.py
@@ -2,13 +2,11 @@ from .discretize import BaseDiscretizer, RoundingDiscretizer, UnbiasedDiscretize
 from .ContinuousFactor import ContinuousFactor
 from .JointGaussianDistribution import JointGaussianDistribution
 from .CanonicalFactor import CanonicalFactor
-from .ContinuousNode import ContinuousNode
 from .LinearGaussianCPD import LinearGaussianCPD
 
 
 __all__ = ['CanonicalFactor',
            'ContinuousFactor',
-           'ContinuousNode',
            'JointGaussianDistribution',
            'LinearGaussianCPD'
            'BaseDiscretizer',
