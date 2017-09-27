dd00b1e5aa059bccf0c0acf35c9fda8c5f1ae371
Ankur Ankan <ankurankan@gmail.com>
2017-09-27 13:54:53 +0200

    update version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="pgmpy",
-    version="0.1.3",
+    version="0.1.4",
     description="A library for Probabilistic Graphical Models",
     packages=find_packages(exclude=['tests']),
     author=open("AUTHORS.rst").read(),
