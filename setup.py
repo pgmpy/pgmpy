dbc3de21889bf5218cb1b589588c90e7d50b3e80
Ankur Ankan <ankurankan@gmail.com>
2017-09-28 20:52:15 +0200

    bump up version number

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="pgmpy",
-    version="0.1.4",
+    version="0.1.5",
     description="A library for Probabilistic Graphical Models",
     packages=find_packages(exclude=['tests']),
     author="Ankur Ankan",
