7050d7f49be30e208440284af2cab606f050cac4
Ankur Ankan <ankurankan@gmail.com>
2017-11-24 12:02:36 +0100

    Removes ContinuousFactor class:
    1. ContinuousFactor is not supposed to be doing things required for CustomDistribution.
    2. CustomDistribution is almost same as ContinuousFactor

--- pgmpy/factors/continuous/__init__.py
+++ pgmpy/factors/continuous/__init__.py
@@ -1,9 +1,7 @@
-from .ContinuousFactor import ContinuousFactor
 from .LinearGaussianCPD import LinearGaussianCPD
 from .discretize import BaseDiscretizer, RoundingDiscretizer, UnbiasedDiscretizer
 
-__all__ = ['ContinuousFactor',
-           'LinearGaussianCPD'
+__all__ = ['LinearGaussianCPD'
            'BaseDiscretizer',
            'RoundingDiscretizer',
            'UnbiasedDiscretizer'
