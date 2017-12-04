03f2543707841cf1f96851efedc3aec37647afbd
Ankur Ankan <ankurankan@gmail.com>
2017-12-04 13:46:06 +0100

    Adds LinearGaussianCPD class

--- pgmpy/factors/continuous/LinearGaussianCPD.py
+++ pgmpy/factors/continuous/LinearGaussianCPD.py
@@ -3,9 +3,10 @@
 from __future__ import division
 
 import numpy as np
-from scipy.stats import multivariate_normal
 
+from pgmpy.extern import six
 from pgmpy.factors.base import BaseFactor
+from pgmpy.factors.distributions import GaussianDistribution
 
 
 class LinearGaussianCPD(BaseFactor):
@@ -29,106 +30,31 @@ class LinearGaussianCPD(BaseFactor):
     p(Y |x) = N(β0 + β.T * x ; σ2)
 
     """
-    def __init__(self, variable, beta, variance, evidence=[]):
+    def __init__(self, variable, mean, cov, evidence=[], dist=None):
         """
+        Initialize a new LinearGaussianCPD object.
+
         Parameters
         ----------
+        variable: Any hashable python object (optional)
+            The variable on which the CPD is defined. If dist is an instance of `BaseDistribution`,
+            the variable would automatically be taken from dist.
 
-        variable: any hashable python object
-            The variable whose CPD is defined.
-
-        beta: iterable of int or float
-            An iterable representing the coefficient vector of the linear equation.
-            The first term represents the constant term in the linear equation.
-
-        variance: int, float
-            The variance of the variable defined.
-
-        evidence: iterable of any hashabale python objects
-            An iterable of the parents of the variable. None if there are no parents.
 
         Examples
         --------
 
-        # For P(Y| X1, X2, X3) = N(-2x1 + 3x2 + 7x3 + 0.2; 9.6)
-
-        >>> cpd = LinearGaussianCPD('Y',  [0.2, -2, 3, 7], 9.6, ['X1', 'X2', 'X3'])
-        >>> cpd.variable
-        'Y'
-        >>> cpd.variance
-        9.6
-        >>> cpd.evidence
-        ['x1', 'x2', 'x3']
-        >>> cpd.beta_vector
-        [0.2, -2, 3, 7]
-
         """
-        self.variable = variable
-        self.beta = beta
-        self.beta_0 = beta[0]
-        self.variance = variance
-
-        if len(evidence) != len(beta) - 1:
-            raise ValueError("The number of variables in evidence must be one less than the "
-                             "length of the beta vector.")
-
-        self.evidence = evidence
-        self.beta_vector = np.asarray(beta[1:])
-
-        variables = [variable] + evidence
-        super(LinearGaussianCPD, self).__init__(variables, pdf='gaussian',
-                                                mean=self.beta_vector,
-                                                covariance=self.variance)
-
-    @property
-    def pdf(self):
-
-        def _pdf(*args):
-            # The first element of args is the value of the variable on which CPD is defined
-            # and the rest of the elements give the mean values of the parent variables.
-            mean = sum([arg * coeff for (arg, coeff) in zip(args[1:], self.beta_vector)]) + self.beta_0
-            return multivariate_normal.pdf(args[0], np.array(mean), np.array([[self.variance]]))
-
-        return _pdf
-
-    def copy(self):
-        """
-        Returns a copy of the distribution.
-
-        Returns
-        -------
-        LinearGaussianCPD: copy of the distribution
-
-        Examples
-        --------
-        >>> from pgmpy.factors.continuous import LinearGaussianCPD
-        >>> cpd = LinearGaussianCPD('Y',  [0.2, -2, 3, 7], 9.6, ['X1', 'X2', 'X3'])
-        >>> copy_cpd = cpd.copy()
-        >>> copy_cpd.variable
-        'Y'
-        >>> copy_cpd.evidence
-        ['X1', 'X2', 'X3']
-        """
-        copy_cpd = LinearGaussianCPD(self.variable, self.beta, self.variance,
-                                     list(self.evidence))
-
-        return copy_cpd
-
-    def __str__(self):
-        if self.evidence and list(self.beta_vector):
-            # P(Y| X1, X2, X3) = N(-2*X1_mu + 3*X2_mu + 7*X3_mu; 0.2)
-            rep_str = "P({node} | {parents}) = N({mu} + {b_0}; {sigma})".format(
-                node=str(self.variable),
-                parents=', '.join([str(var) for var in self.evidence]),
-                mu=" + ".join(["{coeff}*{parent}".format(
-                    coeff=coeff, parent=parent) for coeff, parent in
-                                zip(self.beta_vector, self.evidence)]),
-                b_0=str(self.beta_0),
-                sigma=str(self.variance))
+        if dist:
+            super(LinearGaussianCPD, self).__init__(dist=dist)
         else:
-            # P(X) = N(1, 4)
-            rep_str = "P({X}) = N({beta_0}; {variance})".format(
-                X=str(self.variable),
-                beta_0=str(self.beta_0),
-                variance=str(self.variance))
-        return rep_str
+            variables = [variable]
+            if len(mean) != len(evidence) + 1:
+                raise ValueError("mean: Expected size: {ex}, got: {size}".format(
+                    ex=len(evidence)+1, size=len(mean)))
+
+            super(LinearGaussianCPD, self).__init__(variables=variables,
+                                                    mean=mean,
+                                                    cov=cov,
+                                                    evidence=evidence)
+
