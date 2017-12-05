44e67c02028b72ef958710597934976e02397e68
Ankur Ankan <ankurankan@gmail.com>
2017-12-05 19:22:29 +0100

    Adds Linear Gaussian CPD

--- pgmpy/factors/continuous/LinearGaussianCPD.py
+++ pgmpy/factors/continuous/LinearGaussianCPD.py
@@ -6,10 +6,11 @@ import numpy as np
 
 from pgmpy.extern import six
 from pgmpy.factors.base import BaseFactor
+from pgmpy.factors.continuous import ContinuousFactor
 from pgmpy.factors.distributions import GaussianDistribution
 
 
-class LinearGaussianCPD(BaseFactor):
+class LinearGaussianCPD(ContinuousFactor):
     u"""
     For, X -> Y the Linear Gaussian model assumes that the mean
     of Y is a linear function of mean of X and the variance of Y does
@@ -30,31 +31,56 @@ class LinearGaussianCPD(BaseFactor):
     p(Y |x) = N(β0 + β.T * x ; σ2)
 
     """
-    def __init__(self, variable, mean, cov, evidence=[], dist=None):
+    def __init__(self, variable=None, mean=None, cov=None, evidence=[], dist=None):
         """
         Initialize a new LinearGaussianCPD object.
 
         Parameters
         ----------
         variable: Any hashable python object (optional)
-            The variable on which the CPD is defined. If dist is an instance of `BaseDistribution`,
-            the variable would automatically be taken from dist.
+            The variable on which the CPD is defined.
+            Not required if dist is not None.
 
+        mean: list/iterable (optional)
+            The mean of the Gaussian Distribution. For a Linear Conditional Gaussian 
+            Distribution `P(Y | A, B, C) = N(2A + 3B - 4C + 1; 1)`, the mean vector
+            should be [2, 3, -4].
+            Not required if `dist` is not None.
+
+        cov: float (optional)
+            The covariance of the `varaible`.
+            Not reuired if `dist` is not None.
+
+        evidence: list/iterable (optional)
+            The variables on which the LinearGaussianCPD is conditioned on. 
+            Not required if `dist` is not None or there are no conditional variables.
+
+        dist: Instance of pgmpy.factors.distributions.GaussianDistribution (optional)
+            The GaussianDistribution which is to be represented by the LinearGaussianCPD
+            instance. The GaussianDistribution should have only one variable.
+            Not required if all other parameters are given.
 
         Examples
         --------
+        For representing the following CPD:
+            P(Y | A, B, C) = N(2A + 3B - 4C + 1; 1)
 
+        >>> from pgmpy.factors.continuous import LinearGaussianCPD
+        >>> cpd = LinearGaussianCPD(variable='Y', mean=[2, 3, -4, 1], cov=1)
         """
         if dist:
+            if len(dist.variables) != 1:
+                raise ValueError("The distribution should be defined on a single variable")
             super(LinearGaussianCPD, self).__init__(dist=dist)
         else:
             variables = [variable]
             if len(mean) != len(evidence) + 1:
                 raise ValueError("mean: Expected size: {ex}, got: {size}".format(
                     ex=len(evidence)+1, size=len(mean)))
-
-            super(LinearGaussianCPD, self).__init__(variables=variables,
-                                                    mean=mean,
-                                                    cov=cov,
-                                                    evidence=evidence)
-
+            
+            dist = GaussianDistribution(variables=variables,
+                                        mean=mean,
+                                        cov=cov,
+                                        evidence=evidence)
+                                        
+            super(LinearGaussianCPD, self).__init__(dist=dist)
--- pgmpy/tests/test_factors/test_continuous/test_Linear_Gaussain_CPD.py
+++ pgmpy/tests/test_factors/test_continuous/test_Linear_Gaussain_CPD.py
@@ -3,75 +3,31 @@ import unittest
 import numpy.testing as np_test
 
 from pgmpy.factors.continuous import LinearGaussianCPD
+from pgmpy.factors.distributions import GaussianDistribution
+
+class TestInit(unittest.TestCase):
+    def test_init(self):
+        cpd = LinearGaussianCPD(variable='A', mean=[1, 1, 1], cov=1, evidence=['B', 'C'])
+        self.assertEqual(list(cpd.dist.variables), ['A'])
+        np_test.assert_almost_equal(cpd.dist.mean, [[1, 1, 1]])
+        np_test.assert_almost_equal(cpd.dist.cov, [[1]])
+        self.assertEqual(list(cpd.dist.evidence), ['B', 'C'])
+
+    def test_init_wrong_mean_size(self):
+        self.assertRaises(ValueError, LinearGaussianCPD, variable='A', mean=[1, 1, 1, 1],
+                          cov=1, evidence=['B', 'C'])
+
+    def test_init_single_var_dist(self):
+        single_var_dist = GaussianDistribution(variables=['A'], mean=[1, 1, 1], cov=[1],
+                                               evidence=['B', 'C'])
+        cpd = LinearGaussianCPD(dist=single_var_dist)
+        self.assertEqual(list(cpd.dist.variables), ['A'])
+        np_test.assert_almost_equal(cpd.dist.mean, [[1, 1, 1]])
+        np_test.assert_almost_equal(cpd.dist.cov, [[1]])
+        self.assertEqual(list(cpd.dist.evidence), ['B', 'C'])
 
 
-class TestLGCPD(unittest.TestCase):
-    @unittest.skip("TODO")
-    def test_class_init(self):
-        cpd1 = LinearGaussianCPD('x', [0.23], 0.56)
-        self.assertEqual(cpd1.variable, 'x')
-        self.assertEqual(cpd1.beta_0, 0.23)
-        self.assertEqual(cpd1.variance, 0.56)
-
-        cpd2 = LinearGaussianCPD('y', [0.67, 1, 4.56, 8], 2,
-                                 ['x1', 'x2', 'x3'])
-        self.assertEqual(cpd2.variable, 'y')
-        self.assertEqual(cpd2.beta_0, 0.67)
-        self.assertEqual(cpd2.variance, 2)
-        self.assertEqual(cpd2.evidence, ['x1', 'x2', 'x3'])
-        np_test.assert_array_equal(cpd2.beta_vector, [1, 4.56, 8])
-
-        self.assertRaises(ValueError, LinearGaussianCPD, 'x', [1, 1, 2], 2,
-                          ['a', 'b', 'c'])
-        self.assertRaises(ValueError, LinearGaussianCPD, 'x', [1, 1, 2, 3], 2,
-                          ['a', 'b'])
-
-    @unittest.skip("TODO")
-    def test_pdf(self):
-        cpd1 = LinearGaussianCPD('x', [0.23], 0.56)
-        cpd2 = LinearGaussianCPD('y', [0.67, 1, 4.56, 8], 2, ['x1', 'x2', 'x3'])
-        np_test.assert_almost_equal(cpd1.assignment(1), 0.3139868)
-        np_test.assert_almost_equal(cpd2.assignment(1, 1.2, 2.3, 3.4), 1.076e-162)
-
-    @unittest.skip("TODO")
-    def test_copy(self):
-        cpd = LinearGaussianCPD('y', [0.67, 1, 4.56, 8], 2, ['x1', 'x2', 'x3'])
-        copy = cpd.copy()
-
-        self.assertEqual(cpd.variable, copy.variable)
-        self.assertEqual(cpd.beta_0, copy.beta_0)
-        self.assertEqual(cpd.variance, copy.variance)
-        np_test.assert_array_equal(cpd.beta_vector, copy.beta_vector)
-        self.assertEqual(cpd.evidence, copy.evidence)
-
-        cpd.variable = 'z'
-        self.assertEqual(copy.variable, 'y')
-        cpd.variance = 0
-        self.assertEqual(copy.variance, 2)
-        cpd.beta_0 = 1
-        self.assertEqual(copy.beta_0, 0.67)
-        cpd.evidence = ['p', 'q', 'r']
-        self.assertEqual(copy.evidence, ['x1', 'x2', 'x3'])
-        cpd.beta_vector = [2, 2, 2]
-        np_test.assert_array_equal(copy.beta_vector, [1, 4.56, 8])
-
-        copy = cpd.copy()
-
-        copy.variable = 'k'
-        self.assertEqual(cpd.variable, 'z')
-        copy.variance = 0.3
-        self.assertEqual(cpd.variance, 0)
-        copy.beta_0 = 1.5
-        self.assertEqual(cpd.beta_0, 1)
-        copy.evidence = ['a', 'b', 'c']
-        self.assertEqual(cpd.evidence, ['p', 'q', 'r'])
-        copy.beta_vector = [2.2, 2.2, 2.2]
-        np_test.assert_array_equal(cpd.beta_vector, [2, 2, 2])
-
-    @unittest.skip("TODO")
-    def test_str(self):
-        cpd1 = LinearGaussianCPD('x', [0.23], 0.56)
-        cpd2 = LinearGaussianCPD('y', [0.67, 1, 4.56, 8], 2, ['x1', 'x2', 'x3'])
-        self.assertEqual(cpd1.__str__(), "P(x) = N(0.23; 0.56)")
-        self.assertEqual(cpd2.__str__(), "P(y | x1, x2, x3) = N(1.0*x1 + "
-                                         "4.56*x2 + 8.0*x3 + 0.67; 2)")
+    def test_init_multi_var_dist(self):
+        self.assertRaises(ValueError, GaussianDistribution, variables=['A', 'B'], 
+                          mean=[[1, 1, 1], [1, 1, 1]], cov=[[1, 0], [0, 1]],
+                          evidence=['C', 'D'])
