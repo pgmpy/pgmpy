7393f8a1fe969d351153b44324cbd51a66766947
Ankur Ankan <ankurankan@gmail.com>
2019-03-12 10:08:21 +0100

    Adds method to convert SEMLISREL to SEMGraph

--- pgmpy/models/SEMLISREL.py
+++ pgmpy/models/SEMLISREL.py
@@ -46,6 +46,15 @@ class SEMLISREL:
             as params.
 
             If None all the parameters are learnable.
+
+        Returns
+        -------
+        pgmpy.models.SEMLISREL instance: An instance of the object with initalized values.
+
+        Examples
+        --------
+        >>> from pgmpy.models import SEMLISREL
+        # TODO: Finish this example
         """
         if str_model:
             raise NotImplementedError("Specification of model as a string is not supported yet")
@@ -68,15 +77,27 @@ class SEMLISREL:
         for key in self.adjecency.keys():
             self.masks[key] = np.multiply(np.where(self.fixed_masks[key] != 0, 0.0, 1.0), self.adjecency[key])
 
-    def __to_minimal_graph(self, graph, err_graph):
+    def __to_minimal_graph(self, graph):
         """
         Takes a standard LISREL graph structure and removes all the extra added latents.
 
+        Parameters
+        ----------
         graph: nx.DiGraph
             The graph structure except for the error terms.
 
         err_graph: nx.Graph
             The error graph structure.
+
+        Returns
+        -------
+        nx.DiGraph: The graph structure after removing all the reducible latent terms
+                    which start with `_l_`.
+
+        Examples
+        --------
+        >>> from pgmpy.models import SEMGraph
+        # TODO: Finish this example.
         """
         mapping = {}
         for node in chain(self.var_names['xi'], self.var_names['eta']):
@@ -91,129 +112,56 @@ class SEMLISREL:
 
     def to_SEMGraph(self):
         """
-        Creates a graph structure from the LISREL represntation.
-        """
-        # Construct the full ajceceny matrix.
-        # Construct the full error adjececny matrix.
-        # Construct graphs
-        # Minimal graph
-        # Return
+        Creates a graph structure from the LISREL representation.
 
-#         """
-#         Sets the params in the graph structure. The model is defined as:
-# 
-#         eta = B eta + Gamma xi + zeta
-#         y = wedge_y eta + epsilon
-#         x = wedge_x xi + delta
-#         Psi = COV(eta)
-#         Phi = COV(xi)
-#         theta_e = COV(y)
-#         theta_{del} = COV(x)
-# 
-#         Parameters
-#         ----------
-#         params: dict
-#             A dictionary with the parameters to set to the model. Each array should assume that
-#             the variable lists eta, xi, y and x are sorted.
-# 
-#             Expected keys in `params`:
-#                 1. B: array (m x m)
-#                 2. gamma: array (m x n)
-#                 3. wedge_y: array (p x m)
-#                 4. wedge_x: array (q x n)
-#                 5. phi: array (n x n)
-#                 6. theta_e: array (p x p)
-#                 7. theta_del: array (q x q)
-#                 8. psi: array (m x m)
-#             m = len(self.eta)
-#             n = len(self.xi)
-#             p = len(self.y)
-#             q = len(self.x)
-# 
-#         Returns
-#         -------
-#         None
-# 
-#         Examples
-#         --------
-#         """
-#         # Edges to use to inialize SEMGraph.
-#         ebunch = []
-#         err_ebunch = []
-#         err_var = {}
-# 
-#         # Sort the variable names so that it gets correct values assigned
-#         eta = self.var_names['eta']
-#         xi = self.var_names['xi']
-#         y = self.var_names['y']
-#         x = self.var_names['x']
-# 
-#         # Add edges and weights
-#         for i, u_var in enumerate(eta):
-#             for j, v_var in enumerate(eta):
-#                 if self.params['B'][i, j] == 1:
-#                     ebunch.append((v_var, u_var,
-#                                    np.NaN if self.fixed_params['B'][i, j]==0 else self.fixed_params['B'][i, j]))
-# 
-#         for i, u_var in enumerate(eta):
-#             for j, v_var in enumerate(xi):
-#                 if self.params['gamma'][i, j] == 1:
-#                     ebunch.append(
-#                         (v_var, u_var,
-#                          np.NaN if self.fixed_params['gamma'][i, j]==0 else self.fixed_params['gamma'][i, j]))
-# 
-#         for i, u_var in enumerate(y):
-#             for j, v_var in enumerate(eta):
-#                 if self.parms['wedge_y'][i, j] == 1:
-#                     ebunch.append((v_var, u_var, np.NaN if self.fixed_params['wedge_y'][i, j]==0
-#                                    else self.fixed_params['wedge_y'][i, j]))
-# 
-#         for i, u_var in enumerate(x):
-#             for j, v_var in enumerate(xi):
-#                 if self.params['wedge_x'][i, j] == 1:
-#                     ebunch.append((v_var, u_var, np.NaN if self.fixed_params['wedge_x'][i, j]==0
-#                                    else self.fixed_params['wedge_x'][i, j]))
-# 
-#         for i, u_var in enumerate(xi):
-#             for j, v_var in enumerate(xi):
-#                 if i == j:
-#                     err_var[u_var] = (np.NaN if self.fixed_params['phi'][i, j]==0 else
-#                                       self.fixed_params['phi'][i, j])
-#                 else:
-#                     err_ebunch.append(u_var, v_var, np.NaN if self.fixed_params['phi'][i, j]==0 else
-#                                       self.fixed_params['phi'][i, j])
-# 
-#         for i, u_var in enumerate(eta):
-#             for j, v_var in enumerate(eta):
-#                 if i == j:
-#                     err_var[u_var] = (np.NaN if self.fixed_params['psi'][i, j]==0 else
-#                                       self.fixed_params['psi'][i, j])
-#                 else:
-#                     err_ebunch.append(u_var, v_var, np.NaN if self.fixed_params['psi'][i, j]==0 else
-#                                       self.fixed_params['psi'][i, j])
-# 
-#         for i, u_var in enumerate(y):
-#             for j, v_var in enumerate(y):
-#                 if i == j:
-#                     err_var[u_var] = (np.NaN if self.fixed_params['theta_e'][i, j]==0 else
-#                                       self.fixed_params['theta_e'][i, j])
-#                 else:
-#                     err_ebunch.append(u_var, v_var, np.NaN if self.fixed_params['theta_e'][i, j]==0 else
-#                                       self.fixed_params['theta_e'][i, j])
-# 
-#         for i, u_var in enumerate(x):
-#             for j, v_var in enumerate(x):
-#                 if i == j:
-#                     err_var[u_var] = (np.NaN if self.fixed_params['theta_del'][i, j]==0 else
-#                                       self.fixed_params['theta_del'][i, j])
-#                 else:
-#                     err_ebunch.append(u_var, v_var, np.NaN if self.fixed_params['theta_del'][i, j]==0 else
-#                                       self.fixed_params['theta_del'][i, j])
-# 
-#         # Construct the SEMGraph object and return it.
-#         from pgmpy.models import SEMGraph
-#         sem_graph = SEMGraph(ebunch=ebunch, latents=eta+xi, err_corr=err_ebunch)
-#         for node in sem_graph.err_graph.nodes():
-#             sem_graph.err_graph[node]['var'] = err_var[node]
-# 
-#         return sem_graph
+        Returns
+        -------
+        pgmpy.models.SEMGraph instance: A path model of the model.
+
+        Examples
+        --------
+        >>> from pgmpy.models import SEMLISREL
+        >>> model = SEMLISREL()
+        # TODO: Finish this example
+        """
+        y_vars, x_vars, eta_vars, xi_vars = (self.var_names['y'], self.var_names['x'],
+                                             self.var_names['eta'], self.var_names['xi'])
+
+        p, q, m, n = (len(y_vars), len(x_vars), len(eta_vars), len(xi_vars))
+
+        graph_adj_matrix = np.zeros(p+q+m+n, p+q+m+n)
+        err_graph_adj_matrix = np.zeros(p+q+m+n, p+q+m+n)
+
+        # Replacing non fixed edges with np.NaN as networkx assumes that as edges as well
+        # and sets weights to NaN.
+        adj_sub_matrices = {}
+        for key in self.masks.keys():
+            adj_sub_matrices[key] = np.where((self.masks[key] + self.fixed_masks[key]) == 1,
+                                             np.NaN, self.masks[key] + self.fixed_masks[key])
+
+        node_dict = {i: node_name for i, node_name in enumerate(chain(y_vars, x_vars, eta_vars, xi_vars))}
+        graph_adj_matrix[p+q:p+q+m, p+q:p+q+m] = adj_sub_matrices['B']
+        graph_adj_matrix[p+q:p+q+m, p+q+m:] = adj_sub_matrices['gamma']
+        graph_adj_matrix[0:p, p+q:p+q+m] = adj_sub_matrices['wedge_y']
+        graph_adj_matrix[p:p+q, p+q+m:] = adj_sub_matrices['wedge_x']
+        graph = nx.convert_matrix.from_numpy_matrix(graph_adj_matrix.T, create_using=nx.DiGraph)
+        graph = nx.relabel_nodes(graph, mapping=node_dict)
+
+        err_graph_adj_matrix[:p, :p] = adj_sub_matrices['theta_e']
+        err_graph_adj_matrix[p:p+q, p:p+q] = adj_sub_matrices['theta_del']
+        err_graph_adj_matrix[p+q:p+q+m, p+q:p+q+m] = adj_sub_matrices['psi']
+        err_graph_adj_matrix[p+q+m:, p+q+m:] = adj_sub_matrices['phi']
+        err_graph = nx.convert_matrix.from_numpy_matrix(err_graph_adj_matrix.T, create_using=nx.Graph)
+        err_graph = nx.relabel_nodes(err_graph, mapping=node_dict)
+
+        minimal_graph = self.__to_minimal_graph(graph)
+
+        err_var = {node_dict[i]: err_graph_adj_matrix[i, i] for i in range(p+q+m+n)}
+
+        from pgmpy.models import SEMGraph
+        sem_graph = SEMGraph(ebunch=minimal_graph.edges(),
+                             latents=list(filter(lambda t: not t.startswith('_l_'), eta_vars+xi_vars)),
+                             err_corr=minimal_err_graph.edges(),
+                             err_var=err_var)
+        return sem_graph
+
