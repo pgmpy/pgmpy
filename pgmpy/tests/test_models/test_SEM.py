772c12bacc801d8487f22d0f43c7ae4e53f7a116
Ankur Ankan <ankurankan@gmail.com>
2019-03-23 11:40:21 +0100

    Adds tests for SEM LISREL

--- pgmpy/tests/test_models/test_SEM.py
+++ pgmpy/tests/test_models/test_SEM.py
@@ -598,3 +598,30 @@ class TestSEMGraph(unittest.TestCase):
                                     err_var={})
         self.assertEqual(small_test_model.get_conditional_ivs('X', 'Y'), [('I', 'W')])
 
+class TESTSEMLISREL(unittest.TestCase):
+    def setUp(self):
+        self.demo = SEMGraph(ebunch=[('xi1', 'x1'),
+                                     ('xi1', 'x2'),
+                                     ('xi1', 'x3'),
+                                     ('xi1', 'eta1'),
+                                     ('eta1', 'y1'),
+                                     ('eta1', 'y2'),
+                                     ('eta1', 'y3'),
+                                     ('eta1', 'y4'),
+                                     ('eta1', 'eta2'),
+                                     ('xi1', 'eta2'),
+                                     ('eta2', 'y5'),
+                                     ('eta2', 'y6'),
+                                     ('eta2', 'y7'),
+                                     ('eta2', 'y8')],
+                             latents=['xi1', 'eta1', 'eta2'],
+                             err_corr=[('y1', 'y5'),
+                                       ('y2', 'y6'),
+                                       ('y2', 'y4'),
+                                       ('y3', 'y7'),
+                                       ('y4', 'y8'),
+                                       ('y6', 'y8')])
+        self.demo_lisrel = self.demo.to_lisrel()
+
+    def test_get_ivs(self):
+        self.demo_lisrel.get_ivs()
