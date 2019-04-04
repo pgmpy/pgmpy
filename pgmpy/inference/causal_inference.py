21b09b09875bca0840b78e07d7f11e6560f53545
mrklees <perusse.a@gmail.com>
2019-04-04 09:34:12 -0700

    stupid minor fix

--- pgmpy/inference/causal_inference.py
+++ pgmpy/inference/causal_inference.py
@@ -113,7 +113,7 @@ class CausalInference(Inference):
         for deconfounder in possible_deconfounders:
             seenbefore = False
             for cs in complete_sets:
-                print("{} in {}: {}".format(cs, deconfounder, cs in set(deconfounder))
+                print("{} in {}: {}".format(cs, deconfounder, cs in set(deconfounder)))
                 if cs in set(deconfounder):
                     # For each new deconfounder, we want to check if we've already seen a complete set of deconfounders
                     # as a subset of the proposed deconfounding set. If we have, don't search the tree any further.
