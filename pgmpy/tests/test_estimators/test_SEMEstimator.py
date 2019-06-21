707deef07b4e3656f13c33ff18acd67808934145
Ankur Ankan <ankurankan@gmail.com>
2019-06-21 14:53:02 +0200

    Test with more samples to make it robust

--- pgmpy/tests/test_estimators/test_SEMEstimator.py
+++ pgmpy/tests/test_estimators/test_SEMEstimator.py
@@ -163,7 +163,7 @@ class TestIVEstimator(unittest.TestCase):
             latents=["U"],
             err_var={"Z1": 1, "Z2": 1, "W": 1, "X": 1, "U": 1, "Y": 1},
         )
-        self.generated_data = self.model.to_lisrel().generate_samples(10000)
+        self.generated_data = self.model.to_lisrel().generate_samples(100000)
 
     def test_fit(self):
         estimator = IVEstimator(self.model)
