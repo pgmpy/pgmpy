794ecd36d1d4d45c88de1af5c7b18fefafc48590
Ankur Ankan <ankurankan@gmail.com>
2019-08-02 04:14:16 +0200

    Updates config file for dtypes

--- pgmpy/config.py
+++ pgmpy/config.py
@@ -1,4 +1,5 @@
 import torch
+import numpy as np
 
 
 # Check if pandas is available
@@ -10,7 +11,7 @@ except ImportError:
     HAS_PANDAS = False
 
 # Check if GPU is available
-def set_device(device_='cpu'):
+def set_device(device_="cpu"):
     """
     Set the device to be used for pytorch.
 
@@ -20,9 +21,9 @@ def set_device(device_='cpu'):
         Device to use for computation.
     """
     global device
-    if device_ == 'cpu':
+    if device_ == "cpu":
         device = torch.device("cpu")
-    elif device == 'gpu':
+    elif device == "gpu":
         if torch.cuda.is_available():
             device = torch.device("cuda")
         else:
@@ -30,8 +31,9 @@ def set_device(device_='cpu'):
     else:
         raise ValueError("Unknown device. Please select between cpu or gpu")
 
+
 # Set backend to use numpy or pytorch
-def set_backend(backend_='numpy'):
+def set_backend(backend_="numpy"):
     """
     Sets the backend framework to use.
 
@@ -42,20 +44,39 @@ def set_backend(backend_='numpy'):
     """
     global backend
 
-    if backend_ in ['numpy', 'pytorch']:
+    if backend_ in ["numpy", "pytorch"]:
         backend = backend_
     else:
         raise ValueError("Unknown backend. Please select between numpy or pytorch")
 
-def set_dtype(dtype_='float'):
+
+def set_dtype(dtype_="float"):
     """
     Globally set dtype for all arrays.
 
     Parameters
     ----------
     dtype: str
+        Options: float32 | float64 | float16 | uint8 | int8 | int16 | int32 | int64
     """
     global dtype
 
-    if dtype_ == 'float':
-        dtype = torch.float
+    if dtype_ in [
+        "float32",
+        "float64",
+        "float16",
+        "uint8",
+        "int8",
+        "int16",
+        "int32",
+        "int64",
+    ]:
+        if backend == "pytorch":
+            dtype = getattr(torch, dtype_)
+        elif backend == "numpy":
+            dtype = getattr(np, dtype_)
+
+    else:
+        raise ValueError(
+            "Argument for dtype_ not recognized. Please check docstring for acceptable values"
+        )
