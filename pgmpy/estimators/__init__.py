bf53903aa2cd6ab5cd97d2785785a039d14c5b0b
Ankur Ankan <ankurankan@gmail.com>
2019-09-19 18:23:37 +0200

    Fix import problem

--- pgmpy/estimators/__init__.py
+++ pgmpy/estimators/__init__.py
@@ -1,14 +1,15 @@
 from pgmpy.estimators.base import BaseEstimator, ParameterEstimator, StructureEstimator
-from pgmpy.estimators.MLE import MaximumLikelihoodEstimator
 from pgmpy.estimators.BayesianEstimator import BayesianEstimator
-from pgmpy.estimators.StructureScore import StructureScore
-from pgmpy.estimators.K2Score import K2Score
 from pgmpy.estimators.BdeuScore import BdeuScore
 from pgmpy.estimators.BicScore import BicScore
+from pgmpy.estimators.CITests import test_conditional_independence
+from pgmpy.estimators.ConstraintBasedEstimator import ConstraintBasedEstimator
 from pgmpy.estimators.ExhaustiveSearch import ExhaustiveSearch
 from pgmpy.estimators.HillClimbSearch import HillClimbSearch
-from pgmpy.estimators.ConstraintBasedEstimator import ConstraintBasedEstimator
+from pgmpy.estimators.K2Score import K2Score
+from pgmpy.estimators.MLE import MaximumLikelihoodEstimator
 from pgmpy.estimators.SEMEstimator import SEMEstimator, IVEstimator
+from pgmpy.estimators.StructureScore import StructureScore
 
 __all__ = [
     "BaseEstimator",
@@ -25,4 +26,5 @@ __all__ = [
     "BicScore",
     "SEMEstimator",
     "IVEstimator",
+    "test_conditional_independence",
 ]
