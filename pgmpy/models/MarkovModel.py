4170bf64445ac268749e6e3e58c5270f47bb9b11
Ankur Ankan <ankurankan@gmail.com>
2021-07-03 16:28:20 +0200

    Improve warning message

--- pgmpy/models/MarkovModel.py
+++ pgmpy/models/MarkovModel.py
@@ -6,7 +6,7 @@ from pgmpy.models import MarkovNetwork
 class MarkovModel(MarkovNetwork):
     def __init__(self, ebunch=None, latents=set()):
         warnings.warn(
-            "MarkovModel has been renamed to MarkovNetwork. MarkovModel will be removed in v0.1.17.",
+            "MarkovModel has been renamed to MarkovNetwork. Please use MarkovNetwork class, MarkovModel will be removed in future.",
             FutureWarning,
         )
         super(MarkovModel, self).__init__(ebunch=ebunch, latents=latents)
