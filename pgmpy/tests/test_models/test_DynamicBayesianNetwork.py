04386838da4d6c22e0d95df95065b21ca64cc9c5
Felipe <fgiro@uliege.be>
2021-08-03 14:41:41 +0200

    add comment for issue

--- pgmpy/tests/test_models/test_DynamicBayesianNetwork.py
+++ pgmpy/tests/test_models/test_DynamicBayesianNetwork.py
@@ -149,7 +149,7 @@ class TestDynamicBayesianNetworkMethods(unittest.TestCase):
         self.assertRaises(ValueError, self.network.get_interface_nodes, "-")
 
     def test_get_interface_nodes_1node2edges_divergent_edges(self):
-        # divergent interface edges from one node (a0->a1,b1)
+        # divergent interface edges from one node (a0->a1,b1). issue #1364
         self.network.add_edges_from(
             [
                 (("A", 0), ("A", 1)),
@@ -162,7 +162,7 @@ class TestDynamicBayesianNetworkMethods(unittest.TestCase):
         self.assertListEqual(self.network.get_interface_nodes(1), [("A", 1), ("B", 1)])
 
     def test_get_interface_nodes_convergent_edges(self):
-        # convergent interface edges to one node(a0,b0->b1)
+        # convergent interface edges to one node(a0,b0->b1). issue #1364
         self.network.add_edges_from(
             [
                 (("A", 0), ("B", 1)),
