4853d1dc5bb25b7a294ac889affbff51c959f917
Ankur Ankan <ankurankan@gmail.com>
2021-09-23 17:02:11 +0200

    Improved documentation

--- pgmpy/inference/ApproxInference.py
+++ pgmpy/inference/ApproxInference.py
@@ -40,6 +40,22 @@ class ApproxInference(object):
         )
 
     def get_distribution(self, samples, variables, joint=True):
+        """
+        Computes distribution of `variables` from given data `samples`.
+
+        Parameters
+        ----------
+        samples: pandas.DataFrame
+            A dataframe of samples generated from the model.
+
+        variables: list (array-like)
+            A list of variables whose distribution needs to be computed.
+
+        joint: boolean
+            If joint=True, computes the joint distribution over `variables`.
+            Else, returns a dict with marginal distribution of each variable in
+            `variables`.
+        """
         if joint == True:
             return self._get_factor_from_df(
                 samples.groupby(variables).size() / samples.shape[0]
@@ -94,7 +110,13 @@ class ApproxInference(object):
         >>> from pgmpy.inference import ApproxInference
         >>> model = get_example_model("alarm")
         >>> infer = ApproxInference(model)
-        >>> infer.query(variable=["HISTORY"])
+        >>> infer.query(variables=["HISTORY"])
+        <DiscreteFactor representing phi(HISTORY:2) at 0x7f92d9f5b910>
+        >>> infer.query(variables=["HISTORY", "CVP"], joint=True)
+        <DiscreteFactor representing phi(HISTORY:2, CVP:3) at 0x7f92d9f77610>
+        >>> infer.query(variables=["HISTORY", "CVP"], joint=False)
+        {'HISTORY': <DiscreteFactor representing phi(HISTORY:2) at 0x7f92dc61eb50>,
+         'CVP': <DiscreteFactor representing phi(CVP:3) at 0x7f92d915ec40>}
         """
         # Step 1: Generate samples for the query
         samples = self.model.simulate(
