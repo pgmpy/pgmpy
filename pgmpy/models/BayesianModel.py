eaec44be4f5210f647261ed1f6501a618e6039a1
Vitor Bruno de Oliveira Barth <vitor.barth@gmail.com>
2021-10-30 16:50:51 -0400

    Fixed typo in BayesianModel deprecation warning (#1474)

--- pgmpy/models/BayesianModel.py
+++ pgmpy/models/BayesianModel.py
@@ -6,7 +6,7 @@ from pgmpy.models import BayesianNetwork
 class BayesianModel(BayesianNetwork):
     def __init__(self, ebunch=None, latents=set()):
         warnings.warn(
-            "BayesianModel has been renamed to BayesianNetwork. Please use BayesianNewtork class, BayesianModel will be removed in future.",
+            "BayesianModel has been renamed to BayesianNetwork. Please use BayesianNetwork class, BayesianModel will be removed in future.",
             FutureWarning,
         )
         super(BayesianModel, self).__init__(ebunch=ebunch, latents=latents)
