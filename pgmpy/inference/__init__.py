ba675c5dd08944b9f976ee9f8f3f002182bbd63d
tristan <tristan.trebaol@gmail.com>
2024-03-01 19:31:36 +0000

    Fix circular import error

--- pgmpy/inference/__init__.py
+++ pgmpy/inference/__init__.py
@@ -1,13 +1,10 @@
-from .ApproxInference import ApproxInference
 from .base import Inference
 from .CausalInference import CausalInference
-
+from .ExactInference import BeliefPropagation
+from .ExactInference import VariableElimination
+from .ExactInference import BeliefPropagationWithMessageParsing
+from .ApproxInference import ApproxInference
 from .dbn_inference import DBNInference
-from .ExactInference import (
-    BeliefPropagation,
-    BeliefPropagationWithMessageParsing,
-    VariableElimination,
-)
 from .mplp import Mplp
 
 __all__ = [
