05f92de2f0b46152133804acb81797cc854f88d2
B3CODER <bhavya312003@gmail.com>
2024-10-14 16:12:18 +0530

    Updated get_cpds method: ensured consistent return type and improved error handling

--- pgmpy/models/BayesianNetwork.py
+++ pgmpy/models/BayesianNetwork.py
@@ -279,18 +279,35 @@ class BayesianNetwork(DAG):
 
         Returns
         -------
-        A list of TabularCPDs: list
+        cpd : TabularCPD object or list of TabularCPD objects
+            If 'node' is specified, returns the 'TabularCPD' object corresponding to the node.
+            If 'node' is not specified, returns a list of all 'TabularCPD' objects added to the model.
+
+        Raises
+        ------
+        ValueError
+            If the specified node is not present in the model.
 
         Examples
         --------
-        >>> from pgmpy.models import BayesianNetwork
-        >>> from pgmpy.factors.discrete import TabularCPD
-        >>> student = BayesianNetwork([('diff', 'grade'), ('intel', 'grade')])
-        >>> cpd = TabularCPD('grade', 2, [[0.1, 0.9, 0.2, 0.7],
-        ...                               [0.9, 0.1, 0.8, 0.3]],
-        ...                  ['intel', 'diff'], [2, 2])
-        >>> student.add_cpds(cpd)
-        >>> student.get_cpds()
+        >>> from pgmpy.utils import get_example_model
+        >>> model = get_example_model('asia')
+        >>> cpds=model.get_cpds()
+        >>> cpds
+        [<TabularCPD representing P(asia:2) at 0x7dbbd9bdbb80>,
+        <TabularCPD representing P(bronc:2 | smoke:2) at 0x7dbbd9bda3e0>,
+        <TabularCPD representing P(dysp:2 | bronc:2, either:2) at 0x7dbbd9bd8550>,
+        <TabularCPD representing P(either:2 | lung:2, tub:2) at 0x7dbbd9bda800>,
+        <TabularCPD representing P(lung:2 | smoke:2) at 0x7dbbd9bd89d0>,
+        <TabularCPD representing P(smoke:2) at 0x7dbbd9bd8f70>,
+        <TabularCPD representing P(tub:2 | asia:2) at 0x7dbbd9bda860>,
+        <TabularCPD representing P(xray:2 | either:2) at 0x7dbbd9bd9a80>]
+        >>>  cpds = model.get_cpds('bronc')     # Returns the CPD of node
+        >>>  cpds
+        <TabularCPD representing P(bronc:2 | smoke:2) at 0x7dbbd9bda3e0>
+        >>> cpds = model.get_cpds('tbu')
+        >>> cpds
+        ValueError: Node 'tub' not present in the model
         """
         if node is not None:
             if node not in self.nodes():
@@ -299,6 +316,7 @@ class BayesianNetwork(DAG):
                 for cpd in self.cpds:
                     if cpd.variable == node:
                         return cpd
+                raise ValueError(f"Node '{node}' does not have an associated CPD.")
         else:
             return self.cpds
 
