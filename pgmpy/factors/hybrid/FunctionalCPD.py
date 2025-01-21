0993cd54110844880521de0abd5318251e57ab51
Nuna7 <zonunmawiazadeng7@gmail.com>
2025-01-21 13:09:18 +0530

    Attempt to resolve Conflicts.

--- pgmpy/factors/hybrid/FunctionalCPD.py
+++ pgmpy/factors/hybrid/FunctionalCPD.py
@@ -8,8 +8,9 @@ class FunctionalCPD(BaseFactor):
     """
     Defines a Functional CPD.
 
-    Functional CPD can represent any arbitrary conditional probability distribution
-    where the distribution to represented is defined by function (input as parameter) which calls pyro.sample function.
+    Functional CPD can represent any arbitrary conditional probability
+    distribution where the distribution to represented is defined by function
+    (input as parameter) which calls pyro.sample function.
     """
 
     def __init__(self, variable, fn, parents=[]):
@@ -100,20 +101,20 @@ class FunctionalCPD(BaseFactor):
 
             sampled_values = []
             for _, row in parent_sample.iterrows():
-                sampled_values.append(self.fn(row)().detach().numpy())
+                sampled_values.append(self.fn(row))
 
             sampled_values = np.array(sampled_values)
         else:
             sampled_values = []
             for _ in range(n_samples):
-                sampled_values.append(self.fn(parent_sample)().detach().numpy())
+                sampled_values.append(self.fn(parent_sample))
 
             sampled_values = np.array(sampled_values)
 
         return sampled_values
 
     def __str__(self):
-        fn_name = "lambda fn." if self.fn.__name__ == "<lambda>" else self.fn.__name__
+        fn_name = "lambda fun." if self.fn.__name__ == "<lambda>" else self.fn.__name__
         if self.parents:
             return f"P({self.variable} | {', '.join(self.parents)}) = {fn_name}"
         return f"P({self.variable}) = {fn_name}"
