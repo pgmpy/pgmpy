3bc99e580210bf61880469bf8596ffd16c134924
Jihye Sofia Seo <jihyeseo@gmail.com>
2025-01-28 18:56:00 +0100

    Move lavaan parser to utils

--- pgmpy/models/SEM.py
+++ pgmpy/models/SEM.py
@@ -7,6 +7,7 @@ from networkx.algorithms.dag import descendants
 
 from pgmpy.base import DAG
 from pgmpy.global_vars import logger
+from pgmpy.utils.parser import parse_lavaan
 
 
 class SEMGraph(DAG):
@@ -1023,88 +1024,9 @@ class SEM(SEMGraph):
         """
         if syntax.lower() == "lavaan":
             # Create a SEMGraph model using the lavaan str.
-            # Step 0: Check if pyparsing is installed
-            try:
-                from pyparsing import (
-                    OneOrMore,
-                    Optional,
-                    Suppress,
-                    Word,
-                    alphanums,
-                    nums,
-                )
-            except ImportError as e:
-                raise ImportError(
-                    e.msg
-                    + ". pyparsing is required for using lavaan syntax. Please install using: pip install pyparsing"
-                )
+            ebunch, latents, err_corr, err_var = parse_lavaan(kwargs["lavaan_str"])
 
-            # Step 1: Define the grammar for each type of string.
-            var = Word(alphanums)
-            reg_gram = (
-                OneOrMore(
-                    var.setResultsName("predictors", listAllMatches=True)
-                    + Optional(Suppress("+"))
-                )
-                + "~"
-                + OneOrMore(
-                    var.setResultsName("covariates", listAllMatches=True)
-                    + Optional(Suppress("+"))
-                )
-            )
-            intercept_gram = var("inter_var") + "~" + Word("1")
-            covar_gram = (
-                var("covar_var1")
-                + "~~"
-                + OneOrMore(
-                    var.setResultsName("covar_var2", listAllMatches=True)
-                    + Optional(Suppress("+"))
-                )
-            )
-            latent_gram = (
-                var("latent")
-                + "=~"
-                + OneOrMore(
-                    var.setResultsName("obs", listAllMatches=True)
-                    + Optional(Suppress("+"))
-                )
-            )
-
-            # Step 2: Preprocess string to lines
-            lines = kwargs["lavaan_str"]
-
-            # Step 3: Initialize arguments and fill them by parsing each line.
-            ebunch = []
-            latents = []
-            err_corr = []
-            err_var = []
-            for line in lines:
-                line = line.strip()
-                if (line != "") and (not line.startswith("#")):
-                    if intercept_gram.matches(line):
-                        continue
-                    elif reg_gram.matches(line):
-                        results = reg_gram.parseString(line, parseAll=True)
-                        for pred in results["predictors"]:
-                            ebunch.extend(
-                                [
-                                    (covariate, pred)
-                                    for covariate in results["covariates"]
-                                ]
-                            )
-                    elif covar_gram.matches(line):
-                        results = covar_gram.parseString(line, parseAll=True)
-                        for var in results["covar_var2"]:
-                            err_corr.append((results["covar_var1"], var))
-
-                    elif latent_gram.matches(line):
-                        results = latent_gram.parseString(line, parseAll=True)
-                        latents.append(results["latent"])
-                        ebunch.extend(
-                            [(results["latent"], obs) for obs in results["obs"]]
-                        )
-
-            # Step 4: Call the parent __init__ with the arguments
+            # Call the parent __init__ with the arguments
             super(SEM, self).__init__(ebunch=ebunch, latents=latents, err_corr=err_corr)
 
         elif syntax.lower() == "graph":
---
+++ pgmpy/utils/parser.py
@@ -0,0 +1,83 @@
+
+def parse_lavaan(lines):
+    # Step 0: Check if pyparsing is installed
+    try:
+        from pyparsing import (
+            OneOrMore,
+            Optional,
+            Suppress,
+            Word,
+            alphanums,
+            nums,
+        )
+    except ImportError as e:
+        raise ImportError(
+            e.msg
+            + ". pyparsing is required for using lavaan syntax. Please install using: pip install pyparsing"
+        )
+
+    # Step 1: Define the grammar for each type of string.
+    var = Word(alphanums)
+    reg_gram = (
+            OneOrMore(
+                var.setResultsName("predictors", listAllMatches=True)
+                + Optional(Suppress("+"))
+            )
+            + "~"
+            + OneOrMore(
+        var.setResultsName("covariates", listAllMatches=True)
+        + Optional(Suppress("+"))
+    )
+    )
+    intercept_gram = var("inter_var") + "~" + Word("1")
+    covar_gram = (
+            var("covar_var1")
+            + "~~"
+            + OneOrMore(
+        var.setResultsName("covar_var2", listAllMatches=True)
+        + Optional(Suppress("+"))
+    )
+    )
+    latent_gram = (
+            var("latent")
+            + "=~"
+            + OneOrMore(
+        var.setResultsName("obs", listAllMatches=True)
+        + Optional(Suppress("+"))
+    )
+    )
+
+    # Step 2: Preprocess string to lines
+
+
+    # Step 3: Initialize arguments and fill them by parsing each line.
+    ebunch = []
+    latents = []
+    err_corr = []
+    err_var = []
+    for line in lines:
+        line = line.strip()
+        if (line != "") and (not line.startswith("#")):
+            if intercept_gram.matches(line):
+                continue
+            elif reg_gram.matches(line):
+                results = reg_gram.parseString(line, parseAll=True)
+                for pred in results["predictors"]:
+                    ebunch.extend(
+                        [
+                            (covariate, pred)
+                            for covariate in results["covariates"]
+                        ]
+                    )
+            elif covar_gram.matches(line):
+                results = covar_gram.parseString(line, parseAll=True)
+                for var in results["covar_var2"]:
+                    err_corr.append((results["covar_var1"], var))
+
+            elif latent_gram.matches(line):
+                results = latent_gram.parseString(line, parseAll=True)
+                latents.append(results["latent"])
+                ebunch.extend(
+                    [(results["latent"], obs) for obs in results["obs"]]
+                )
+    return ebunch, latents, err_corr, err_var
