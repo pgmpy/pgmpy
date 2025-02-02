fc6ba2b9545fb55769e786285293cf93e2735306
Jihye Sofia Seo <jihyeseo@gmail.com>
2025-02-02 18:38:44 +0100

    Fix forward_sample for cat_model with partial_samples

--- pgmpy/sampling/Sampling.py
+++ pgmpy/sampling/Sampling.py
@@ -129,7 +129,10 @@ class BayesianModelSampling(BayesianModelInference):
                     weights = cpd.values
                     sampled[node] = sample_discrete(states, weights, size)
 
-        samples_df = _return_samples(sampled, self.state_names_map)
+        # The data in partial_samples are already in state_name. The other columns are in indices.
+        samples_df = _return_samples(
+            sampled, self.state_names_map, partial_samples.columns
+        )
         if not include_latents and any(
             latent in samples_df.columns for latent in self.model.latents
         ):
