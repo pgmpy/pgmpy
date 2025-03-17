aee8d940d17479b02438cd7e7770b58d18d9c487
Ankur Ankan <ankurankan@gmail.com>
2025-03-17 07:37:04 +0100

    Fixes

--- pgmpy/factors/continuous/__init__.py
+++ pgmpy/factors/continuous/__init__.py
@@ -1,9 +1,5 @@
-from .discretize import BaseDiscretizer, RoundingDiscretizer, UnbiasedDiscretizer
 from .LinearGaussianCPD import LinearGaussianCPD
 
 __all__ = [
     "LinearGaussianCPD",
-    "BaseDiscretizer",
-    "RoundingDiscretizer",
-    "UnbiasedDiscretizer",
 ]
