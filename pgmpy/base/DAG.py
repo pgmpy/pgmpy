f08b894fdfd428384faf98b137ff73a1fee4136b
Ankur Ankan <ankurankan@gmail.com>
2025-03-30 17:59:50 +0200

    Adds DAG.copy method and fixes PDAG.copy mehtod

--- pgmpy/base/DAG.py
+++ pgmpy/base/DAG.py
@@ -1253,6 +1253,11 @@ class DAG(nx.DiGraph):
                 return (node, type(node))
         return False
 
+    def copy(self):
+        dag = DAG(ebunch=self.edges(), latents=self.latents)
+        dag.add_nodes_from(self.nodes())
+        return dag
+
 
 class PDAG(nx.DiGraph):
     """
@@ -1316,11 +1321,13 @@ class PDAG(nx.DiGraph):
         Copy of PDAG: pgmpy.dag.PDAG
             Returns a copy of self.
         """
-        return PDAG(
+        pdag = PDAG(
             directed_ebunch=list(self.directed_edges.copy()),
             undirected_ebunch=list(self.undirected_edges.copy()),
             latents=self.latents,
         )
+        pdag.add_nodes_from(self.nodes())
+        return pdag
 
     def to_dag(self):
         """
