c647d706f4670ebcc601efc6265e139451ded323
Ankur Ankan <ankurankan@gmail.com>
2025-03-31 18:12:44 +0200

    Updates

--- pgmpy/models/BayesianNetwork.py
+++ pgmpy/models/BayesianNetwork.py
@@ -1,8 +1,5 @@
-from pgmpy.global_vars import logger
-
-
 class BayesianNetwork(object):
     def __init__(self, ebunch=None, latents=set(), lavaan_str=None, dagitty_str=None):
-        logger.error(
-            "BayesianNetwork class is deprecated. Please use DiscreteBayesianNetwork class instead."
+        raise ImportError(
+            "BayesianNetwork has been deprecated. Please use DiscreteBayesianNetwork instead."
         )
