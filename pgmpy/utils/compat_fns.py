249ff22fb7c0efcc5e3caecc86598be99076b63a
arocapedro <57690041+arocapedro@users.noreply.github.com>
2025-03-31 13:22:14 +0200

    Finish PR

--- pgmpy/utils/compat_fns.py
+++ pgmpy/utils/compat_fns.py
@@ -20,7 +20,9 @@ def copy(arr):
             return np.array(arr)
         elif isinstance(arr, (int, float)):
             return deepcopy(arr)
-        raise Exception(f"Invalid backend ({config.get_backend()}) for data type {type(arr)}")
+        raise Exception(
+            f"Invalid backend ({config.get_backend()}) for data type {type(arr)}"
+        )
     else:
         return arr.clone().detach()
 
