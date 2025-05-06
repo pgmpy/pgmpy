f9858ff32cdaa67aef22880f07942f671191d195
30215210 <susmita.chakrabarty@ucalgary.ca>
2025-05-06 20:06:26 +0600

    Fix: Allow add_factors to accept factor scopes that are subsets of cluster nodes in ClusterGraph

--- pgmpy/models/ClusterGraph.py
+++ pgmpy/models/ClusterGraph.py
@@ -154,10 +154,15 @@ class ClusterGraph(UndirectedGraph):
         """
         for factor in factors:
             factor_scope = set(factor.scope())
-            nodes = [set(node) for node in self.nodes()]
-            if factor_scope not in nodes:
+            #nodes = [set(node) for node in self.nodes()]
+            found_cluster = any(factor_scope.issubset(set(node)) for node in self.nodes())
+            #if factor_scope not in nodes:
+                #raise ValueError(
+                    #"Factors defined on clusters of variable not" "present in model"
+                #)
+            if not found_cluster:
                 raise ValueError(
-                    "Factors defined on clusters of variable not" "present in model"
+                    f"Factor scope {factor.scope()} is not a subset of any cluster in the model."
                 )
 
             self.factors.append(factor)
