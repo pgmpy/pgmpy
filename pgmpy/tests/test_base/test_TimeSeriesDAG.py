6656192a73ebdd4507afe50dea57a3acf3885c12
Spinachboul <jain.mridul.20@gmail.com>
2025-05-13 15:24:37 +0530

    add test for TimeSeriesDAG

--- pgmpy/tests/test_base/test_TimeSeriesDAG.py
+++ pgmpy/tests/test_base/test_TimeSeriesDAG.py
@@ -58,32 +58,25 @@ class TestTimeSeriesDAGCreation(unittest.TestCase):
         self.graph.add_node(("c", 2))
         self.assertListEqual(sorted(self.graph.nodes()), [("a", 0), ("b", 1), ("c", 2)])
 
-    # def test_add_temporal_edge(self):
-    #     self.graph = TimeSeriesDAG(num_time_slices=2)
-    #     self.graph.add_temporal_edge("a", "b", time_slice=0)
-    #     self.assertIn((("a", 0), ("b", 0)), self.graph.edges())
+    def test_plot_summary_graph(self):
+        import matplotlib.pyplot as plt
 
-    # self.graph.add_temporal_edge("a", "b", from_time_slice=0, to_time_slice=1)
-    # self.assertIn((("a", 0), ("b", 1)), self.graph.edges())
+        # Create a simple TimeSeriesDAG
+        self.graph = TimeSeriesDAG(
+            ebunch=[(("a", 0), ("b", 1)), (("b", 1), ("c", 2))],
+            num_time_slices=3,
+        )
 
-    # def test_temporal_markov_blanket(self):
-    #     self.graph = TimeSeriesDAG(
-    #         [
-    #             (("a", 0), ("b", 0)),
-    #             (("b", 0), ("c", 0)),
-    #             (("a", 1), ("b", 1)),
-    #             (("b", 1), ("c", 1)),
-    #             (("a", 0), ("a", 1)),
-    #             (("b", 0), ("b", 1)),
-    #             (("c", 0), ("c", 1)),
-    #         ],
-    #         num_time_slices=2,
-    #     )
-    #     mb_a0 = self.graph.get_temporal_markov_blanket(("a", 0))
-    #     self.assertEqual(set(mb_a0), {("b", 0), ("a", 1)})
+        # Plot the summary graph
+        fig, ax = self.graph.plot_summary_graph()
 
-    #     mb_b1 = self.graph.get_temporal_markov_blanket(("b", 1))
-    #     self.assertEqual(set(mb_b1), {("a", 1), ("c", 1), ("b", 0)})
+        # Check that the plot was created
+        self.assertIsInstance(fig, plt.Figure)
+        self.assertIsInstance(ax, plt.Axes)
+        graph = TimeSeriesDAG(
+            ebunch=[(("a", 0), ("b", 1), ("c", 2), ("d", 1))],
+            num_time_slices=3,
+        )
 
     def tearDown(self):
         del self.graph
