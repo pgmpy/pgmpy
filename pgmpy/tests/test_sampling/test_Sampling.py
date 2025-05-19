bf5ac33bd0a17d98bdd8ecfd0e8b99accc265cbb
30215210 <susmita.chakrabarty@ucalgary.ca>
2025-05-19 19:35:43 +0600

    Fix Attribute Error in the  tests/test_sampling/test_Sampling.py

--- pgmpy/tests/test_sampling/test_Sampling.py
+++ pgmpy/tests/test_sampling/test_Sampling.py
@@ -147,9 +147,9 @@ class TestBayesianModelSampling(unittest.TestCase):
 
     def test_sample_raises_typeerror_for_invalid_variables(self):
         with self.assertRaises(TypeError):
-            self.sampling_inference.sample(variables="A")  # Should be list, not str
+            self.sampling_inference.forward_sample(variables="A")  # Should be list, not str
         with self.assertRaises(TypeError):
-            self.sampling_inference.sample(variables=5)  # Should be iterable, not int
+            self.sampling_inference.forward_sample(variables=5)  # Should be iterable, not int
 
     def test_pre_compute_reduce_maps(self):
         base_infer = BayesianModelInference(self.bayesian_model)
