f56bfa13025eb05343c5a1c05d6832b192f242cb
nimish <nimishpurohitn@gmail.com>
2025-05-30 20:55:02 +0530

    Add PC workflow to docstring

--- pgmpy/estimators/PC.py
+++ pgmpy/estimators/PC.py
@@ -8,7 +8,6 @@ from tqdm.auto import tqdm
 
 from pgmpy import config
 from pgmpy.base import PDAG, UndirectedGraph
-from pgmpy.estimators import StructureEstimator
 from pgmpy.estimators import ExpertKnowledge, StructureEstimator
 from pgmpy.estimators.CITests import get_ci_test
 from pgmpy.global_vars import logger
@@ -21,6 +20,45 @@ class PC(StructureEstimator):
     set using statistical independence tests and estimates a DAG pattern
     that satisfies the identified dependencies. The DAG pattern can then be
     completed to a faithful DAG, if possible.
+
+    When used with expert knowledge, the following flowchart can help you figure
+    out the expected results based on different choices of parameters and the
+    structure learned from the data.
+
+                                        ┌──────────────────┐    No      ┌─────────────┐
+                                        │ Expert Knowledge ├──────────► │  Normal PC  │
+                                        │    specified?    │            │    run      │
+                                        └────────┬─────────┘            └─────────────┘
+                                                 │
+                                            Yes  │
+                                                 │
+                                                 ▼
+                                        ┌──────────────────┐
+                                        │  Enforce expert  │
+                                        │    knowledge?    │
+                                        └────────┬─────────┘
+                                                 │
+                                                 │
+                                Yes              │                No
+                       ┌─────────────────────────┴───────────────────────┐
+                       │                                                 │
+                       ▼                                                 ▼
+        ┌──────────────────────────────┐                     ┌─────────────────────────┐
+        │                              │                     │                         │
+        │ 1) Forbidden edges are       │                     │ Conflicts with learned  │
+        │    removed from the skeleton │                     │   structure (opposite   │
+        │                              │                     │  edge orientations)?    │
+        │ 2) Required edges will be    │                     │                         │
+        │    present in the final      │                     └───────────┬─────────────┘
+        │    model (but direction is   │                                 │
+        │    not guaranteed)           │                ┌────────────────┴──────────────────┐
+        │                              │            Yes │                                   │ No
+        └──────────────────────────────┘                │                                   │
+                                                        ▼                                   ▼
+                                            ┌───────────────────┐                ┌──────────────────┐
+                                            │ Conflicting edges │                │ Expert knowledge │
+                                            │    are ignored    │                │  applied fully   │
+                                            └───────────────────┘                └──────────────────┘
 
     Parameters
     ----------
