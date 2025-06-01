6ad95ed6737123428bae9274e9c934cf4bd4f738
hillhack <2jyotihill@gmail.com>
2025-06-01 08:54:59 +0530

    1. shifted pyparsing to core 2. remove e.msg as ImportError as no attribute msg ,instead use f{e}.

--- pgmpy/readwrite/XMLBIF.py
+++ pgmpy/readwrite/XMLBIF.py
@@ -10,8 +10,7 @@ try:
     import pyparsing as pp
 except ImportError as e:
     raise ImportError(
-        e.msg
-        + ". pyparsing is required for using read/write methods. Please install using: pip install pyparsing."
+    f"{e} . pyparsing is required for using read/write methods. Please install using: pip install pyparsing."
     ) from None
 
 from pgmpy.factors.discrete import State, TabularCPD
