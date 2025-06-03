ee3fcf5a9fca6ec0dfc622adae862ab5f06ac5a9
Ankur Ankan <ankurankan@gmail.com>
2025-06-03 16:07:15 +0200

    Minor change

--- pgmpy/base/DAG.py
+++ pgmpy/base/DAG.py
@@ -1071,7 +1071,7 @@ class DAG(nx.DiGraph):
         else:
             nodes = list(nodes)
 
-        if set(nodes) not in self.nodes():
+        if set(nodes) not in set(self.nodes()):
             raise ValueError(
                 f"Nodes not found in the model: {set(nodes) - set(self.nodes())}"
             )
