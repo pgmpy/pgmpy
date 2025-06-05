6d0ae22ba003af22f3749dfcbf88dc932e4f5c39
arocapedro <57690041+arocapedro@users.noreply.github.com>
2025-06-05 14:37:02 +0200

    fix #2187

--- pgmpy/readwrite/NET.py
+++ pgmpy/readwrite/NET.py
@@ -140,14 +140,20 @@ class NETWriter(object):
         -------
         string: CPT format of .net files
         """
+        import json
+
         cpt = self.tables[var_name]
         cpt_array = np.moveaxis(compat_fns.to_numpy(cpt, decimals=4), 0, -1)
-        cpt_string = str(cpt_array)
+        # using json dumps to avoid truncated output when serializing to str
+        cpt_string = json.dumps(cpt_array.tolist())
         net_cpt_string = (
             cpt_string.replace("[", "(")
             .replace("]", ")")
             .replace(". ", ".0 ")
             .replace(".)", ".0)")
+            .replace(
+                ",", ""
+            )  # remove commas with spaces to match parseString functionality
         )
         # Genie does not read potentials such as 1. therefore last line adds .0 to those
         return net_cpt_string
