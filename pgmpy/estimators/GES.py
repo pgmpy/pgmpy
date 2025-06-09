1003fffd1a1567e31f5b31edbcf4522c8f31eeb5
hillhack <2jyotihill@gmail.com>
2025-06-09 10:15:46 +0530

    Added hints to GES.py

--- pgmpy/estimators/GES.py
+++ pgmpy/estimators/GES.py
@@ -1,7 +1,9 @@
 from itertools import combinations
+from typing import List, Tuple, Optional, Union
 
 import networkx as nx
 import numpy as np
+import pandas as pd
 
 from pgmpy import config
 from pgmpy.base import DAG
@@ -53,12 +55,14 @@ class GES(StructureEstimator):
       Journal of machine learning research 3.Nov (2002): 507-554.
     """
 
-    def __init__(self, data, use_cache=True, **kwargs):
+    def __init__(self, data: pd.DataFrame, use_cache: bool = True, **kwargs):
         self.use_cache = use_cache
 
         super(GES, self).__init__(data=data, **kwargs)
 
-    def _legal_edge_additions(self, current_model, expert_knowledge):
+    def _legal_edge_additions(
+        self, current_model: DAG, expert_knowledge: ExpertKnowledge
+    ) -> List[Tuple[str, str]]:
         """
         Returns a list of all edges that can be added to the graph such that it remains a DAG.
         """
@@ -75,7 +79,9 @@ class GES(StructureEstimator):
                     edges.append((v, u))
         return edges
 
-    def _legal_edge_removals(self, current_model, expert_knowledge):
+    def _legal_edge_removals(
+        self, current_model: DAG, expert_knowledge: ExpertKnowledge
+    ) -> List[Tuple[str, str]]:
         """
         Returns a list of all edges that can be removed from the graph such that it remains a DAG.
         """
@@ -85,7 +91,9 @@ class GES(StructureEstimator):
                 edges.append((u, v))
         return edges
 
-    def _legal_edge_flips(self, current_model, expert_knowledge):
+    def _legal_edge_flips(
+        self, current_model: DAG, expert_knowledge: ExpertKnowledge
+    ) -> List[Tuple[str, str]]:
         """
         Returns a list of all the edges in the `current_model` that can be flipped such that the model
         remains a DAG.
@@ -106,11 +114,11 @@ class GES(StructureEstimator):
 
     def estimate(
         self,
-        scoring_method=None,
-        expert_knowledge=None,
-        min_improvement=1e-6,
-        debug=False,
-    ):
+        scoring_method: Optional[Union[str, StructureScore]] = None,
+        expert_knowledge: Optional[ExpertKnowledge] = None,
+        min_improvement: float = 1e-6,
+        debug: bool = False,
+    ) -> DAG:
         """
         Estimates the DAG from the data.
 
