683115523ceef4fff499210d6b60ea496864839c
andoriyaprashant <prashantandoriya@gmail.com>
2025-06-14 16:20:41 +0530

    Fix: Restore Compatibility for MarkovNetwork and Prevent Crashes in MPLP Inference

--- pgmpy/models/MarkovNetwork.py
+++ pgmpy/models/MarkovNetwork.py
@@ -1,5 +1,8 @@
-class MarkovNetwork(object):
-    def __init__(self, ebunch=None, latents=[]):
-        raise ImportError(
-            "MarkovNetwork has been deprecated. Please use DiscreteMarkovNetwork instead."
+from pgmpy.models import DiscreteMarkovNetwork
+
+class MarkovNetwork(DiscreteMarkovNetwork):
+    def __init__(self, *args, **kwargs):
+        print(
+            "WARNING: MarkovNetwork has been deprecated. Please use DiscreteMarkovNetwork instead."
         )
+        super().__init__(*args, **kwargs)
