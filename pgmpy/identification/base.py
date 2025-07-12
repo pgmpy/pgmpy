a94ba3ee7485793f6d5bc6ad8555e8b43beaba96
Ankur Ankan <ankurankan@gmail.com>
2025-07-12 08:14:06 -0700

    Restructure methods

--- pgmpy/identification/base.py
+++ pgmpy/identification/base.py
@@ -9,20 +9,34 @@ class BaseIdentification:
     >>> class SimpleId(BaseIdentification):
     ...     "A simple identification method when all variable are observed"
     ...
-    ...     def __call__(self):
+    ...     def _identify(self, causal_graph):
     ...         outcome_parents = set(
-    ...             self.causal_graph.predecessors(self.causal_graph.exposure)
-    ...         ) - {self.exposure}
-    ...         self.causal_graph.add_roles("adjustment", outcome_parents)
+    ...             causal_graph.predecessors(causal_graph.exposure)
+    ...         ) - {causal_graph.exposure}
+    ...         causal_graph.add_roles("adjustment", outcome_parents)
     ...         return outcome_parents, True
     ...
     """
 
-    def __init__(self, causal_graph):
-        # TODO: Once CausalGraph class is defined,
-        #       add a check here for the type of causal_graph.
-        self.causal_graph = causal_graph.copy()
-        self.observed_variables = frozenset(self.causal_graph.nodes()).difference(
-            self.causal_graph.latents
-        )
-        self.latent_variables = self.causal_graph.latents
+    def identify(self, causal_graph):
+        """Method to run the identification method.
+
+        The method accepts a causal graph and returns a causal
+        graph with the same structure and with defined variable
+        roles based on the identification method.
+
+        Returns
+        -------
+        causal_graph: Instance of CausalGraph
+            The causal graph with variable roles assigned.
+
+        success: bool
+            Whether the causal graph with given exposure and outcome is
+            identified.
+        """
+        causal_graph = causal_graph.copy()
+        return self._identify(causal_graph)
+
+    def __call__(self, causal_graph):
+        """Alias for the `identify` method"""
+        return self.identify(causal_graph)
