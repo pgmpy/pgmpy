a08f0c2f5faa14c23d085bcad0cb0bff653a4d1a
Spinachboul <jain.mridul.20@gmail.com>
2025-08-04 11:23:37 +0530

    FCI and base ancestral class

---
+++ pgmpy/base/ancestral/base.py
@@ -0,0 +1,301 @@
+import networkx as nx
+from pgmpy.base import DAG
+
+# Note: This is a conceptual implementation. In a real-world pgmpy project,
+# you would likely create a more general 'MixedGraph' base class.
+# This implementation assumes DAG is a suitable base for inheriting NetworkX-based
+# functionality.
+
+
+class AncestralGraph(DAG):
+    """
+    Base class for all ancestral graphical models.
+
+    This class extends the pgmpy.base.DAG to handle mixed graphs with
+    directed, bidirected, and partially directed edges. Edges are represented
+    with an attribute that specifies the marks at each endpoint.
+
+    The valid marks are:
+    - 'tail': A tail mark (e.g., in A -> B, A has a tail)
+    - 'arrowhead': An arrowhead mark (e.g., in A -> B, B has an arrowhead)
+    - 'circle': A circle mark (e.g., in A o-> B, A has a circle)
+    """
+
+    def __init__(self, ebunch=None):
+        """
+        Initializes an empty AncestralGraph.
+
+        Parameters
+        ----------
+        ebunch : list-like, optional
+            A container of edges. Each edge must be a tuple of the form
+            (u, v, u_mark, v_mark), where u and v are the nodes, and u_mark
+            and v_mark are the marks at the endpoints of u and v respectively.
+            Example: [('A', 'B', 'tail', 'arrowhead')] for A -> B.
+        """
+        super().__init__()
+        # Use NetworkX for the underlying graph structure.
+        self.graph = nx.DiGraph()
+
+        if ebunch:
+            self.add_edges_from(ebunch)
+
+    def add_edge(self, u, v, u_mark, v_mark):
+        """
+        Adds an edge between nodes u and v with specified endpoint marks.
+
+        Parameters
+        ----------
+        u : hashable
+            The starting node of the edge.
+        v : hashable
+            The ending node of the edge.
+        u_mark : str
+            The mark at the 'u' endpoint. Must be one of 'tail', 'arrowhead', 'circle'.
+        v_mark : str
+            The mark at the 'v' endpoint. Must be one of 'tail', 'arrowhead', 'circle'.
+        """
+        # A single 'add_edge' call might not be enough for bidirected or undirected edges,
+        # as NetworkX's DiGraph treats u->v and v->u as separate edges.
+        # We store the mark at the 'v' end for the u->v edge.
+        # For symmetric edges (bidirected, undirected, etc.), we need to add two edges.
+        if u_mark not in {"tail", "arrowhead", "circle"} or v_mark not in {
+            "tail",
+            "arrowhead",
+            "circle",
+        }:
+            raise ValueError("Marks must be one of 'tail', 'arrowhead', or 'circle'.")
+
+        self.add_node(u)
+        self.add_node(v)
+
+        # Store the mark for the edge u -> v
+        self.graph.add_edge(u, v, mark=v_mark)
+
+        # For symmetric edges, we must also add the reverse edge
+        # and store its mark.
+        if self._is_symmetric_edge(u_mark, v_mark):
+            self.graph.add_edge(v, u, mark=u_mark)
+        elif self.graph.has_edge(v, u):
+            # If a reverse edge already exists, and the new edge is not symmetric,
+            # this is an invalid operation for this implementation's logic.
+            # A more robust implementation might handle mixed edge types between nodes.
+            pass
+
+    def add_edges_from(self, ebunch):
+        """
+        Adds multiple edges from an iterable container.
+
+        Parameters
+        ----------
+        ebunch : list-like
+            A container of edges, where each edge is a tuple of the form
+            (u, v, u_mark, v_mark).
+        """
+        for u, v, u_mark, v_mark in ebunch:
+            self.add_edge(u, v, u_mark, v_mark)
+
+    def _is_symmetric_edge(self, u_mark, v_mark):
+        """
+        Helper function to determine if an edge type is symmetric.
+        This includes bidirected (<->) and undirected (o-o) edges.
+        """
+        return (u_mark == "arrowhead" and v_mark == "arrowhead") or (
+            u_mark == "circle" and v_mark == "circle"
+        )
+
+    def is_directed(self, u, v):
+        """
+        Checks if there is a directed edge from u to v (u -> v).
+        """
+        if self.graph.has_edge(u, v) and self.graph.has_edge(v, u):
+            return (
+                self.graph.get_edge_data(u, v).get("mark") == "arrowhead"
+                and self.graph.get_edge_data(v, u).get("mark") == "tail"
+            )
+        else:
+            return (
+                self.graph.has_edge(u, v)
+                and self.graph.get_edge_data(u, v).get("mark") == "arrowhead"
+            )
+
+    def is_bidirected(self, u, v):
+        """
+        Checks if there is a bidirected edge between u and v (u <-> v).
+        This is a symmetric relationship.
+        """
+        return (
+            self.graph.has_edge(u, v)
+            and self.graph.has_edge(v, u)
+            and self.graph.get_edge_data(u, v).get("mark") == "arrowhead"
+            and self.graph.get_edge_data(v, u).get("mark") == "arrowhead"
+        )
+
+    def has_arrowhead_at(self, u, v):
+        """
+        Checks if the edge between u and v has an arrowhead at the v endpoint.
+        """
+        return (
+            self.graph.has_edge(u, v)
+            and self.graph.get_edge_data(u, v).get("mark") == "arrowhead"
+        )
+
+    def has_circle_at(self, u, v):
+        """
+        Checks if the edge between u and v has a circle at the v endpoint.
+        """
+        return (
+            self.graph.has_edge(u, v)
+            and self.graph.get_edge_data(u, v).get("mark") == "circle"
+        )
+
+    def has_tail_at(self, u, v):
+        """
+        Checks if the edge between u and v has a tail at the v endpoint.
+        """
+        return (
+            self.graph.has_edge(u, v)
+            and self.graph.get_edge_data(u, v).get("mark") == "tail"
+        )
+
+    ## get all the relationahips between the nodes ##
+
+    # Only for directed edegs for now
+    def get_parents(self, node):
+        """
+        Returns a set of all parents of the given node.
+
+        A node 'p' is a parent of 'c' if there is a directed edge p -> c.
+
+        Parameters
+        ----------
+        node : hashable
+            The node for which to find parents.
+
+        Returns
+        -------
+        set
+            A set of all parents of `node`.
+        """
+        parents = set()
+        for neighbor in self.graph.predecessors(node):
+            if (
+                self.graph.get_edge_data(neighbor, node).get("mark") == "arrowhead"
+                and self.graph.get_edge_data(node, neighbor).get("mark") == "tail"
+            ):
+                parents.add(neighbor)
+        return parents
+
+    def get_children(self, node):
+        """
+        Returns a set of all children of the given node.
+
+        A node 'c' is a child of 'p' if there is a directed edge p -> c.
+
+        Parameters
+        ----------
+        node : hashable
+            The node for which to find children.
+
+        Returns
+        -------
+        set
+            A set of all children of `node`.
+        """
+        children = set()
+        for neighbor in self.graph.adj[node]:
+            if (
+                self.graph.get_edge_data(node, neighbor).get("mark") == "arrowhead"
+                and self.graph.get_edge_data(neighbor, node).get("mark") == "tail"
+            ):
+                children.add(neighbor)
+        return children
+
+    # A method only for bidirected edges
+    def get_spouses(self, node):
+        """
+        Returns a set of all spouses of the given node.
+
+        A node 's' is a spouse of 'u' if there is a bidirected edge u <-> s.
+
+        Parameters
+        ----------
+        node : hashable
+            The node for which to find spouses.
+
+        Returns
+        -------
+        set
+            A set of all spouses of `node`.
+        """
+        spouses = set()
+        for neighbor in self.graph.adj[node]:
+            if (
+                self.graph.get_edge_data(node, neighbor).get("mark") == "arrowhead"
+                and self.graph.get_edge_data(neighbor, node).get("mark") == "arrowhead"
+            ):
+                spouses.add(neighbor)
+        return spouses
+
+    # Implementation logic only works for directed edges for now
+    def get_ancestors(self, node):
+        """
+        Returns a set of all ancestors of the given node.
+
+        An ancestor of a node 'n' is any node from which there is a directed
+        path ending at 'n'.
+
+        Parameters
+        ----------
+        node : hashable
+            The node for which to find ancestors.
+
+        Returns
+        -------
+        set
+            A set of all ancestors of `node`.
+        """
+
+        ancestors = set()
+        queue = list(self.get_parents(node))
+        visited = set(queue)
+
+        while queue:
+            current_node = queue.pop(0)
+            ancestors.add(current_node)
+            for parent in self.get_parents(current_node):
+                if parent not in visited:
+                    visited.add(parent)
+                    queue.append(parent)
+        return ancestors
+
+    # Implementation logic only works for directed edges for now
+    def get_descendants(self, node):
+        """
+        Returns a set of all descendants of the given node.
+
+        A descendant of a node 'n' is any node to which there is a directed
+        path starting from 'n'.
+
+        Parameters
+        ----------
+        node : hashable
+            The node for which to find descendants.
+
+        Returns
+        -------
+        set
+            A set of all descendants of `node`.
+        """
+        descendants = set()
+        queue = list(self.get_children(node))
+        visited = set(queue)
+
+        while queue:
+            current_node = queue.pop(0)
+            descendants.add(current_node)
+            for child in self.get_children(current_node):
+                if child not in visited:
+                    visited.add(child)
+                    queue.append(child)
+        return descendants
--- pgmpy/estimators/FCI.py
+++ pgmpy/estimators/FCI.py
@@ -1,23 +1,238 @@
-"""
-Implementation of the Fast Causal Inference (FCI) algorithm."""
+#!/usr/bin/env python
 
 from itertools import chain, combinations, permutations
+from tqdm import tqdm
+
+import networkx as nx
+import pandas as pd
 from typing import (
     Callable,
+    Collection,
     Dict,
+    Hashable,
+    FrozenSet,
     Iterable,
     List,
     Optional,
     Set,
-    Collection,
     Tuple,
     Union,
 )
 
-import networkx as nx
-import pandas as pd
-
-
+from pgmpy import config
 from pgmpy.estimators.PC import PC
+from pgmpy.estimators import StructureEstimator
+from pgmpy.base import DAG, PDAG, UndirectedGraph
+from pgmpy.independencies import Independencies
+from pgmpy.global_vars import logger
 from pgmpy.estimators.CITests import get_callable_ci_test
-from pgmpy.base import ADMG, DAG, UndirectedGraph
+from pgmpy.base.ancestral.base import AncestralGraph
+
+
+class FCI(StructureEstimator, AncestralGraph):
+    """
+    An implementation of the FCI (Fast Causal Inference) algorithm.
+
+    The FCI algorithm is a causal discovery method that extends the PC algorithm
+    to handle latent confounding variables and selection bias.
+    It outputs a Partial Ancestral Graph (PAG) which uses a combination of
+    circles(o), tails(--), and arrowheads(>) to represent the
+    causal relationships.
+    """
+
+    def __init__(
+        self,
+        edge_types: Tuple[str, str],
+        data: Optional[pd.DataFrame] = None,
+        independencies: Optional[Independencies] = None,
+        **kwargs,
+    ) -> None:
+        super(PC, self).__init__(data, independencies, edge_types, **kwargs)
+
+    def build_skeleton(
+        self,
+        variant: str = "stable",
+        ci_test: Union[str, Callable, None] = None,
+        significance_level: float = 0.01,
+        max_cond_vars: int = 5,
+        show_progress: bool = True,
+        **kwargs,
+    ) -> Tuple[UndirectedGraph, Dict[Tuple[str, str], Set[str]]]:
+
+        # Initilize initial values and structures
+        lim_neighbors = 0
+        separating_sets = dict()
+        ci_test = get_callable_ci_test(ci_test, full=True, data=None)
+
+        if show_progress and config.SHOW_PROGRESS:
+            pbar = tqdm(total=max_cond_vars)
+            pbar.set_description("Working for n conditional variables")
+
+        # Step1: Initialize the uindirected graph
+        graph = nx.complete_graph(n=self.variables, create_using=nx.Graph)
+        # currently skipping the temporal ordering due to lack of expert knowledge
+
+        # Exit Condition: (AS PER THE PC algorithm)
+        while not all(
+            [len(list(graph.neighbors(var))) < lim_neighbors for var in self.variables]
+        ):
+            # Step2: iterate over all the edges and find a conditioning set
+            # of size `lim_neighbors` which makes u and v independent
+
+            # Currently including only a single variant
+            if variant == "orig":
+                for u, v in graph.edges():
+                    for separating_set in PC._get_potential_sepsets(
+                        u, v, graph, lim_neighbors=lim_neighbors
+                    ):
+                        # If a conditioning set exists, remove the edge,
+                        # store the separating set and move on to finding
+                        # the conditioning set for the next edge.
+                        if ci_test(
+                            u,
+                            v,
+                            separating_set,
+                            data=self.data,
+                            Independencies=self.independencies,
+                            significance_level=significance_level,
+                            **kwargs,
+                        ):
+                            separating_set[frozenset((u, v))] = separating_set
+                            graph.remove_edge(u, v)
+                            break
+
+            # step3 : After iterating over al the edges, expand the search space,
+            # by increasing the size of the conditioning set
+            if lim_neighbors >= max_cond_vars:
+                logger.info("Reached maximum number of allowed variables. Exiting")
+                break
+            lim_neighbors += 1
+
+            if show_progress and config.SHOW_PROGRESS:
+                pbar.update(1)
+                pbar.set_description(
+                    f"Working for n conditional variables: {lim_neighbors}"
+                )
+
+        if show_progress and config.SHOW_PROGRESS:
+            pbar.update(max_cond_vars - lim_neighbors)
+            pbar.close()
+        return graph, separating_sets
+
+    @staticmethod
+    def _get_potential_sepsets(
+        u: Hashable,
+        v: Hashable,
+        temporal_ordering: Dict[Hashable, int],
+        graph: UndirectedGraph,
+        lim_neighbors: int,
+    ) -> Collection[Tuple]:
+        """
+        Return the temporally consistent superset of separating set of u, v.
+        """
+        separating_set_u = set(graph.neighbors(u))
+        separating_set_v = set(graph.neighbors(v))
+        separating_set_u.discard(v)
+        separating_set_v.discard(u)
+
+        if temporal_ordering != dict():
+            max_order = min(temporal_ordering[u], temporal_ordering[u])
+            for neigh in list(separating_set_u):
+                if temporal_ordering[neigh] > max_order:
+                    separating_set_u.discard(neigh)
+
+            for neigh in list(separating_set_v):
+                if temporal_ordering[neigh] > max_order:
+                    separating_set_v.discard(neigh)
+
+        return chain(
+            combinations(separating_set_u, lim_neighbors),
+            combinations(separating_set_v, lim_neighbors),
+        )
+
+    def _orient_colliders(self):
+        """
+        Orients unshielded triples as colliders (Rule R0).
+
+        An unshielded triple (a, c, b) is oriented as a collider a*->c<-*b if
+        [cite_start]and only if c is not in the separating set of a and b[cite: 40].
+        """
+        for a in self.graph.nodes():
+            for b in self.graph.neighbors(a):
+                for c in self.graph.neighbors(b):
+                    if a == c or b == a:
+                        continue
+                    # Check for unshielded triple
+                    if not self.graph.has_edge(a, c):
+                        if (
+                            c not in self._separating_sets[frozenset((a, b))]
+                            and a not in self._separating_sets[frozenset((c, b))]
+                        ):
+                            # a*->b<-*c
+                            self.graph.add_edge(a, b, mark="arrowhead")
+                            self.graph.add_edge(c, b, mark="arrowhead")
+
+    def _orient_edges(self):
+        """
+        Applies the 10 orientation rules (R1-R10) iteratively.
+        """
+        changed = True
+        while changed:
+            changed = False
+
+            # [cite_start]Rule 1: If a*->b o-*c, and a and c are not adjacent, then orient a*->b->c[cite: 41].
+            for b in self.graph.nodes():
+                for a in self.graph.predecessors(b):
+                    for c in self.graph.successors(b):
+                        if a == c:
+                            continue
+                        if (
+                            self.graph.has_edge(a, b)
+                            and self.graph.get_edge_data(a, b).get("mark")
+                            == "arrowhead"
+                            and self.graph.has_edge(b, c)
+                            and self.graph.get_edge_data(b, c).get("mark") == "circle"
+                            and not self.graph.has_edge(a, c)
+                            and not self.graph.has_edge(c, a)
+                        ):
+                            # Orient b o-* c as b -> c
+                            self.graph.add_edge(b, c, mark="arrowhead")
+                            self.graph.add_edge(c, b, mark="tail")
+                            changed = True
+
+            # [cite_start]Rule 2: If α->β*->γ, α*-o γ, and α and γ are adjacent, then orient α*-o γ as α*->γ[cite: 42].
+            for a in self.graph.nodes():
+                for c in self.graph.neighbors(a):
+                    if (
+                        self.has_circle_at(c, a)
+                        and self.has_tail_at(a, c)
+                        and not self.graph.has_edge(a, c)
+                    ):  # α*-o γ
+                        for b in self.graph.nodes():
+                            if b == a or b == c:
+                                continue
+                            if self.is_directed(a, b) and self.has_arrowhead_at(
+                                b, c
+                            ):  # α->β*->γ
+                                self.add_edge(a, c, "tail", "arrowhead")
+                                changed = True
+
+            # [cite_start]Rule 3: If α*->β<-*γ, α and γ are not adjacent, and θ*-o β, then orient θ*-o β as θ*->β[cite: 43].
+            for b in self.graph.nodes():
+                for a, c in permutations(self.graph.predecessors(b), 2):
+                    if (
+                        not self.graph.has_edge(a, c)
+                        and self.graph.get_edge_data(a, b).get("mark") == "arrowhead"
+                        and self.graph.get_edge_data(c, b).get("mark") == "arrowhead"
+                    ):
+                        for d in self.graph.predecessors(b):
+                            if (
+                                d != a
+                                and d != c
+                                and self.graph.get_edge_data(d, b).get("mark")
+                                == "circle"
+                            ):
+                                # Orient d*-o b as d*->b
+                                self.graph.add_edge(d, b, mark="arrowhead")
+                                changed = True
+        return self.graph
