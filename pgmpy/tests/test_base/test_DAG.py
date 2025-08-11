ca988d3df7e17ce7c24fbac6e5a3f606fefa3ff4
daehyun99 <daehyun99@kakao.com>
2025-08-11 12:10:18 +0900

    Test: Verify role copying in PDAG class (#2279)
    - feat: Add `pdag_role` fixture to `TestPDAG.setUp`.
    - test: Add a test case to `TestPDAG.test_copy` to ensure that node roles are correctly carried over when a `PDAG` object is copied.
    - feat: This test verifies that:
        1. A new `PDAG` instance is created with assigned roles.
        2. After calling the `copy()` method, the new object has the same node roles as the original.
        3. The `get_role()` and `get_roles()` methods on the copied object return the expected values.

--- pgmpy/tests/test_base/test_DAG.py
+++ pgmpy/tests/test_base/test_DAG.py
@@ -916,6 +916,11 @@ class TestPDAG(unittest.TestCase):
             undirected_ebunch=[("B", "A"), ("B", "D")],
             latents=["A", "D"],
         )
+        self.pdag_role = PDAG(
+            directed_ebunch=[("A", "C"), ("D", "C")],
+            undirected_ebunch=[("B", "A"), ("B", "D")],
+            roles={"exposure": "A", "adjustment": "D", "outcome": "C"},
+        )
 
     def test_init_normal(self):
         # Mix directed and undirected
@@ -1121,6 +1126,25 @@ class TestPDAG(unittest.TestCase):
         self.assertEqual(pdag_copy.directed_edges, set([("A", "C"), ("D", "C")]))
         self.assertEqual(pdag_copy.undirected_edges, set([("B", "A"), ("B", "D")]))
         self.assertEqual(pdag_copy.latents, set(["A", "D"]))
+
+        pdag_copy = self.pdag_role.copy()
+        expected_edges = {
+            ("A", "C"),
+            ("D", "C"),
+            ("A", "B"),
+            ("B", "A"),
+            ("B", "D"),
+            ("D", "B"),
+        }
+        self.assertEqual(set(pdag_copy.edges()), expected_edges)
+        self.assertEqual(set(pdag_copy.nodes()), {"A", "B", "C", "D"})
+        self.assertEqual(pdag_copy.directed_edges, set([("A", "C"), ("D", "C")]))
+        self.assertEqual(pdag_copy.undirected_edges, set([("B", "A"), ("B", "D")]))
+        self.assertEqual(pdag_copy.latents, set())
+        self.assertEqual(pdag_copy.get_role("exposure"), ["A"])
+        self.assertEqual(pdag_copy.get_role("adjustment"), ["D"])
+        self.assertEqual(pdag_copy.get_role("outcome"), ["C"])
+        self.assertEqual(sorted(pdag_copy.get_roles()), sorted(['adjustment', 'exposure', 'outcome']))
 
     def test_pdag_to_dag(self):
         # PDAG no: 1  Possibility of creating a v-structure
