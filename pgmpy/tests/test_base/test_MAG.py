9e50c892c45f1dd505c9b239be2fffe5dc23037e
Spinachboul <jain.mridul.20@gmail.com>
2025-09-04 15:25:16 +0530

    simplify test names

--- pgmpy/tests/test_base/test_MAG.py
+++ pgmpy/tests/test_base/test_MAG.py
@@ -20,13 +20,13 @@ def mag():
     return MAG(ebunch=edges, latents=latents)
 
 
-def test_init_empty():
+def test_empty_init():
     empty = MAG()
     assert len(empty.nodes()) == 0
     assert empty.latents == set()
 
 
-def test_roles_assignment_and_equality():
+def test_roles_and_equality():
     e = [("X", "Z", "-", ">"), ("Y", "Z", "-", ">")]
     roles = {"exposure": "X", "outcome": "Z", "adjustment": {"Y"}}
     m1 = MAG(ebunch=e, latents={"L"}, roles=roles)
@@ -49,32 +49,29 @@ def test_roles_assignment_and_equality():
     assert m1 != m5
 
 
-def test_is_collider_on_fixture(mag):
+def test_is_collider_fixture(mag):
     assert mag._is_collider("A", "D", "B")
-
     assert not mag._is_collider("A", "B", "C")
 
 
-def test_is_collider_simple():
+def test_is_collider_basic():
     m = MAG(ebunch=[("X", "Z", "-", ">"), ("Y", "Z", "-", ">")])
     assert m._is_collider("X", "Z", "Y")
 
 
-def test_has_inducing_path_on_fixture_is_false_for_pairs_without_valid_colliders(mag):
+def test_no_inducing_path_fixture(mag):
     assert not mag.has_inducing_path("A", "B", {"L"})
     assert not mag.has_inducing_path("C", "D", {"L"})
     assert not mag.has_inducing_path("A", "D", {"L"})
 
-    assert not mag.has_inducing_path("X", "A", {"L"})
 
-
-def test_has_inducing_path_latent_collider_true_when_in_W():
+def test_inducing_path_with_latent():
     m = MAG(ebunch=[("X", "L", "-", ">"), ("Y", "L", "-", ">")], latents={"L"})
     assert m.has_inducing_path("X", "Y", {"L"})
     assert not m.has_inducing_path("X", "Y", set())
 
 
-def test_is_visible_edge_on_fixture(mag):
+def test_visible_edges_fixture(mag):
     assert mag.is_visible_edge("A", "B")
     assert mag.is_visible_edge("A", "D")
     assert mag.is_visible_edge("B", "C")
@@ -83,16 +80,15 @@ def test_is_visible_edge_on_fixture(mag):
     assert not mag.is_visible_edge("Z", "Z")
 
 
-def test_is_visible_edge_invisible_due_to_latent_inducing_path():
+def test_invisible_edge_due_to_latent():
     m = MAG(
         ebunch=[("X", "Y", "-", ">"), ("X", "L", "-", ">"), ("Y", "L", "-", ">")],
         latents={"L"},
     )
-
     assert not m.is_visible_edge("X", "Y")
 
 
-def test_lower_manipulation_on_fixture_removes_L_and_preserves_others(mag):
+def test_lower_manipulation_fixture(mag):
     new_mag = mag.lower_manipulation({"L"})
     assert "L" not in new_mag.nodes()
     assert new_mag.has_edge("A", "B")
@@ -103,7 +99,7 @@ def test_lower_manipulation_on_fixture_removes_L_and_preserves_others(mag):
     assert new_mag.has_edge("B", "C")
 
 
-def test_lower_manipulation_adds_bidirected_when_node_is_collider():
+def test_lower_manipulation_adds_bidirected():
     m = MAG(ebunch=[("X", "L", "-", ">"), ("Y", "L", "-", ">")], latents={"L"})
     new_m = m.lower_manipulation({"L"})
     assert "L" not in new_m.nodes()
@@ -113,7 +109,7 @@ def test_lower_manipulation_adds_bidirected_when_node_is_collider():
     assert marks["X"] == ">" and marks["Y"] == ">"
 
 
-def test_upper_manipulation_removes_outgoing_edges_only(mag):
+def test_upper_manipulation_fixture(mag):
     new_mag = mag.upper_manipulation({"A"})
     assert not new_mag.has_edge("A", "D")
     assert new_mag.has_edge("A", "B")
@@ -125,7 +121,7 @@ def test_upper_manipulation_removes_outgoing_edges_only(mag):
     assert new_mag_b.has_edge("B", "D")
 
 
-def test_manipulations_do_not_modify_original(mag):
+def test_manipulations_keep_original(mag):
     original_nodes = set(mag.nodes())
     original_edges = {
         (u, v, frozenset(data["marks"].items())) for u, v, data in mag.edges(data=True)
@@ -141,7 +137,7 @@ def test_manipulations_do_not_modify_original(mag):
     assert now_edges == original_edges
 
 
-def test_graph_properties_fixture(mag):
+def test_graph_properties(mag):
     assert len(mag.nodes()) == 5
     assert mag.latents == {"L"}
     assert mag.has_edge("A", "B")
