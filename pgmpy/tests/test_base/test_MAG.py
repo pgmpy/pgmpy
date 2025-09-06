cfc187dbc440d1f34886931166ade68107914766
Spinachboul <jain.mridul.20@gmail.com>
2025-09-07 05:18:32 +0530

    write MAG tests in class

--- pgmpy/tests/test_base/test_MAG.py
+++ pgmpy/tests/test_base/test_MAG.py
@@ -17,88 +17,84 @@ def mag():
     return MAG(ebunch=edges, latents={"L"}, roles=roles)
 
 
-def test_empty_init():
-    empty = MAG()
-    assert len(empty.nodes()) == 0
-    assert empty.latents == set()
+class TestMAG:
+    def test_empty_init(self):
+        empty = MAG()
+        assert len(empty.nodes()) == 0
+        assert empty.latents == set()
 
+    def test_roles_and_equality(self):
+        e = [("X", "Z", "-", ">"), ("Y", "Z", "-", ">")]
+        roles = {"exposure": "X", "outcome": "Z", "adjustment": {"Y"}}
+        m1 = MAG(ebunch=e, latents={"L"}, roles=roles)
+        m2 = MAG(
+            ebunch=e,
+            latents={"L"},
+            roles={"exposure": "X", "outcome": "Z", "adjustment": {"Y"}},
+        )
+        assert m1 == m2
 
-def test_roles_and_equality():
-    e = [("X", "Z", "-", ">"), ("Y", "Z", "-", ">")]
-    roles = {"exposure": "X", "outcome": "Z", "adjustment": {"Y"}}
-    m1 = MAG(ebunch=e, latents={"L"}, roles=roles)
-    m2 = MAG(
-        ebunch=e,
-        latents={"L"},
-        roles={"exposure": "X", "outcome": "Z", "adjustment": {"Y"}},
-    )
-    assert m1 == m2
+        m3 = MAG(ebunch=e, latents={"L"}, roles={"exposure": "X"})
+        assert m1 != m3
 
-    m3 = MAG(ebunch=e, latents={"L"}, roles={"exposure": "X"})
-    assert m1 != m3
+        m4 = MAG(
+            ebunch=[("X", "Z", ">", ">"), ("Y", "Z", "-", ">")],
+            latents={"L"},
+            roles=roles,
+        )
+        assert m1 != m4
 
-    m4 = MAG(
-        ebunch=[("X", "Z", ">", ">"), ("Y", "Z", "-", ">")], latents={"L"}, roles=roles
-    )
-    assert m1 != m4
+        m5 = MAG(ebunch=e, latents={"L", "U"}, roles=roles)
+        assert m1 != m5
 
-    m5 = MAG(ebunch=e, latents={"L", "U"}, roles=roles)
-    assert m1 != m5
+    def test_is_collider(self, mag):
+        assert mag._is_collider("A", "D", "B")
+        assert not mag._is_collider("A", "B", "C")
 
+    def test_has_inducing_path(self, mag):
+        assert not mag.has_inducing_path("A", "B", {"L"})
+        assert not mag.has_inducing_path("C", "D", {"L"})
+        assert not mag.has_inducing_path("A", "D", {"L"})
 
-def test_is_collider(mag):
-    assert mag._is_collider("A", "D", "B")
-    assert not mag._is_collider("A", "B", "C")
+        edges = [
+            ("X", "L", "-", ">"),
+            ("Y", "L", "-", ">"),
+        ]
+        new_mag = MAG(ebunch=edges, latents={"L"})
+        assert new_mag.has_inducing_path("X", "Y", {"L"})
 
+    def test_is_visible_edge(self, mag):
+        assert not mag.is_visible_edge("A", "D")
+        assert not mag.is_visible_edge("B", "C")
+        assert not mag.is_visible_edge("A", "B")
+        assert not mag.is_visible_edge("C", "D")
+        assert not mag.is_visible_edge("A", "C")
+        assert not mag.is_visible_edge("B", "D")
 
-def test_has_inducing_path(mag):
-    assert not mag.has_inducing_path("A", "B", {"L"})
-    assert not mag.has_inducing_path("C", "D", {"L"})
-    assert not mag.has_inducing_path("A", "D", {"L"})
+    # def test_lower_manipulation(mag):
 
-    edges = [
-        ("X", "L", "-", ">"),
-        ("Y", "L", "-", ">"),
-    ]
-    new_mag = MAG(ebunch=edges, latents={"L"})
-    assert new_mag.has_inducing_path("X", "Y", {"L"})
+    def test_upper_manipulation(self, mag):
+        new_mag = mag.upper_manipulation({"A"})
+        assert not new_mag.has_edge("A", "D")
+        assert mag.has_edge("A", "B")
+        assert mag.has_edge("A", "C")
+        assert mag.has_edge("A", "D")
 
+        mag_copy = mag.copy()
+        mag_copy.upper_manipulation({"A"}, inplace=True)
+        assert not mag_copy.has_edge("A", "D")
+        assert mag_copy.has_edge("A", "B")
+        assert mag_copy.has_edge("A", "C")
 
-def test_is_visible_edge(mag):
-    assert not mag.is_visible_edge("A", "D")
-    assert not mag.is_visible_edge("B", "C")
-    assert not mag.is_visible_edge("A", "B")
-    assert not mag.is_visible_edge("C", "D")
-    assert not mag.is_visible_edge("A", "C")
-    assert not mag.is_visible_edge("B", "D")
+        new_mag_b = mag.upper_manipulation({"B"})
+        assert not new_mag_b.has_edge("B", "C")
+        assert new_mag_b.has_edge("A", "B")
+        assert new_mag_b.has_edge("B", "D")
 
-
-# def test_lower_manipulation(mag):
-
-
-def test_upper_manipulation(mag):
-    new_mag = mag.upper_manipulation({"A"})
-    assert not new_mag.has_edge("A", "D")
-    assert mag.has_edge("A", "B")
-    assert mag.has_edge("A", "C")
-    assert mag.has_edge("A", "D")
-
-    mag_copy = mag.copy()
-    mag_copy.upper_manipulation({"A"}, inplace=True)
-    assert not mag_copy.has_edge("A", "D")
-    assert mag_copy.has_edge("A", "B")
-    assert mag_copy.has_edge("A", "C")
-
-    new_mag_b = mag.upper_manipulation({"B"})
-    assert not new_mag_b.has_edge("B", "C")
-    assert new_mag_b.has_edge("A", "B")
-    assert new_mag_b.has_edge("B", "D")
-
-
-def test_graph_properties(mag):
-    assert len(mag.nodes()) == 4
-    assert mag.latents == {"L"}
-    assert mag.has_edge("A", "B")
-    assert mag.has_edge("C", "D")
-    assert mag.has_edge("A", "D")
-    assert mag.has_edge("B", "C")
+    def test_graph_properties(self, mag):
+        assert len(mag.nodes()) == 4
+        assert mag.latents == {"L"}
+        assert mag.has_edge("A", "B")
+        assert mag.has_edge("C", "D")
+        assert mag.has_edge("A", "D")
+        assert mag.has_edge("B", "C")
