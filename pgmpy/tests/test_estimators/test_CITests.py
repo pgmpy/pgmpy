32a7a50be47c910112829fd35a6d2e9c04bdbc4f
Ankur Ankan <ankurankan@gmail.com>
2025-11-10 17:13:23 +0100

    Updates the use of random number generator

--- pgmpy/tests/test_estimators/test_CITests.py
+++ pgmpy/tests/test_estimators/test_CITests.py
@@ -18,28 +18,28 @@ from pgmpy.estimators.CITests import (
 from pgmpy.factors.continuous import LinearGaussianCPD
 from pgmpy.models import LinearGaussianBayesianNetwork
 
-np.random.seed(42)
-
 
 class TestPearsonr(unittest.TestCase):
     def setUp(self):
+        rng = np.random.default_rng(seed=42)
+
         self.df_ind = pd.DataFrame(np.random.randn(10000, 3), columns=["X", "Y", "Z"])
 
-        Z = np.random.randn(10000)
-        X = 3 * Z + np.random.normal(loc=0, scale=0.1, size=10000)
-        Y = 2 * Z + np.random.normal(loc=0, scale=0.1, size=10000)
+        Z = rng.normal(10000)
+        X = 3 * Z + rng.normal(loc=0, scale=0.1, size=10000)
+        Y = 2 * Z + rng.normal(loc=0, scale=0.1, size=10000)
 
         self.df_cind = pd.DataFrame({"X": X, "Y": Y, "Z": Z})
 
-        Z1 = np.random.randn(10000)
-        Z2 = np.random.randn(10000)
-        X = 3 * Z1 + 2 * Z2 + np.random.normal(loc=0, scale=0.1, size=10000)
-        Y = 2 * Z1 + 3 * Z2 + np.random.normal(loc=0, scale=0.1, size=10000)
+        Z1 = rng.normal(10000)
+        Z2 = rng.normal(10000)
+        X = 3 * Z1 + 2 * Z2 + rng.normal(loc=0, scale=0.1, size=10000)
+        Y = 2 * Z1 + 3 * Z2 + rng.normal(loc=0, scale=0.1, size=10000)
         self.df_cind_mul = pd.DataFrame({"X": X, "Y": Y, "Z1": Z1, "Z2": Z2})
 
-        X = np.random.rand(10000)
-        Y = np.random.rand(10000)
-        Z = 2 * X + 2 * Y + np.random.normal(loc=0, scale=0.1, size=10000)
+        X = rng.normal(10000)
+        Y = rng.normal(10000)
+        Z = 2 * X + 2 * Y + rng.normal(loc=0, scale=0.1, size=10000)
         self.df_vstruct = pd.DataFrame({"X": X, "Y": Y, "Z": Z})
 
     def test_pearsonr(self):
