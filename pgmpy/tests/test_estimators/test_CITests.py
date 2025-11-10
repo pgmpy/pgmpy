e94d6d3d8831c519a3e3d669c488df74f8fe54f7
Ankur Ankan <ankurankan@gmail.com>
2025-11-10 17:20:20 +0100

    Fixes test for py 3.13

--- pgmpy/tests/test_estimators/test_CITests.py
+++ pgmpy/tests/test_estimators/test_CITests.py
@@ -23,7 +23,7 @@ class TestPearsonr(unittest.TestCase):
     def setUp(self):
         rng = np.random.default_rng(seed=42)
 
-        self.df_ind = pd.DataFrame(np.random.randn(10000, 3), columns=["X", "Y", "Z"])
+        self.df_ind = pd.DataFrame(np.random.randn(1000, 3), columns=["X", "Y", "Z"])
 
         Z = rng.normal(10000)
         X = 3 * Z + rng.normal(loc=0, scale=0.1, size=10000)
