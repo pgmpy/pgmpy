b0491e1e1c58deb0fd18a1825563cc1024d561e8
Saba Siddique <sabasiddique001@gmail.com>
2025-12-17 14:55:10 +0500

    Add Student Performance dataset
    - Refs #2459

--- pgmpy/datasets/__init__.py
+++ pgmpy/datasets/__init__.py
@@ -12,6 +12,7 @@ from ._sachs import (  # noqa: F401
     SachsMixed,
 )
 from ._boston_housing import BostonHousing  # noqa: F401
+from ._student_performance import StudentPerformance  # noqa: F401
 
 __all__ = [
     "_BaseDataset",
