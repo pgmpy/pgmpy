6f1d08591d587755380d7dbf8bae3c799482ca72
mavrick-1 <mavrickrishi@gmail.com>
2025-12-17 14:39:48 +0530

    Remove apple-watch-fitbit, auto-mpg, blue-driver from dropouts-dry-bean branch
    This branch now contains only:
    - dropouts dataset (8 variables, 159 samples, continuous data with custom covariance parser)
    - dry_bean dataset (17 variables, 13611 samples, mixed data with expert knowledge)
    
    The other 3 datasets are in the separate 'add-missing-datasets' branch.

--- pgmpy/datasets/__init__.py
+++ pgmpy/datasets/__init__.py
@@ -12,9 +12,6 @@ from ._sachs import (  # noqa: F401
     SachsMixed,
 )
 from ._boston_housing import BostonHousing  # noqa: F401
-from ._apple_watch_fitbit import AppleWatchFitbit  # noqa: F401
-from ._auto_mpg import AutoMpg  # noqa: F401
-from ._blue_driver import BlueDriver  # noqa: F401
 from ._dropouts import Dropouts  # noqa: F401
 from ._dry_bean import DryBean  # noqa: F401
 
