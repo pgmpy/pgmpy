22aecc1448e19048674b0a8c5cacfe05259533bf
mavrick-1 <mavrickrishi@gmail.com>
2025-12-19 01:45:58 +0530

    Add uscrime dataset

---
+++ pgmpy/datasets/_uscrime.py
@@ -0,0 +1,30 @@
+from pgmpy.datasets import register_dataset_class
+from pgmpy.datasets._base import _BaseDataset
+
+
+@register_dataset_class
+class USCrime(_BaseDataset):
+    name = "uscrime"
+
+    tags = {
+        "n_variables": 14,
+        "n_samples": 47,
+        "has_ground_truth": False,
+        "has_expert_knowledge": False,
+        "has_missing_data": False,
+        "is_simulated": False,
+        "is_interventional": False,
+        "is_discrete": False,
+        "is_continuous": True,
+        "is_mixed": False,
+        "is_ordinal": False,
+    }
+
+    base_url = (
+        "https://raw.githubusercontent.com/pgmpy/example-causal-datasets/refs/heads/main/"
+        "real/uscrime/"
+    )
+
+    data_url = base_url + "data/uscrime.continuous.txt"
+    ground_truth_url = None
+    expert_knowledge_url = None
