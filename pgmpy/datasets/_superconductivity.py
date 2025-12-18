4501f613fe716ca1115d96e5a31554dcab8b6ae4
Raunak Dev <raunak.r.dev@gmail.com>
2025-12-18 13:26:32 +0530

    registers supercoductivity dataset

---
+++ pgmpy/datasets/_superconductivity.py
@@ -0,0 +1,25 @@
+from pgmpy.datasets import register_dataset_class
+from pgmpy.datasets._base import _BaseDataset
+
+
+@register_dataset_class
+class superconductivity(_BaseDataset):
+    name = "superconductivity_continuous"
+    tags = {
+        "n_variables": 81,
+        "n_samples": 22163,
+        "has_ground_truth": False,
+        "has_expert_knowledge": True,
+        "is_simulated": False,
+        "is_interventional": False,
+        "is_discrete": False,
+        "is_continuous": True,
+        "is_mixed": False,
+        "is_ordinal": False,
+    }
+
+    base_url = "https://raw.githubusercontent.com/pgmpy/example-causal-datasets/refs/heads/main/real/superconductivity/"
+    data_url = base_url + "data/superconductivity.continuous.txt"
+
+    ground_truth_url = None
+    expert_knowledge_url = base_url + "ground.truth/superconductivity.knowledge.txt"
