28aa95c8d04d6d9f689d283d1828c3696e7f9233
Ankur Ankan <ankurankan@gmail.com>
2025-12-18 14:08:25 +0100

    Add 'has_missing_data' key to dataset metadata

--- pgmpy/datasets/_credit_approval.py
+++ pgmpy/datasets/_credit_approval.py
@@ -11,6 +11,7 @@ class CreditApproval(_BaseDataset):
         "n_samples": 690,
         "has_ground_truth": False,
         "has_expert_knowledge": False,
+        "has_missing_data": False,
         "is_simulated": False,
         "is_interventional": False,
         "is_discrete": False,
