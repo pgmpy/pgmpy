831d746f3d49bdfe35526512ac02fda042244121
Ankur Ankan <ankurankan@gmail.com>
2025-12-18 14:32:11 +0100

    Update _residential_building.py

--- pgmpy/datasets/_residential_building.py
+++ pgmpy/datasets/_residential_building.py
@@ -11,6 +11,7 @@ class ResidentialBuilding(_BaseDataset):
         "n_samples": 372,
         "has_ground_truth": False,
         "has_expert_knowledge": False,
+        "has_missing_data": False,
         "is_simulated": False,
         "is_interventional": False,
         "is_discrete": False,
