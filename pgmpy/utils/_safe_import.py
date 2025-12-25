0cc1673bf2655a6f5043a0c2638305e5d0178b7c
Franz Király <fkiraly@gcos.ai>
2025-12-25 20:39:01 +0100

    Update _safe_import.py

--- pgmpy/utils/_safe_import.py
+++ pgmpy/utils/_safe_import.py
@@ -54,7 +54,7 @@ def _safe_import(import_path, pkg_name=None):
 
     Examples
     --------
-    >>> from sktime.utils.dependencies._safe_import import _safe_import
+    >>> from pgmpy.utils._safe_import import _safe_import
 
     >>> # Import a top-level module
     >>> torch = _safe_import("torch")
