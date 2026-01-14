b192a35a3759163acf06f1c0ac79474e53c08155
Spinachboul <jain.mridul.20@gmail.com>
2026-01-14 21:18:40 +0530

    change orient_colliders and work on FCI tests

--- pgmpy/tests/test_estimators/test_FCI.py
+++ pgmpy/tests/test_estimators/test_FCI.py
@@ -1,8 +1,54 @@
-import numpy as np
-import pandas as pd
-
 from pgmpy.estimators.FCI import FCI
 from pgmpy.independencies.Independencies import Independencies
+from pgmpy.sampling.Sampling import BayesianModelSampling
+from pgmpy.utils import get_example_model
+
+
+def fake_ci_t(X, Y, Z=[], **kwargs):
+    """
+    A mock CI testing function which gives False for every condition
+    except for the following:
+        1. B \u27c2 C
+        2. B \u27c2 D
+        3. C \u27c2 D
+        4. A \u27c2 B | C
+        5. A \u27c2 C | B
+    """
+    Z = list(Z)
+    if X == "B":
+        if Y == "C" or Y == "D":
+            return True
+        elif Y == "A" and Z == ["C"]:
+            return True
+    elif X == "C" and Y == "D" and Z == []:
+        return True
+    elif X == "D" and Y == "C" and Z == []:
+        return True
+    elif Y == "B":
+        if X == "C" or X == "D":
+            return True
+        elif X == "A" and Z == ["C"]:
+            return True
+    elif X == "A" and Y == "C" and Z == ["B"]:
+        return True
+    elif X == "C" and Y == "A" and Z == ["B"]:
+        return True
+    return False
+
+
+# Function to define different real world datasets
+
+
+def pc_alarm_data():
+    alarm_model = get_example_model("alarm")
+    data = BayesianModelSampling(alarm_model).forward_sample(size=int(1e4), seed=42)
+    return data
+
+
+def pc_asia_data():
+    model = get_example_model("asia")
+    data = model.simulate(n_samples=5000, show_progress=False)
+    return data
 
 
 def skeleton(pag):
@@ -10,89 +56,93 @@ def skeleton(pag):
     return {frozenset((u, v)) for u, v, _, _ in pag.edges()}
 
 
-def mark(pag, u, v):
-    """
-    Direction-agnostic access to PAG edge marks.
-    Always returns (mark_u, mark_v) in the order requested.
-    """
-    return pag.get_edge_marks(u, v)
-
-
 def test_estimate_collider_structure():
-    np.random.seed(42)
-    n = 5000
+    # np.random.seed(42)
+    # n = 5000
 
-    A = np.random.randn(n)
-    B = np.random.randn(n)
-    C = A + B + np.random.randn(n) * 0.1
+    # A = np.random.randn(n)
+    # B = np.random.randn(n)
+    # C = A + B + np.random.randn(n) * 0.1
 
-    data = pd.DataFrame({"A": A, "B": B, "C": C})
-    pag = FCI(data).estimate(ci_test="pearsonr")
+    # data = pd.DataFrame({"A": A, "B": B, "C": C})
+
+    data = pc_alarm_data()
+    pag = FCI(data).estimate(ci_test=fake_ci_t)
 
     assert skeleton(pag) == {
         frozenset(("A", "C")),
         frozenset(("B", "C")),
     }
 
-    assert mark(pag, "A", "C")[1] == ">"
-    assert mark(pag, "B", "C")[1] == ">"
+    assert pag.get_edge_pag.get_edge_markss(pag, "A", "C")[1] == ">"
+    assert pag.get_edge_pag.get_edge_markss(pag, "B", "C")[1] == ">"
 
 
 def test_estimate_fork_structure():
-    np.random.seed(42)
-    n = 5000
+    # np.random.seed(42)
+    # n = 5000
 
-    C = np.random.randn(n)
-    A = C + np.random.randn(n) * 0.1
-    B = C + np.random.randn(n) * 0.1
+    # C = np.random.randn(n)
+    # A = C + np.random.randn(n) * 0.1
+    # B = C + np.random.randn(n) * 0.1
 
-    data = pd.DataFrame({"A": A, "B": B, "C": C})
-    pag = FCI(data).estimate(ci_test="pearsonr")
+    # data = pd.DataFrame({"A": A, "B": B, "C": C})
+
+    data = pc_asia_data()
+    # can use the fake ci test from test_PC.py
+    pag = FCI(data).estimate(ci_test=fake_ci_t)
 
     assert skeleton(pag) == {
         frozenset(("A", "C")),
         frozenset(("B", "C")),
     }
 
-    assert mark(pag, "A", "C")[1] != ">"
-    assert mark(pag, "B", "C")[1] != ">"
+    assert pag.get_edge_marks(pag, "A", "C")[1] != ">"
+    assert pag.get_edge_marks(pag, "B", "C")[1] != ">"
 
 
 def test_estimate_chain_structure():
-    np.random.seed(42)
-    n = 5000
+    # np.random.seed(42)
+    # n = 5000
 
-    A = np.random.randn(n)
-    B = A + np.random.randn(n) * 0.1
-    C = B + np.random.randn(n) * 0.1
+    # A = np.random.randn(n)
+    # B = A + np.random.randn(n) * 0.1
+    # C = B + np.random.randn(n) * 0.1
 
-    data = pd.DataFrame({"A": A, "B": B, "C": C})
-    pag = FCI(data).estimate(ci_test="pearsonr")
+    # data = pd.DataFrame({"A": A, "B": B, "C": C})
+
+    data = pc_asia_data()
+    pag = FCI(data).estimate(ci_test=fake_ci_t)
 
     assert skeleton(pag) == {
         frozenset(("A", "B")),
         frozenset(("B", "C")),
     }
 
-    assert not (mark(pag, "A", "B")[1] == ">" and mark(pag, "C", "B")[1] == ">")
+    assert not (
+        pag.get_edge_marks(pag, "A", "B")[1] == ">"
+        and pag.get_edge_marks(pag, "C", "B")[1] == ">"
+    )
 
 
 def test_estimate_latent_confounder():
-    np.random.seed(42)
-    n = 6000
+    # np.random.seed(42)
+    # n = 6000
 
-    L = np.random.randn(n)
-    X = L + np.random.randn(n) * 0.1
-    Y = L + np.random.randn(n) * 0.1
+    # L = np.random.randn(n)
+    # X = L + np.random.randn(n) * 0.1
+    # Y = L + np.random.randn(n) * 0.1
 
-    data = pd.DataFrame({"X": X, "Y": Y})
-    pag = FCI(data).estimate(ci_test="pearsonr")
+    # data = pd.DataFrame({"X": X, "Y": Y})
+
+    data = pc_asia_data()
+    pag = FCI(data).estimate(ci_test=fake_ci_t)
 
     assert skeleton(pag) == {
         frozenset(("X", "Y")),
     }
 
-    assert mark(pag, "X", "Y") == ("o", "o")
+    assert pag.get_edge_marks(pag, "X", "Y") == ("o", "o")
 
 
 def test_estimate_from_independencies():
