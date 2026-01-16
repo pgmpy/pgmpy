a50ceb8d94107374a4a8de8be4cf57c6eb7b45f5
Ankur Ankan <ankurankan@gmail.com>
2026-01-16 22:11:46 +0100

    Adds US crime dataset

--- pgmpy/datasets/__init__.py
+++ pgmpy/datasets/__init__.py
@@ -13,7 +13,6 @@ from .sachs import (  # noqa: F401
     SachsDiscrete,
     SachsMixed,
 )
-from .uscrime import USCrime  # noqa: F401
 from .lead import Lead  # noqa: F401
 from .goldberg import Goldberg  # noqa: F401
 from .hitters import Hitters  # noqa: F401
@@ -36,6 +35,7 @@ from .pima_diabetes import PimaDiabetes  # noqa: F401
 from .superconductivity import Superconductivity  # noqa: F401
 from .seoul_bike import SeoulBike  # noqa: F401
 from .cities import Cities  # noqa: F401
+from .uscrime import USCrime  # noqa: F401
 from .wine_quality import (  # noqa: F401
     WineQualityWhite,
     WineQualityRed,
--- pgmpy/datasets/uscrime.py
+++ pgmpy/datasets/uscrime.py
@@ -8,8 +8,9 @@ class USCrime(_BaseDataset):
     .. [1] Der, G., and Everitt, B.S. (2002). A Handbook of Statistical Analysis using SAS, 2e. CRC.
     .. [2] https://www.imsbio.co.jp/RGM/R_rdfile?f=ACSWR/man/usc.Rd&d=R_CC
     """
-    tags = {
-        "name": "uscrime"
+
+    _tags = {
+        "name": "uscrime",
         "n_variables": 14,
         "n_samples": 47,
         "has_ground_truth": False,
@@ -32,5 +33,7 @@ class USCrime(_BaseDataset):
     ground_truth_url = None
     expert_knowledge_url = None
 
-    categorical_variables = ["S",]
+    categorical_variables = [
+        "S",
+    ]
     ordinal_variables = dict()
