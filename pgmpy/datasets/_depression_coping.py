c4d5a8c21e7c1088b824ef959bc6c925128a5b56
Ankur Ankan <ankurankan@gmail.com>
2026-01-16 22:02:27 +0100

    Update DepressionCoping dataset metadata

--- pgmpy/datasets/_depression_coping.py
+++ pgmpy/datasets/_depression_coping.py
@@ -1,17 +1,16 @@
-from pgmpy.datasets import register_dataset_class
 from pgmpy.datasets._base import _BaseDataset
 
 
-@register_dataset_class
 class DepressionCoping(_BaseDataset):
     name = "depression_coping"
 
     tags = {
+        "name": "depression_coping",
         "n_variables": 79,
         "n_samples": 127,
         "has_ground_truth": False,
         "has_expert_knowledge": False,
-        "has_missing_data": False,
+        "has_missing_data": True,
         "is_simulated": False,
         "is_interventional": False,
         "is_discrete": False,
@@ -28,3 +27,7 @@ class DepressionCoping(_BaseDataset):
     data_url = base_url + "data/depressioncoping.continuous.dat"
     ground_truth_url = None
     expert_knowledge_url = None
+    missing_values_marker = "*"
+    
+    categorical_variables = []
+    ordinal_variables = dict()
