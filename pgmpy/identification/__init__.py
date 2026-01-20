7be18ab86855b08e3282fb3d43eb76b5bb4e41d3
Ankur Ankan <ankurankan@gmail.com>
2026-01-20 15:56:43 +0100

    Updates to the identification template

--- pgmpy/identification/__init__.py
+++ pgmpy/identification/__init__.py
@@ -1,5 +1,5 @@
-from .base import BaseIdentification
+from .base import _BaseIdentification
 from .adjustment import Adjustment
 from .frontdoor import Frontdoor
 
-__all__ = ["BaseIdentification", "Adjustment", "Frontdoor"]
+__all__ = ["_BaseIdentification", "Adjustment", "Frontdoor"]
