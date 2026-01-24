63aef3df68ae7e18e698ce563053f294b76969b1
Anshuraj70 <anshukumar9798373284@gmail.com>
2026-01-25 01:02:14 +0530

    Updated dropouts dataset class

--- pgmpy/datasets/dropouts.py
+++ pgmpy/datasets/dropouts.py
@@ -1,17 +1,7 @@
-# This extension template provides instructions to add new datasets to pgmpy.
-#
-# Please follow the following steps:
-# 1. Copy this file to `pgmpy/datasets` and rename the file as `your_dataset_name.py` (e.g., `my_dataset.py`).
-#    Note: Do NOT start the filename with an underscore `_`, otherwise it won't be discovered.
-# 2. Go through the file and address all the TODOs.
-# 3. Add an import statement in the `pgmpy/datasets/__init__.py` file (e.g. `from .my_dataset import MyDataset`).
-# 4. If you would like to contribute the dataset to pgmpy, please add the dataset name to ALL_DATASETS in
-#   `pgmpy/tests/test_datasets/test_datasets.py` file.
-
 from pgmpy.datasets._base import _BaseDataset, _CovarianceMixin
 
 
-class Dropout(_CovarianceMixin, _BaseDataset):
+class Dropouts(_CovarianceMixin, _BaseDataset):
 
     _tags = {
         "name": "dropouts",
@@ -28,7 +18,10 @@ class Dropout(_CovarianceMixin, _BaseDataset):
         "is_ordinal": False,
     }
 
-    base_url = "https://github.com/pgmpy/example-causal-datasets/tree/main/real/dropouts"
+    base_url = (
+        "https://raw.githubusercontent.com/pgmpy/example-causal-datasets/refs/heads/main/",
+        "real/dropouts",
+    )
 
     data_url = base_url + "/data/dropouts.cov.txt"
 
@@ -40,8 +33,3 @@ class Dropout(_CovarianceMixin, _BaseDataset):
 
     categorical_variables = []
     ordinal_variables = dict()
-
-   
-
-
-    
