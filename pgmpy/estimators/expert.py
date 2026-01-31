be954f58191f6805e8ed1fa113d08e23b043925f
Shreyaav18 <shreya.vernekar18@gmail.com>
2026-01-31 16:41:43 +0530

    Fix ExpertInLoop.test_all() to handle discrete CI tests

--- pgmpy/estimators/expert.py
+++ pgmpy/estimators/expert.py
@@ -45,11 +45,11 @@ class ExpertInLoop(StructureEstimator):
                 edge_present = False
 
             cond_set = list(set(u_parents).union(v_parents))
-            result = ci_test(
-                X=u, Y=v, Z=cond_set, data=self.data, boolean=False
-            )
+            result = ci_test(X=u, Y=v, Z=cond_set, data=self.data, boolean=False)
             if len(result) == 3:
-                effect, p_value, _ = result  # Discrete tests return (stat, p_value, dof)
+                effect, p_value, _ = (
+                    result  # Discrete tests return (stat, p_value, dof)
+                )
             else:
                 effect, p_value = result
             cis.append([u, v, cond_set, edge_present, effect, p_value])
