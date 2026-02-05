dacda9a02f9b87f51dc12e6bf941fee56b1c6d37
Ankur Ankan <ankurankan@gmail.com>
2026-02-05 16:55:23 +0100

    Fixes hungary chickenpox

--- pgmpy/datasets/hungary_chickenpox.py
+++ pgmpy/datasets/hungary_chickenpox.py
@@ -5,16 +5,18 @@ class HungaryChickenpox(_BaseDataset):
     """
     References
     ----------
-    .. [1] Rozemberczki, B., Scherer, P., Kiss, O., Sarkar, R., & Ferenci, T. (2021). Chickenpox cases in hungary: a benchmark
-           dataset for spatiotemporal signal processing with graph neural networks. arXiv preprint arXiv:2102.08100.
+    .. [1] Rozemberczki, B., Scherer, P., Kiss, O., Sarkar, R., & Ferenci, T. (2021). Chickenpox cases in hungary:
+           a benchmark dataset for spatiotemporal signal processing with graph neural networks.
+           arXiv preprint arXiv:2102.08100.
     .. [2] https://archive.ics.uci.edu/ml/datasets/Hungarian+Chickenpox+Cases
     """
+
     _tags = {
         "name": "hungary_chickenpox",
         "n_variables": 20,
         "n_samples": 522,
         "has_ground_truth": True,
-        "has_expert_knowledge": True,
+        "has_expert_knowledge": False,
         "has_index_col": False,
         "is_simulated": False,
         "is_interventional": False,
@@ -31,7 +33,7 @@ class HungaryChickenpox(_BaseDataset):
 
     data_url = base_url + "data/hungary-chickenpox.continuous.txt"
 
-    ground_truth_url = "ground.truth/hungary_county_edges.txt"
+    ground_truth_url = base_url + "ground.truth/hungary_dag.txt"
     expert_knowledge_url = None
 
     categorical_variables = []
