932607ebaa1a61bfd15f061d6fc689c5d619a655
Saba Siddique <sabasiddique001@gmail.com>
2026-02-07 00:37:38 +0500

    fix(example_models): set didelez_2010 n_edges to 11

--- pgmpy/example_models/didelez_2010.py
+++ pgmpy/example_models/didelez_2010.py
@@ -11,7 +11,7 @@ class Didelez2010(DAGMixin, _BaseExampleModel):
     _tags = {
         "name": "didelez_2010",
         "n_nodes": 7,
-        "n_edges": 10,
+        "n_edges": 11,
         "is_parameterized": False,
     }
     data_url = "dags/Didelez_2010.txt"
