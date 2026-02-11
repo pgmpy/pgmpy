32983ef00ac2c41fcf57e1c65d984332a22ed050
SHIVAANSH0610_LUFFY <shivaansh0610@gmail.com>
2026-02-11 12:40:31 +0530

    Refactor Titanic example model to follow _BaseExampleModel structure

--- pgmpy/example_models/titanic.py
+++ pgmpy/example_models/titanic.py
@@ -7,6 +7,78 @@ Model: Titanic
 This example is based on the Titanic Bayesian network distributed
 with the bnRep repository.
 """
+Titanic Bayesian Network.
+
+Source: bnRep R package (https://github.com/manueleleonelli/bnRep)
+Model: Titanic
+
+This example is based on the Titanic Bayesian network distributed
+with the bnRep repository.
+"""
+
+import numpy as np
+from pgmpy.models import DiscreteBayesianNetwork
+from pgmpy.factors.discrete import TabularCPD
+from ._base import DiscreteMixin, _BaseExampleModel
+
+
+class Titanic(DiscreteMixin, _BaseExampleModel):
+    _tags = {
+        "name": "titanic",
+        "n_nodes": 3,
+        "n_edges": 2,
+        "is_parameterized": True,
+        "is_discrete": True,
+        "is_continuous": False,
+        "is_hybrid": False,
+    }
+
+    def get_model(self):
+        model = DiscreteBayesianNetwork(
+            [
+                ("Class", "Survived"),
+                ("Sex", "Survived"),
+            ]
+        )
+
+        cpd_class = TabularCPD(
+            variable="Class",
+            variable_card=4,
+            values=np.array([[0.25], [0.25], [0.25], [0.25]]),
+            state_names={"Class": ["1st", "2nd", "3rd", "Crew"]},
+        )
+
+        cpd_sex = TabularCPD(
+            variable="Sex",
+            variable_card=2,
+            values=np.array([[0.5], [0.5]]),
+            state_names={"Sex": ["Male", "Female"]},
+        )
+
+        cpd_survived = TabularCPD(
+            variable="Survived",
+            variable_card=2,
+            values=np.array(
+                [
+                    [0.62, 0.47, 0.24, 0.11, 0.88, 0.73, 0.14, 0.86],
+                    [0.38, 0.53, 0.76, 0.89, 0.12, 0.27, 0.86, 0.14],
+                ]
+            ),
+            evidence=["Class", "Sex"],
+            evidence_card=[4, 2],
+            state_names={
+                "Survived": ["Yes", "No"],
+                "Class": ["1st", "2nd", "3rd", "Crew"],
+                "Sex": ["Male", "Female"],
+            },
+        )
+
+        model.add_cpds(cpd_class, cpd_sex, cpd_survived)
+
+        model.check_model()
+
+        return model
+"""
 
 import numpy as np
 
