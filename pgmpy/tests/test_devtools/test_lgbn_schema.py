0b43e073150436f8e4fd0ac06c9b50fdae30c654
Ankur Ankan <ankurankan@gmail.com>
2026-02-17 12:07:24 +0100

    Fixes test for continuous model schema

---
+++ pgmpy/tests/test_devtools/test_lgbn_schema.py
@@ -0,0 +1,20 @@
+import json
+from pathlib import Path
+
+from jsonschema import validate
+
+from pgmpy.example_models.ecoli70 import Ecoli70
+
+
+def test_lgbn_schema():
+    """Validate data against LGBN schema with semantic checks."""
+
+    schema_path = (
+        Path(__file__).parent.parent.parent.parent
+        / "devtools"
+        / "schema"
+        / "lgbn_schema.json"
+    )
+
+    ecoli_model = json.loads(Ecoli70()._get_raw_data())
+    validate(instance=ecoli_model, schema=json.load(open(schema_path)))
