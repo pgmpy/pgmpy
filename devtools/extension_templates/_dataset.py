d5df48b2ff4f412d34e89388569a43e8feabe058
Melvin T <meltuc@example.com>
2026-02-19 14:22:11 -0500

    Fix: Add base_url to dataset template
    The dataset template file didn't ask developers to specify base_url,
    but base_url is required by the caching functionality in _BaseDataset.
    
    This fixes issue #2567

--- devtools/extension_templates/_dataset.py
+++ devtools/extension_templates/_dataset.py
@@ -40,6 +40,9 @@ class YourDatasetClass(_BaseDataset):
     # row containing the names of the columns.
     data_url = None
 
+    # TODO: Add the base URL for the dataset files. This is required for the caching functionality.
+    base_url = None
+
     # TODO: Add the URL for the ground truth. The current parser expects the ground truth to be a dagitty model string.
     ground_truth_url = None
 
