7cd8062f20c36e9fde17a1e71a2277c60f1f6831
StephanieLeeS <stephanie.lee.tjio@gmail.com>
2026-02-20 20:15:32 +0700

    " "

--- pgmpy/datasets/nslm.py
+++ pgmpy/datasets/nslm.py
@@ -31,7 +31,7 @@ class NSLM(_BaseDataset):
     }
 
     base_url = "https://raw.githubusercontent.com/grf-labs/grf/refs/heads/master/experiments/"
-    data_url = "acic18/synthetic_data.csv"
+    data_url = base_url + "acic18/synthetic_data.csv"
     ground_truth_url = None
     expert_knowledge_url = None
 
