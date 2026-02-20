f2f47a2f042f8dffd24e487107afa16b795df340
StephanieLeeS <stephanie.lee.tjio@gmail.com>
2026-02-20 20:07:18 +0700

    "adding base_url"

--- pgmpy/datasets/nslm.py
+++ pgmpy/datasets/nslm.py
@@ -1,11 +1,9 @@
 # This extension template provides instructions to add new datasets to pgmpy.
 #
 # Please follow the following steps:
-# 1. Copy this file to `pgmpy/datasets` and rename the file as `your_dataset_name.py` (e.g., `my_dataset.py`).
-#    Note: Do NOT start the filename with an underscore `_`, otherwise it won't be discovered.
+
 # 2. Go through the file and address all the TODOs.
-# 3. If you would like to contribute the dataset to pgmpy, please add the dataset name to ALL_DATASETS in
-#   `pgmpy/tests/test_datasets/test_datasets.py` file.
+
 
 import pandas
 
@@ -32,16 +30,46 @@ class NSLM(_BaseDataset):
         "is_ordinal": False,
     }
 
-    data_url = "https://raw.githubusercontent.com/grf-labs/grf/refs/heads/master/experiments/acic18/synthetic_data.csv"
-
+    base_url = "https://raw.githubusercontent.com/grf-labs/grf/refs/heads/master/experiments/"
+    data_url = "acic18/synthetic_data.csv"
     ground_truth_url = None
-
     expert_knowledge_url = None
 
     missing_values_marker = None
 
     categorical_variables = ["C1", "C2", "C3", "XC"]
-
     ordinal_variables = dict()
 
-    
+    # TODO: If the ground truth file is in dagitty format, remove the following `load_ground_truth` method.
+    @classmethod
+    def load_ground_truth(cls) -> DAG:
+        if not cls.get_class_tag("has_ground_truth"):
+            return None
+
+        _ = cls._get_raw_data("ground_truth", cls.ground_truth_url).decode(
+            "utf-8-sig", errors="ignore"
+        )
+        # TODO: Add logic for parsing the data from the line above into a `pgmpy.base.DAG` object.
+        dag = None
+        return dag
+
+    # TODO: If the data is in tabular text format, remove the following `load_dataframe` method.
+    @classmethod
+    def load_dataframe(cls) -> pandas.DataFrame:
+        _ = cls._get_raw_data("data", cls.data_url)
+
+        # TODO: Add logic to construct a pandas DataFrame object from data in line above.
+        dataframe = None
+        return dataframe
+
+    # TODO: If the expert knowledge is in the expected format, remove the following `load_expert_knowledge` method.
+    @classmethod
+    def load_expert_knowledge(cls) -> ExpertKnowledge:
+        if not cls.get_class_tag("has_expert_knowledge"):
+            return None
+
+        _ = cls._get_raw_data("expert_knowledge", cls.expert_knowledge_url)
+
+        # TODO: Add logic to construct a `pgmpy.estimator.ExpertKnowledge` object from data in line above.
+        expert_knowledge = None
+        return expert_knowledge
