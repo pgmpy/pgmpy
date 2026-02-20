af8c60c1f6757cc7debb57d901a2a48616c5f6ca
StephanieLeeS <stephanie.lee.tjio@gmail.com>
2026-02-20 20:46:38 +0700

    "revise some properties of the dataset"

--- pgmpy/datasets/nslm.py
+++ pgmpy/datasets/nslm.py
@@ -17,7 +17,7 @@ class NSLM(_BaseDataset):
 
     _tags = {
         "name": "nslm",
-        "n_variables": 11,
+        "n_variables": 13,
         "n_samples": 10391,
         "has_ground_truth": False,
         "has_expert_knowledge": False,
@@ -38,39 +38,13 @@ class NSLM(_BaseDataset):
 
     missing_values_marker = None
 
-    categorical_variables = ["C1", "C2", "C3", "XC"]
+    categorical_variables = ["C1", "C2", "C3", "XC", "schoolid", "Z"]
     ordinal_variables = dict()
 
-    # TODO: If the ground truth file is in dagitty format, remove the following `load_ground_truth` method.
-    @classmethod
-    def load_ground_truth(cls) -> DAG:
-        if not cls.get_class_tag("has_ground_truth"):
-            return None
 
-        _ = cls._get_raw_data("ground_truth", cls.ground_truth_url).decode(
-            "utf-8-sig", errors="ignore"
-        )
-        # TODO: Add logic for parsing the data from the line above into a `pgmpy.base.DAG` object.
-        dag = None
-        return dag
-
-    # TODO: If the data is in tabular text format, remove the following `load_dataframe` method.
     @classmethod
     def load_dataframe(cls) -> pandas.DataFrame:
         raw_data = cls._get_raw_data("data", cls.data_url)
 
-        # TODO: Add logic to construct a pandas DataFrame object from data in line above.
         dataframe = pandas.read_csv(io.BytesIO(raw_data), sep=",")
         return dataframe
-
-    # TODO: If the expert knowledge is in the expected format, remove the following `load_expert_knowledge` method.
-    @classmethod
-    def load_expert_knowledge(cls) -> ExpertKnowledge:
-        if not cls.get_class_tag("has_expert_knowledge"):
-            return None
-
-        _ = cls._get_raw_data("expert_knowledge", cls.expert_knowledge_url)
-
-        # TODO: Add logic to construct a `pgmpy.estimator.ExpertKnowledge` object from data in line above.
-        expert_knowledge = None
-        return expert_knowledge
