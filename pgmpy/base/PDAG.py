c141e55fc9152a4cf35122fbec64f8852aac3464
soonprinceton <soonprinceton@gmail.com>
2026-02-27 10:04:58 -0500

    added subgraph to pdag

--- pgmpy/base/PDAG.py
+++ pgmpy/base/PDAG.py
@@ -248,7 +248,7 @@ class PDAG(_GraphRolesMixin, nx.DiGraph):
 
         if not inplace:
             return pdag
-        
+
     def subgraph(self, nodes):
         """
         Returns the subgraph of the PDAG induced by the specified nodes.
@@ -263,15 +263,21 @@ class PDAG(_GraphRolesMixin, nx.DiGraph):
         pgmpy.base.PDAG: The subgraph of the PDAG induced by the specified nodes.
         """
         pdag = PDAG(
-            directed_ebunch=[(u, v) for (u, v) in self.directed_edges if u in nodes and v in nodes],
-            undirected_ebunch=[(u, v) for (u, v) in self.undirected_edges if u in nodes and v in nodes],
-            latents=list(self.latents.intersection(nodes)),
+            directed_ebunch=[
+                (u, v) for (u, v) in self.directed_edges if u in nodes and v in nodes
+            ],
+            undirected_ebunch=[
+                (u, v) for (u, v) in self.undirected_edges if u in nodes and v in nodes
+            ],
+            latents=self.latents.intersection(nodes),
             exposures=self.exposures.intersection(nodes),
-            outcomes=self.outcomes.intersection(nodes)
+            outcomes=self.outcomes.intersection(nodes),
         )
 
         for role, vars in self.get_role_dict().items():
-            pdag.with_role(role=role, variables=set(vars).intersection(nodes), inplace=True)
+            pdag.with_role(
+                role=role, variables=set(vars).intersection(nodes), inplace=True
+            )
         return pdag
 
     def _check_new_unshielded_collider(self, u, v):
