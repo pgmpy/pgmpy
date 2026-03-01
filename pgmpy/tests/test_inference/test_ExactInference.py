b6f119a15f780e4277a8dc37589afb55ab3ae8ce
Anikesh Parashar <anikeshparashar@gmail.com>
2026-03-01 20:42:34 +0530

    update pytest fixture in ExactInference test

--- pgmpy/tests/test_inference/test_ExactInference.py
+++ pgmpy/tests/test_inference/test_ExactInference.py
@@ -108,7 +108,7 @@ def snow_network_model():
 
 
 @pytest.fixture
-def test_duplicated_factors_markov_inference():
+def duplicated_factors_markov_inference():
     markov_model = DiscreteMarkovNetwork([("A", "B"), ("A", "C")])
     f1 = DiscreteFactor(variables=["A", "B"], cardinality=[2, 2], values=np.eye(2) * 2)
     f2 = DiscreteFactor(variables=["A", "C"], cardinality=[2, 2], values=np.eye(2) * 2)
@@ -718,8 +718,8 @@ class TestSnowNetwork:
 
 
 class TestVariableEliminationDuplicatedFactors:
-    def test_duplicated_factors(self, test_duplicated_factors_markov_inference):
-        query_result = test_duplicated_factors_markov_inference.query(
+    def test_duplicated_factors(self, duplicated_factors_markov_inference):
+        query_result = duplicated_factors_markov_inference.query(
             ["A"], show_progress=False
         )
         assert query_result == DiscreteFactor(
