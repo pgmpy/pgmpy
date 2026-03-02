658acaf086ff5572e352ebd89e627a9426004acb
LokeshRanjan <lokesh.ranjan.phe23@itbhu.ac.in>
2026-03-02 20:18:10 +0530

    Fix docstring example in frontdoor.py

--- pgmpy/identification/frontdoor.py
+++ pgmpy/identification/frontdoor.py
@@ -33,9 +33,7 @@ class Frontdoor(_BaseIdentification):
     ...     roles={"exposures": "X", "outcomes": "Y"},
     ... )
     >>> dag_with_adj, is_identified = Frontdoor().identify(dag)
-    >>> dag_with_adj.roles
-    {'exposure': 'x1', 'outcome': 'y1', 'frontdoor': ['M']}
-    >>> Frontdoor.validate(dag)
+    >>> Frontdoor().validate(dag_with_adj)
     True
     """
 
