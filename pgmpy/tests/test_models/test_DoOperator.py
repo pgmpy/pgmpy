ce10489dc4af1fa11755731253a8de01630d16e3
B Sunil kumar <s2kumar2007@gmail.com>
2026-03-06 21:08:08 +0530

    Apply formatting fixes from pre-commit

--- pgmpy/tests/test_models/test_DoOperator.py
+++ pgmpy/tests/test_models/test_DoOperator.py
@@ -1,22 +1,23 @@
-from pgmpy.models import BayesianNetwork
 from pgmpy.factors.discrete import TabularCPD
+from pgmpy.models import BayesianNetwork
 
 
 def test_do_operator():
     """Test the do operator.
-     Create model"""
-    
+    Create model"""
+
     model = BayesianNetwork([("A", "B")])
 
     # Define CPDs
-    cpd_A = TabularCPD(variable="A", variable_card=2,
-                       values=[[0.6], [0.4]])
+    cpd_A = TabularCPD(variable="A", variable_card=2, values=[[0.6], [0.4]])
 
-    cpd_B = TabularCPD(variable="B", variable_card=2,
-                       values=[[0.7, 0.2],
-                               [0.3, 0.8]],
-                       evidence=["A"],
-                       evidence_card=[2])
+    cpd_B = TabularCPD(
+        variable="B",
+        variable_card=2,
+        values=[[0.7, 0.2], [0.3, 0.8]],
+        evidence=["A"],
+        evidence_card=[2],
+    )
 
     model.add_cpds(cpd_A, cpd_B)
 
