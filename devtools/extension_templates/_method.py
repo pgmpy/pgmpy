67de7b34764d34514a612a48397bbc4719a5c3fa
김대현 (Kim DaeHyun) <daehyun99@kakao.com>
2026-03-16 01:54:06 +0900

    Apply Review

--- devtools/extension_templates/_method.py
+++ devtools/extension_templates/_method.py
@@ -2,7 +2,7 @@
 #
 # Please follow the instructions below:
 # 1. Copy the `template_method` below and add it to the file you are working on.
-# 2. Implement the method's core logic and write the corresponding unit tests.
+# 2. Implement the method's core logic and write pytest-compatible unit tests.
 # 3. Provide type hints for the input parameters and the return value.
 # 4. Ensure the docstring follows the format provided below.
 #    Note: The documentation follows the numpydoc style. (https://numpydoc.readthedocs.io/en/latest/format.html#)
@@ -31,6 +31,5 @@ class TemplateClass:
         --------
 
         """
-        # TODO: Implement the method's core logic
-        # TODO: write the corresponding unit tests.
+        # TODO: Implement the method's core logic and write pytest-compatible unit tests.
         ...
