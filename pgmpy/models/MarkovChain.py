ee59b568a07f3ff792d3e0eca8abdd79d88a1528
Mohit <mohityadav8@users.noreply.github.com>
2026-03-16 03:37:34 +0530

    fix: remove trailing whitespace in MarkovChain.py

--- pgmpy/models/MarkovChain.py
+++ pgmpy/models/MarkovChain.py
@@ -207,7 +207,7 @@ class MarkovChain(object):
         # Validate that the variable exists in the model
         if variable not in self.cardinalities:
             raise ValueError(f"Varible{variable} not found in model.")
-        
+
         if isinstance(transition_model, list):
             transition_model = np.array(transition_model, dtype=float)
 
