671019e31ee18e255750a8a97eccd9bc1c906e70
김대현 (Kim DaeHyun) <daehyun99@kakao.com>
2026-03-15 14:18:19 +0900

    Chore: add extension template for new methods(unify format)

---
+++ devtools/extension_templates/_method.py
@@ -0,0 +1,36 @@
+# This extension template provides instructions to add new methods to pgmpy.
+#
+# Please follow the instructions below:
+# 1. Copy the `template_method` below and add it to the file you are working on.
+# 2. Implement the method's core logic and write the corresponding unit tests.
+# 3. Provide type hints for the input parameters and the return value.
+# 4. Ensure the docstring follows the format provided below.
+#    Note: The documentation follows the numpydoc style. (https://numpydoc.readthedocs.io/en/latest/format.html#)
+
+
+class TemplateClass:
+    def template_method(
+        self,
+    ):  # TODO: Provide type hints for the input parameters and the return value.
+        """
+        # TODO: Ensure the docstring follows the format provided below.
+
+        Parameters
+        ----------
+
+        Returns
+        -------
+
+        See Also
+        --------
+
+        Notes
+        -----
+
+        Examples
+        --------
+
+        """
+        # TODO: Implement the method's core logic
+        # TODO: write the corresponding unit tests.
+        ...
