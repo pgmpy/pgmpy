ab56e07b7abaf1d6f6ee79379f699d60b8fc33bd
theavinash02 <avi130303@gmail.com>
2026-03-16 21:10:07 +0530

    corrected hint error

--- pgmpy/datasets/_base.py
+++ pgmpy/datasets/_base.py
@@ -247,7 +247,7 @@ class _TubingenBenchmarkMixin:
     """
 
     @classmethod
-    def load_dataframe(cls, pair_id: int) -> DAG:
+    def load_dataframe(cls, pair_id: int) -> pd.DataFrame:
         url = f"{cls.base_url}/pair{pair_id:04}.txt"
         cache_name = f"pair_{pair_id:04}_data"
         raw_data = cls._get_raw_data(cache_name, url)
@@ -256,7 +256,7 @@ class _TubingenBenchmarkMixin:
         )
 
     @classmethod
-    def load_ground_truth(cls, pair_id: int) -> pd.DataFrame:
+    def load_ground_truth(cls, pair_id: int) -> DAG:
         url = f"{cls.base_url}/pair{pair_id:04}_graph.txt"
         cache_name = f"pair_{pair_id:04}_graph"
         raw_data = cls._get_raw_data(cache_name, url)
