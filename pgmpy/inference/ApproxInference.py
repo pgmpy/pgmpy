afa81ce330acff6f0b8764f76989311c9a025191
Yasmin Sebei <yasminsbii09@gmail.com>
2026-03-17 02:40:36 +0100

    DOC: Fix blackdoc formatting in ApproxInference.py doctest

--- pgmpy/inference/ApproxInference.py
+++ pgmpy/inference/ApproxInference.py
@@ -147,7 +147,9 @@ class ApproxInference(object):
         <DiscreteFactor representing phi(HISTORY:2) at 0x...>
         >>> infer.query(variables=["HISTORY", "CVP"], joint=True)  # doctest: +ELLIPSIS
         <DiscreteFactor representing phi(HISTORY:2, CVP:3) at 0x...>
-        >>> infer.query(variables=["HISTORY", "CVP"], joint=False)  # doctest: +ELLIPSIS, +NORMALIZE_WHITESPACE
+        >>> infer.query(
+        ...     variables=["HISTORY", "CVP"], joint=False
+        ... )  # doctest: +ELLIPSIS, +NORMALIZE_WHITESPACE
         {'HISTORY': <DiscreteFactor representing phi(HISTORY:2) at 0x...>,
          'CVP': <DiscreteFactor representing phi(CVP:3) at 0x...>}
         """
