55fa8f5ca08e56f9beb274394b209aab71b1a183
Peter Nelson Subrata <peterns1609@gmail.com>
2026-03-19 15:16:37 +0700

    test: add test for negative CPD values in check_model

--- pgmpy/tests/test_models/test_DiscreteBayesianNetwork.py
+++ pgmpy/tests/test_models/test_DiscreteBayesianNetwork.py
@@ -731,6 +731,15 @@ class TestBayesianNetworkCPD(unittest.TestCase):
 
         self.assertTrue(self.G.check_model())
 
+    def test_check_model_negative_cpd(self):
+        model = DiscreteBayesianNetwork()
+        model.add_node("A")
+
+        cpd_a = TabularCPD("A", 2, values=[[-0.5], [1.5]])
+        model.add_cpds(cpd_a)
+
+        self.assertRaises(ValueError, model.check_model)
+
     def test_check_model1(self):
         cpd_g = TabularCPD(
             "g",
