5e614d4d10849d3ee7d969637a561ed9ca8e8aa0
stephanielees <stephanie.lee.tjio@gmail.com>
2026-03-19 11:23:05 +0700

    "Add `sep` property and remove load_dataframe method"

--- pgmpy/datasets/nslm.py
+++ pgmpy/datasets/nslm.py
@@ -1,10 +1,7 @@
-import pandas as pd
-
 from pgmpy.datasets._base import _BaseDataset
 
 
 class NSLM(_BaseDataset):
-
     _tags = {
         "name": "nslm",
         "n_variables": 13,
@@ -21,22 +18,14 @@ class NSLM(_BaseDataset):
         "is_ordinal": False,
     }
 
-    base_url = (
-        "https://raw.githubusercontent.com/pgmpy/example_datasets/"
-        "refs/heads/main/nslm/"
-    )
+    base_url = "https://raw.githubusercontent.com/pgmpy/example_datasets/refs/heads/main/nslm/"
 
     data_url = base_url + "data/synthetic_data.csv"
     ground_truth_url = None
     expert_knowledge_url = None
 
     missing_values_marker = None
+    sep = ","
 
     categorical_variables = ["schoolid", "Z", "C1", "C2", "C3", "XC"]
     ordinal_variables = dict()
-
-    @classmethod
-    def load_dataframe(cls) -> pd.DataFrame:
-        return pd.read_csv(
-            cls.data_url, dtype={k: "category" for k in cls.categorical_variables}
-        )
