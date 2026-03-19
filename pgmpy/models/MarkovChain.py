fc0c23cc40f01ddc59797ad9a212851c90862234
Mohit <mohityadav8@users.noreply.github.com>
2026-03-19 23:26:38 +0530

    fix: use self.variables instead of self.cardinalities for validation

--- pgmpy/models/MarkovChain.py
+++ pgmpy/models/MarkovChain.py
@@ -203,9 +203,9 @@ class MarkovChain:
         """
 
         # Validate that the variable exists in the model
-        if variable not in self.cardinalities:
-            raise ValueError(f"Varible{variable} not found in model.")
-
+        if variable not in self.variables:
+            raise ValueError(f"Variable {variable} not found in model",)
+        
         if isinstance(transition_model, list):
             transition_model = np.array(transition_model, dtype=float)
 
