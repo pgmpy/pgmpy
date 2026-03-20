11f9f45a025063e4946cdcd82d54b99e94b91ae5
dhanam755 <rajakumarisudha1981@gmail.com>
2026-03-20 14:22:00 +0530

    Fix validation in MarkovChain.add_transition_model to check self.variables

---
+++ tests/test_MarkovChain.py
@@ -0,0 +1,14 @@
+# tests/test_MarkovChain.py
+import pytest
+from pgmpy.models import MarkovChain
+
+def test_add_transition_model_invalid_variable():
+    mc = MarkovChain()
+    mc.add_variables(['A', 'B'])
+    
+    import numpy as np
+    tm = np.array([[0.5, 0.5], [0.2, 0.8]])
+    
+    # Should raise ValueError for variable not in mc.variables
+    with pytest.raises(ValueError):
+        mc.add_transition_model('C', tm)
