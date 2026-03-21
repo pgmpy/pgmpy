c7c43423622144dbc9aa87bc5cd113373faed25a
anusa-saha <anusasaha2406@gmail.com>
2026-03-21 14:28:00 +0530

    reviewed changess

--- pgmpy/models/DiscreteMarkovNetwork.py
+++ pgmpy/models/DiscreteMarkovNetwork.py
@@ -681,19 +681,14 @@ class DiscreteMarkovNetwork(UndirectedGraph):
         >>> assertions = independencies.get_assertions()
         >>> len(assertions)
         7
-        >>> sorted(
-        ...     (tuple(sorted(s.event1)),
-        ...      tuple(sorted(s.event2)),
-        ...      tuple(sorted(s.event3)))
-        ...     for s in mm.get_local_independencies().get_assertions()
-        ... )  # doctest: +NORMALIZE_WHITESPACE
-        [(('x1',), ('x2', 'x5', 'x6', 'x7'), ('x3', 'x4')),
-        (('x2',), ('x1', 'x3', 'x6', 'x7'), ('x4', 'x5')),
-        (('x3',), ('x2', 'x4', 'x5', 'x7'), ('x1', 'x6')),
-        (('x4',), ('x3', 'x5'), ('x1', 'x2', 'x6', 'x7')),
-        (('x5',), ('x1', 'x3', 'x4', 'x6'), ('x2', 'x7')),
-        (('x6',), ('x1', 'x2', 'x5', 'x7'), ('x3', 'x4')),
-        (('x7',), ('x1', 'x2', 'x3', 'x6'), ('x4', 'x5'))]
+        >>> mm.get_local_independencies()  # doctest: +SKIP
+        (x1 ⟂ x7, x5, x2, x6 | x3, x4)
+        (x2 ⟂ x7, x6, x1, x3 | x5, x4)
+        (x3 ⟂ x7, x5, x2, x4 | x6, x1)
+        (x4 ⟂ x5, x3 | x7, x6, x1, x2)
+        (x5 ⟂ x6, x1, x3, x4 | x7, x2)
+        (x6 ⟂ x7, x5, x1, x2 | x3, x4)
+        (x7 ⟂ x6, x1, x2, x3 | x5, x4)
         """
         local_independencies = Independencies()
 
