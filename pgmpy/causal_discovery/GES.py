b88b0de6ecc5c2fd1398f46cd3a781e828734176
codervinitjangir <vinitjangirr@gmail.com>
2026-03-21 18:13:16 +0530

    Fix line too long error in GES doctest

--- pgmpy/causal_discovery/GES.py
+++ pgmpy/causal_discovery/GES.py
@@ -104,7 +104,7 @@ class GES(_ScoreMixin, _BaseCausalDiscovery):
     >>> expert = ExpertKnowledge(forbidden_edges=[("HISTORY", "CVP")])
     >>> ges = GES(scoring_method="bic-d", expert_knowledge=expert)
     >>> ges.fit(df)  # doctest: +ELLIPSIS
-    GES(expert_knowledge=<ExpertKnowledge with 0 required edges, 1 forbidden edges, 0 temporal tiers, and 0 search space edges>,
+    GES(expert_knowledge=<ExpertKnowledge with 0 required edges, 1 forbidden edges, ...>,
         scoring_method='bic-d')
 
     References
