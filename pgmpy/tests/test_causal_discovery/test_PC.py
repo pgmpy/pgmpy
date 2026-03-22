74d11c960597f88863f9479dcb288d5bd1496791
Piyush Thakkar <piyush.thakkar054@gmail.com>
2026-03-22 18:14:28 +0530

    fix: skip PC mixed test when xgboost not installed

--- pgmpy/tests/test_causal_discovery/test_PC.py
+++ pgmpy/tests/test_causal_discovery/test_PC.py
@@ -19,7 +19,20 @@ from pgmpy.sampling import BayesianModelSampling
 from pgmpy.tests.test_causal_discovery import check_causal_discovery
 
 
-@pytest.mark.parametrize("data_type", ["discrete", "continuous", "mixed"])
+@pytest.mark.parametrize(
+    "data_type",
+    [
+        "discrete",
+        "continuous",
+        pytest.param(
+            "mixed",
+            marks=pytest.mark.skipif(
+                not _check_soft_dependencies("xgboost", severity="none"),
+                reason="xgboost required for mixed data with PC",
+            ),
+        ),
+    ],
+)
 def test_pc_interface_compliance(data_type):
     check_causal_discovery(PC(return_type="dag", show_progress=False), data_type=data_type)
 
