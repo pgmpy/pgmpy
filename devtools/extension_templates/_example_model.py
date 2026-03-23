db09ab75ba42bf2bf1e7e1cc6966c6233205e997
Ankur Ankan <ankurankan@gmail.com>
2026-03-23 19:38:58 +0100

    Tighter language; some fixes

--- devtools/extension_templates/_example_model.py
+++ devtools/extension_templates/_example_model.py
@@ -1,15 +1,13 @@
 # This extension template provides instructions to add new example models to pgmpy.
 #
 # Please follow the following steps:
-# 1. Copy this file to the appropriate subdirectory in `pgmpy/example_models`
-#    (e.g., `pgmpy/example_models/bnlearn/my_model.py`).
-#    If adding a new model source, create a new subdirectory in `pgmpy/example_models` and add an `__init__.py`
-#    file to that directory so it becomes a Python package; otherwise models in that source will not be
-#    discovered by `load_model()` / `list_models()`.
-#    Note: Do NOT start the filename with an underscore `_`, otherwise it won't be discovered.
+# 1. Copy this file to the appropriate subdirectory in `pgmpy/example_models` (e.g.,
+#   `pgmpy/example_models/bnlearn/my_model.py`). If adding a new model source, create a new subdirectory in
+#   `pgmpy/example_models` and add an `__init__.py` file to that directory
+#   Note: Do NOT start the filename with an underscore `_`, otherwise it won't be discovered.
 # 2. Go through the file and address all the TODOs.
-# 3. If you would like to contribute the model to pgmpy, please add the model name to the appropriate test list in
-#   `pgmpy/tests/test_example_models/test_example_models.py` file.
+# 3. If you would like to contribute the example model to pgmpy, please add the model name to the appropriate test list
+# in `pgmpy/tests/test_example_models/test_example_models.py` file.
 
 # TODO: Import the appropriate mixin class for your model type alongside _BaseExampleModel:
 #   - DiscreteMixin: For discrete Bayesian networks stored as gzipped BIF files
@@ -22,10 +20,9 @@ from .._base import YourMixinClass, _BaseExampleModel
 # from .._base import DiscreteMixin, _BaseExampleModel
 
 
-# TODO: Rename the class to match your model name. PascalCase is recommended
-# (e.g., MyModel, AsiaNetwork).
-# TODO: Inherit from the appropriate Mixin class alongside _BaseExampleModel.
-# For e.g., DiscreteMixin, BIFMixin, ContinuousMixin, DAGMixin.
+# TODO: Rename the class to match your model name. PascalCase is recommended (e.g., MyModel, AsiaNetwork).
+# TODO: Inherit from the appropriate Mixin class alongside _BaseExampleModel. For e.g., DiscreteMixin, BIFMixin,
+#       ContinuousMixin, DAGMixin.
 class YourModelName(YourMixinClass, _BaseExampleModel):
     """[Optional: Short description of the model.]
 
@@ -40,10 +37,9 @@ class YourModelName(YourMixinClass, _BaseExampleModel):
 
     """
 
-    # TODO: Fill in the metadata tags for your model.
-    # All tags are required, except that is_discrete, is_continuous, and is_hybrid should
-    # only be provided when is_parameterized=True (omit them entirely otherwise). Set
-    # boolean tags to True or False as appropriate.
+    # TODO: Fill in the metadata tags for your model. All tags are required, except that `is_discrete`, `is_continuous`,
+    #       and `is_hybrid` should only be provided when is_parameterized=True (omit them entirely otherwise). Set
+    #       boolean tags to True or False as appropriate.
     _tags = {
         # Unique identifier for the model. Format: "source/model_name" (e.g., "bnlearn/asia", "dagitty/confounding")
         "name": "source/your_model_name",
@@ -68,8 +64,8 @@ class YourModelName(YourMixinClass, _BaseExampleModel):
     #   - "dags/confounding.txt" for a dagitty format file
     data_url = "path/to/your_model_file"
 
-    # TODO: If you need custom loading logic that doesn't fit the standard mixins,
-    # you can override the load_model_object method. Otherwise, remove this method.
+    # TODO: If you need custom loading logic that doesn't fit the standard mixins, you can override the
+    #       load_model_object method. Otherwise, remove this method.
     @classmethod
     def load_model_object(cls):
         """Load the model from the data file.
@@ -81,9 +77,7 @@ class YourModelName(YourMixinClass, _BaseExampleModel):
             The loaded model object.
 
         """
-        # By default, the mixin classes handle loading. Only override if you need custom logic.
         # Example custom loading:
         # raw_data = cls._get_raw_data()
         # # Your custom parsing logic here
         # return model
-        return super().load_model_object()
