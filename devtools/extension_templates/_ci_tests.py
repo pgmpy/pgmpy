6373aaea4c52ea9155ab10fd1de41a4a7a27369d
Ankur Ankan <ankurankan@gmail.com>
2026-03-27 13:54:14 +0100

    Correct location; simplify template

--- devtools/extension_templates/_ci_tests.py
+++ devtools/extension_templates/_ci_tests.py
@@ -2,11 +2,11 @@
 
 # Please follow the following steps:
 # 1. Copy this file to `pgmpy/ci_tests` and rename it as `your_ci_test.py`.
-# 2. Go through the file and address all the TODOs.
+# 2. Go through this file and address all the TODOs.
 # 3. Add an import in `pgmpy/ci_tests/__init__.py`.
 # 4. Add tests in `pgmpy/tests/test_ci_tests/test_your_ci_test.py`.
 
-# TODO: Add necessary imports (e.g., numpy, scipy, sklearn, etc.)
+# TODO: Add necessary imports here (e.g., numpy, scipy, sklearn, etc.)
 import pandas as pd
 
 from pgmpy.ci_tests._base import _BaseCITest
@@ -16,7 +16,7 @@ class YourCITest(_BaseCITest):
     """
     [One-line description of the CI test]
 
-    [Detailed description of the test]
+    [Detailed description of the test. Please include test statistic and p-value equations if possible]
 
     Parameters
     ----------
@@ -35,7 +35,7 @@ class YourCITest(_BaseCITest):
         Test statistic computed during the test.
 
     p_value_ : float
-        P-value corresponding to the test statistic.
+        p-value corresponding to the test statistic.
 
     Examples
     --------
@@ -44,28 +44,23 @@ class YourCITest(_BaseCITest):
     >>> data = pd.DataFrame(...)
     >>> test = YourCITest(data=data)
     >>> test("X", "Y", ["Z"], significance_level=0.05)
+    # Show the output
 
     References
     ----------
     .. [1] Add reference for the CI test
     """
 
-    # TODO: Required: Metadata used for registering the CI test
+    # TODO: Required: Metadata used for registering the CI test.
     _tags = {
-        "name": "your_ci_test",
-        "data_types": ("continuous",),  # ("discrete", "continuous", "mixed")
-        "default_for": None,  # Set if this should be default for a data type
-        "requires_data": True,  # False for tests that don’t use data
+        "name": "your_ci_test",  # Unique name allowing users to find this test by name.
+        "data_types": ("continuous",),  # Can be combination of ("discrete", "continuous", "mixed").
+        "default_for": None,  # If specified, this test becomes the default for the specified data type.
+        "requires_data": True,  # False for tests that don’t use data.
     }
 
     def __init__(self, data: pd.DataFrame = None, param1=None, param2=None):
-        """
-        Initialize the CI test.
-
-        TODO:
-        - Store all parameters as attributes
-        - Validate inputs if required
-        """
+        # TODO: Store all inputs as attributes. Add any validation if required by parameters.
         self.data = data
         self.param1 = param1
         self.param2 = param2
@@ -92,24 +87,13 @@ class YourCITest(_BaseCITest):
 
         Returns
         -------
-        statistic : float
-        p_value : float
+        tuple :
+            Tuple of the form (statistic, p_value)
         """
+        # TODO: Add logic for computing the test statistic and p-value. Can use self.data, and other params.
 
-        # TODO: Access data if required and do required augmentations
-        # data = self.data
-
-        # TODO: Implement the CI test logic here
-        # This may include:
-        # - preprocessing
-        # - computing a test statistic
-        # - computing a p-value
-
-        statistic = None
-        p_value = None
-
-        # Required: Store results
-        self.statistic_ = statistic
-        self.p_value_ = p_value
+        # TODO: Define the `statistic_` and `p_value_` attributes.
+        self.statistic_ = None
+        self.p_value_ = None
 
         return self.statistic_, self.p_value_
