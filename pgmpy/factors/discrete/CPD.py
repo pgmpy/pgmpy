9b1061fe8821402097fa3a5ca1cf8d47c8d5675b
Peter Nelson Subrata <88394243+PewterZz@users.noreply.github.com>
2026-03-27 20:23:09 +0700

    fix: allow NaN in TabularCPD non-negative guard

--- pgmpy/factors/discrete/CPD.py
+++ pgmpy/factors/discrete/CPD.py
@@ -164,7 +164,7 @@ class TabularCPD(DiscreteFactor):
         if values_casted.shape != expected_cpd_shape:
             raise ValueError(f"values must be of shape {expected_cpd_shape}. Got shape: {values.shape}")
 
-        if not (values_casted >= 0).all():
+        if (values_casted < 0).any():
             raise ValueError("CPD values must be non-negative.")
 
         if not isinstance(state_names, dict):
