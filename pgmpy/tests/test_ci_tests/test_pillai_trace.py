974be5d480396d789322ead0c28b9e3f80a2d922
anusa-saha <anusasaha2406@gmail.com>
2026-03-28 11:09:23 +0530

    Errors

--- pgmpy/tests/test_ci_tests/test_pillai_trace.py
+++ pgmpy/tests/test_ci_tests/test_pillai_trace.py
@@ -194,7 +194,7 @@ def test_pillai_tests_approx(pillai_data):
         computed_pvalues.append(test.p_value_)
 
     assert np.all(np.array(computed_coefs) <= 0.1)
-    assert np.all(np.array(computed_pvalues) <= 0.05)
+    assert np.all(np.array(computed_pvalues) >= 0.05)
 
     computed_coefs = []
     computed_pvalues = []
