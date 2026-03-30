9621d4339397289c7d567b9602053f9ed855372b
tejaswinisa1 <tejaswinisa39@gmail.com>
2026-03-30 22:09:01 +0530

    Remove stray local files from PR

--- fix.py
+++
@@ -1,5 +0,0 @@
-with open('pgmpy/readwrite/BIF.py', 'r') as f:
-    text = f.read()
-text = text.replace('"""network', "'''network").replace('... """', "... '''")
-with open('pgmpy/readwrite/BIF.py', 'w') as f:
-    f.write(text)
