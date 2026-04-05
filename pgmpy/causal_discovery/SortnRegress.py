d31c7428c8e9fda675b59f2da4a713323866bc16
theavinash02 <avi130303@gmail.com>
2026-04-05 19:43:18 +0530

    Updated the algo based on the review comments. added missed arguments.

--- pgmpy/causal_discovery/SortnRegress.py
+++ pgmpy/causal_discovery/SortnRegress.py
@@ -1,5 +1,6 @@
 import networkx as nx
 import numpy as np
+from sklearn.linear_model import LinearRegression
 
 from pgmpy.base import DAG
 from pgmpy.causal_discovery._base import _BaseCausalDiscovery
@@ -19,8 +20,11 @@ class SortnRegress(_BaseCausalDiscovery):
         The absolute value threshold for regression coefficients. Edges with
         coefficients below this value are pruned to sparsify the graph.
         A default of 0.3 is chosen to align with the benchmarking settings
-        described in Reisach et al. (2021), where it serves as a robust
-        cutoff for identifying significant causal links in sparse models.
+        described in Reisach et al. (2021).
+
+    estimator : sklearn-style regression estimator, default=None
+        The regression estimator instance to use for edge selection.
+        If None, defaults to sklearn.linear_model.LinearRegression().
 
     Attributes
     ----------
@@ -30,6 +34,25 @@ class SortnRegress(_BaseCausalDiscovery):
     adjacency_matrix_ : pd.DataFrame
         Adjacency matrix representation of the learned causal graph.
 
+    n_features_in_: int
+        The number of features in the dataset used to learn the causal graph.
+
+    feature_names_in_: np.ndarray
+        The feature names in the dataset used to learn the causal graph.
+
+    Examples
+    --------
+    >>> import pandas as pd
+    >>> import numpy as np
+    >>> from pgmpy.causal_discovery import SortnRegress
+    >>> data = pd.DataFrame(np.random.randn(1000, 3), columns=['X', 'Y', 'Z'])
+    >>> data['Y'] += 2 * data['X']
+    >>> data['Z'] += 3 * data['Y']
+    >>> model = SortnRegress(threshold=0.3)
+    >>> model.fit(data)
+    >>> model.causal_graph_.edges()
+    [('X', 'Y'), ('Y', 'Z')]
+
     References
     ----------
     Reisach, A. G., Seiler, C., & Weichwald, S. (2021). Beware of the Simulated DAG!
@@ -37,9 +60,10 @@ class SortnRegress(_BaseCausalDiscovery):
     Processing Systems, 34. https://arxiv.org/abs/2102.13647
     """
 
-    def __init__(self, threshold=0.3):
+    def __init__(self, threshold=0.3, estimator=None):
         super().__init__()
         self.threshold = threshold
+        self.estimator = estimator
 
     def _fit(self, X):
         """
@@ -54,7 +78,11 @@ class SortnRegress(_BaseCausalDiscovery):
         self : pgmpy.causal_discovery.SortnRegress
             Returns the instance with the fitted attributes.
         """
+        self.n_features_in_ = X.shape[1]
+        self.feature_names_in_ = np.array(X.columns)
         self.variables_ = list(X.columns)
+
+        model_reg = self.estimator if self.estimator else LinearRegression()
 
         variances = X.var().sort_values()
         sorted_nodes = variances.index.tolist()
@@ -66,10 +94,11 @@ class SortnRegress(_BaseCausalDiscovery):
             target = sorted_nodes[i]
             potential_parents = sorted_nodes[:i]
 
-            y = X[target].values.astype(float)
-            predictors = X[potential_parents].values.astype(float)
+            y = X[target].values
+            predictors = X[potential_parents].values
 
-            coefs, _, _, _ = np.linalg.lstsq(predictors, y, rcond=None)
+            model_reg.fit(predictors, y)
+            coefs = model_reg.coef_
 
             for idx, coef in enumerate(coefs):
                 if abs(coef) >= self.threshold:
