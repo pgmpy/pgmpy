e7f3133f364cc5185c30b43b5b7d873352df4d08
Asish Kumar <officialasishkumar@gmail.com>
2026-04-18 23:17:22 +0530

    doc: keep PomdpX doctest fixtures inline
    Signed-off-by: Asish Kumar <officialasishkumar@gmail.com>

--- pgmpy/readwrite/PomdpX.py
+++ pgmpy/readwrite/PomdpX.py
@@ -39,7 +39,15 @@ class PomdpXReader:
 
         Examples
         --------
-        >>> reader = _get_test_pomdpx_reader()
+        >>> xml = '''
+        ... <pomdpx>
+        ...   <Description>RockSample problem for map size 1 x 3.
+        ...     Rock is at 0, Rover’s initial position is at 1.
+        ...     Exit is at 2.
+        ...   </Description>
+        ... </pomdpx>
+        ... '''
+        >>> reader = PomdpXReader(string=xml)
         >>> " ".join(reader.get_description().split())
         'RockSample problem for map size 1 x 3. Rock is at 0, Rover’s initial position is at 1. Exit is at 2.'
         """
@@ -51,7 +59,7 @@ class PomdpXReader:
 
         Example
         --------
-        >>> reader = _get_test_pomdpx_reader()
+        >>> reader = PomdpXReader(string="<pomdpx><Discount>0.95</Discount></pomdpx>")
         >>> reader.get_discount()
         0.95
         """
@@ -63,7 +71,22 @@ class PomdpXReader:
 
         Example
         -------
-        >>> variables = _get_test_pomdpx_reader().get_variables()
+        >>> xml = '''
+        ... <pomdpx>
+        ...   <Variable>
+        ...     <StateVar vnamePrev="rover_0" vnameCurr="rover_1" fullyObs="true">
+        ...       <NumValues>3</NumValues>
+        ...     </StateVar>
+        ...     <StateVar vnamePrev="rock_0" vnameCurr="rock_1">
+        ...       <ValueEnum>good bad</ValueEnum>
+        ...     </StateVar>
+        ...     <ActionVar vname="action_rover">
+        ...       <ValueEnum>amw ame ac as</ValueEnum>
+        ...     </ActionVar>
+        ...   </Variable>
+        ... </pomdpx>
+        ... '''
+        >>> variables = PomdpXReader(string=xml).get_variables()
         >>> [var["vnameCurr"] for var in variables["StateVar"]]
         ['rover_1', 'rock_1']
         >>> variables["ActionVar"][0]["ValueEnum"]
@@ -117,7 +140,33 @@ class PomdpXReader:
 
         Examples
         --------
-        >>> beliefs = _get_test_pomdpx_reader().get_initial_beliefs()
+        >>> xml = '''
+        ... <pomdpx>
+        ...   <InitialStateBelief>
+        ...     <CondProb>
+        ...       <Var>rover_0</Var>
+        ...       <Parent>null</Parent>
+        ...       <Parameter type="TBL">
+        ...         <Entry>
+        ...           <Instance>-</Instance>
+        ...           <ProbTable>0.0 1.0 0.0</ProbTable>
+        ...         </Entry>
+        ...       </Parameter>
+        ...     </CondProb>
+        ...     <CondProb>
+        ...       <Var>rock_0</Var>
+        ...       <Parent>null</Parent>
+        ...       <Parameter type="TBL">
+        ...         <Entry>
+        ...           <Instance>-</Instance>
+        ...           <ProbTable>uniform</ProbTable>
+        ...         </Entry>
+        ...       </Parameter>
+        ...     </CondProb>
+        ...   </InitialStateBelief>
+        ... </pomdpx>
+        ... '''
+        >>> beliefs = PomdpXReader(string=xml).get_initial_beliefs()
         >>> [belief["Var"] for belief in beliefs]
         ['rover_0', 'rock_0']
         """
@@ -144,7 +193,23 @@ class PomdpXReader:
 
         Example
         --------
-        >>> transitions = _get_test_pomdpx_reader().get_state_transition_function()
+        >>> xml = '''
+        ... <pomdpx>
+        ...   <StateTransitionFunction>
+        ...     <CondProb>
+        ...       <Var>rover_1</Var>
+        ...       <Parent>action_rover rover_0</Parent>
+        ...       <Parameter type="TBL" />
+        ...     </CondProb>
+        ...     <CondProb>
+        ...       <Var>rock_1</Var>
+        ...       <Parent>action_rover rover_0 rock_0</Parent>
+        ...       <Parameter type="TBL" />
+        ...     </CondProb>
+        ...   </StateTransitionFunction>
+        ... </pomdpx>
+        ... '''
+        >>> transitions = PomdpXReader(string=xml).get_state_transition_function()
         >>> [transition["Var"] for transition in transitions]
         ['rover_1', 'rock_1']
         """
@@ -171,7 +236,18 @@ class PomdpXReader:
 
         Example
         --------
-        >>> _get_test_pomdpx_reader().get_obs_function()[0]["Var"]
+        >>> xml = '''
+        ... <pomdpx>
+        ...   <ObsFunction>
+        ...     <CondProb>
+        ...       <Var>obs_sensor</Var>
+        ...       <Parent>action_rover rover_1 rock_1</Parent>
+        ...       <Parameter type="TBL" />
+        ...     </CondProb>
+        ...   </ObsFunction>
+        ... </pomdpx>
+        ... '''
+        >>> PomdpXReader(string=xml).get_obs_function()[0]["Var"]
         'obs_sensor'
         """
         obs_function = []
@@ -197,7 +273,18 @@ class PomdpXReader:
 
         Example
         --------
-        >>> _get_test_pomdpx_reader().get_reward_function()[0]["Var"]
+        >>> xml = '''
+        ... <pomdpx>
+        ...   <RewardFunction>
+        ...     <Func>
+        ...       <Var>reward_rover</Var>
+        ...       <Parent>action_rover rover_0 rock_0</Parent>
+        ...       <Parameter type="TBL" />
+        ...     </Func>
+        ...   </RewardFunction>
+        ... </pomdpx>
+        ... '''
+        >>> PomdpXReader(string=xml).get_reward_function()[0]["Var"]
         'reward_rover'
         """
         reward_function = []
@@ -292,189 +379,6 @@ class PomdpXReader:
             dag["id"] = SubDAGTemplate.get("id")
         dag[root] = get_param(node)
         return dag
-
-
-_TEST_POMDPX_XML = """<pomdpx version="1.0" id="rockSample"
-  xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
-  xsi:noNamespaceSchemaLocation="pomdpx.xsd">
-     <Description>RockSample problem for map size 1 x 3.
-       Rock is at 0, Rover’s initial position is at 1.
-       Exit is at 2.
-     </Description>
-     <Discount>0.95</Discount>
-     <Variable>
-          <StateVar vnamePrev="rover_0" vnameCurr="rover_1" fullyObs="true">
-              <NumValues>3</NumValues>
-          </StateVar>
-          <StateVar vnamePrev="rock_0" vnameCurr="rock_1">
-              <ValueEnum>good bad</ValueEnum>
-          </StateVar>
-          <ObsVar vname="obs_sensor">
-              <ValueEnum>ogood obad</ValueEnum>
-          </ObsVar>
-          <ActionVar vname="action_rover">
-              <ValueEnum>amw ame ac as</ValueEnum>
-          </ActionVar>
-          <RewardVar vname="reward_rover" />
-     </Variable>
-     <InitialStateBelief>
-          <CondProb>
-              <Var>rover_0</Var>
-              <Parent>null</Parent>
-              <Parameter type="TBL">
-                  <Entry>
-                      <Instance>-</Instance>
-                      <ProbTable>0.0 1.0 0.0</ProbTable>
-                  </Entry>
-              </Parameter>
-          </CondProb>
-          <CondProb>
-              <Var>rock_0</Var>
-              <Parent>null</Parent>
-              <Parameter type="TBL">
-                  <Entry>
-                      <Instance>-</Instance>
-                      <ProbTable>uniform</ProbTable>
-                  </Entry>
-              </Parameter>
-          </CondProb>
-      </InitialStateBelief>
-      <StateTransitionFunction>
-          <CondProb>
-              <Var>rover_1</Var>
-              <Parent>action_rover rover_0</Parent>
-              <Parameter type="TBL">
-                  <Entry>
-                      <Instance>amw s0 s2</Instance>
-                      <ProbTable>1.0</ProbTable>
-                  </Entry>
-                  <Entry>
-                      <Instance>amw s1 s0</Instance>
-                      <ProbTable>1.0</ProbTable>
-                  </Entry>
-                  <Entry>
-                      <Instance>ame s0 s1</Instance>
-                      <ProbTable>1.0</ProbTable>
-                  </Entry>
-                  <Entry>
-                      <Instance>ame s1 s2</Instance>
-                      <ProbTable>1.0</ProbTable>
-                  </Entry>
-                  <Entry>
-                      <Instance>ac s0 s0</Instance>
-                      <ProbTable>1.0</ProbTable>
-                  </Entry>
-                  <Entry>
-                      <Instance>ac s1 s1</Instance>
-                      <ProbTable>1.0</ProbTable>
-                  </Entry>
-                  <Entry>
-                      <Instance>as s0 s0</Instance>
-                      <ProbTable>1.0</ProbTable>
-                  </Entry>
-                  <Entry>
-                      <Instance>as s1 s2</Instance>
-                      <ProbTable>1.0</ProbTable>
-                  </Entry>
-                  <Entry>
-                      <Instance>* s2 s2</Instance>
-                      <ProbTable>1.0</ProbTable>
-                  </Entry>
-              </Parameter>
-          </CondProb>
-          <CondProb>
-              <Var>rock_1</Var>
-              <Parent>action_rover rover_0 rock_0</Parent>
-              <Parameter>
-                  <Entry>
-                      <Instance>amw * - -</Instance>
-                      <ProbTable>1.0 0.0 0.0 1.0</ProbTable>
-                  </Entry>
-                  <Entry>
-                      <Instance>ame * - -</Instance>
-                      <ProbTable>identity</ProbTable>
-                  </Entry>
-                  <Entry>
-                      <Instance>ac * - -</Instance>
-                      <ProbTable>identity</ProbTable>
-                  </Entry>
-                  <Entry>
-                      <Instance>as * - -</Instance>
-                      <ProbTable>identity</ProbTable>
-                  </Entry>
-                  <Entry>
-                      <Instance>as s0 * -</Instance>
-                      <ProbTable>0.0 1.0</ProbTable>
-                  </Entry>
-              </Parameter>
-          </CondProb>
-      </StateTransitionFunction>
-      <ObsFunction>
-          <CondProb>
-              <Var>obs_sensor</Var>
-              <Parent>action_rover rover_1 rock_1</Parent>
-              <Parameter type="TBL">
-                  <Entry>
-                      <Instance>amw * * -</Instance>
-                      <ProbTable>1.0 0.0</ProbTable>
-                  </Entry>
-                  <Entry>
-                      <Instance>ame * * -</Instance>
-                      <ProbTable>1.0 0.0</ProbTable>
-                  </Entry>
-                  <Entry>
-                      <Instance>as * * -</Instance>
-                      <ProbTable>1.0 0.0</ProbTable>
-                  </Entry>
-                  <Entry>
-                      <Instance>ac s0 - -</Instance>
-                      <ProbTable>1.0 0.0 0.0 1.0</ProbTable>
-                  </Entry>
-                  <Entry>
-                      <Instance>ac s1 - -</Instance>
-                      <ProbTable>0.8 0.2 0.2 0.8</ProbTable>
-                  </Entry>
-                  <Entry>
-                      <Instance>ac s2 * -</Instance>
-                      <ProbTable>1.0 0.0</ProbTable>
-                  </Entry>
-              </Parameter>
-          </CondProb>
-      </ObsFunction>
-      <RewardFunction>
-          <Func>
-              <Var>reward_rover</Var>
-              <Parent>action_rover rover_0 rock_0</Parent>
-              <Parameter type="TBL">
-                  <Entry>
-                      <Instance>ame s1 *</Instance>
-                      <ValueTable>10</ValueTable>
-                  </Entry>
-                  <Entry>
-                      <Instance>amw s0 *</Instance>
-                      <ValueTable>-100</ValueTable>
-                  </Entry>
-                  <Entry>
-                      <Instance>as s1 *</Instance>
-                      <ValueTable>-100</ValueTable>
-                  </Entry>
-                  <Entry>
-                      <Instance>as s0 good</Instance>
-                      <ValueTable>10</ValueTable>
-                  </Entry>
-                  <Entry>
-                      <Instance>as s0 bad</Instance>
-                      <ValueTable>-10</ValueTable>
-                  </Entry>
-              </Parameter>
-          </Func>
-      </RewardFunction>
-</pomdpx>
-"""
-
-
-def _get_test_pomdpx_reader():
-    return PomdpXReader(string=_TEST_POMDPX_XML)
 
 
 class PomdpXWriter:
