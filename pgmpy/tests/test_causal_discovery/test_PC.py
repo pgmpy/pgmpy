3645635324c4e59115c8675016743611b39ee434
Famous <famousrajbhatt@gmail.com>
2026-05-19 23:31:28 +0530

    Implemented changes

--- pgmpy/tests/test_causal_discovery/test_PC.py
+++ pgmpy/tests/test_causal_discovery/test_PC.py
@@ -20,10 +20,7 @@ from pgmpy.sampling import BayesianModelSampling
 
 
 def expected_failed_checks(estimator):
-    return {
-        # "check_fit_score_takes_y": "Causal discovery estimators do not take y parameter in score method.",
-        # "check_n_features_in_after_fitting": "Failing for score method (not for fit) for unknown reason.",
-    }
+    return {}
 
 
 @parametrize_with_checks(
