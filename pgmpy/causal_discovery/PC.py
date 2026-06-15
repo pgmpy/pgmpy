bfbf4a5f59c6029ec7d42e9fc725479208d075ea
anusa-saha <anusasaha2406@gmail.com>
2026-06-15 14:48:09 +0530

    linting

--- pgmpy/causal_discovery/PC.py
+++ pgmpy/causal_discovery/PC.py
@@ -233,7 +233,7 @@ class PC(_ConstraintMixin, BaseCausalDiscovery):
             expert_knowledge = ExpertKnowledge()
         else:
             expert_knowledge = self.expert_knowledge
-            
+
         # If search_space or screening method in expert_knowledge is not None, limit the search space
         if expert_knowledge.search_space or expert_knowledge.screening_method is not None:
             expert_knowledge.limit_search_space(X)
