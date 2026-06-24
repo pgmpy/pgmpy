06e5a3be92732448bbb0b04ccd7cd014b618e9e1
theavinash02 <avi130303@gmail.com>
2026-06-24 21:34:05 +0530

    updated the example related to  doctest fail

--- pgmpy/causal_discovery/SortnRegress.py
+++ pgmpy/causal_discovery/SortnRegress.py
@@ -85,12 +85,14 @@ class SortnRegress(BaseCausalDiscovery):
     >>> import pandas as pd
     >>> import numpy as np
     >>> from pgmpy.causal_discovery import SortnRegress
+    >>> np.random.seed(42)
     >>> data = pd.DataFrame(np.random.randn(1000, 3), columns=['X', 'Y', 'Z'])
     >>> data['Y'] += 2 * data['X']
     >>> data['Z'] += 3 * data['Y']
     >>> model = SortnRegress(threshold=0.3)
     >>> model.fit(data)
-    >>> model.causal_graph_.edges()
+    SortnRegress(threshold=0.3)
+    >>> list(model.causal_graph_.edges())
     [('X', 'Y'), ('Y', 'Z')]
 
     References
