be09053326dde4f77e102e4d318cfe29ba493f74
theavinash02 <avi130303@gmail.com>
2026-08-19 21:16:00 +0530

    resolved the conflict

--- pgmpy/causal_discovery/__init__.py
+++ pgmpy/causal_discovery/__init__.py
@@ -4,9 +4,11 @@ from .ExpertInLoop import ExpertInLoop
 from .ExpertKnowledge import ExpertKnowledge
 from .GES import GES
 from .HillClimbSearch import HillClimbSearch
+from .IGCI import IGCI
 from .LLMPairwise import LLMPairwise
 from .PC import PC
 from .SortnRegress import SortnRegress
+from .SP import SP
 from .TAN import TAN
 from .TOPIC import TOPIC
 
@@ -17,9 +19,11 @@ __all__ = [
     "ExpertKnowledge",
     "GES",
     "HillClimbSearch",
+    "IGCI",
     "LLMPairwise",
     "PC",
     "SortnRegress",
+    "SP",
     "TAN",
     "TOPIC",
 ]
