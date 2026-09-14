9aefee97cc73a9fe2f2907bb1d1b7d28dae2c8ed
Ankur Ankan <ankurankan@gmail.com>
2026-09-14 18:27:58 +0200

    Slightly better solution

--- pgmpy/models/DiscreteBayesianNetwork.py
+++ pgmpy/models/DiscreteBayesianNetwork.py
@@ -1094,9 +1094,10 @@ class DiscreteBayesianNetwork(DAG):
         model_copy.add_edges_from(self.edges())
         if self.cpds:
             model_copy.add_cpds(*[cpd.copy() for cpd in self.cpds])
-        model_copy.latents = self.latents.copy()
-        model_copy.exposures = self.exposures.copy()
-        model_copy.outcomes = self.outcomes.copy()
+
+        for role, var in self.get_role_dict().copy().items():
+            model_copy.with_role(role=role, variables=var, inplace=True)
+
         return model_copy
 
     def get_markov_blanket(self, node: Hashable) -> list[Hashable]:
