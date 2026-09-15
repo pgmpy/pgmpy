b76bbbac0c4a0608f5c541e1f7f2b990ece5b817
Mridul Jain <jain.mridul.20@gmail.com>
2026-09-15 21:04:31 +0530

    fix FCI tests

--- pgmpy/estimators/FCI.py
+++ pgmpy/estimators/FCI.py
@@ -7,6 +7,38 @@ class FCI(BaseConstraintEstimator):
     def __init__(self, data=None, independencies=None, **kwargs):
         super().__init__(data=data, independencies=independencies, **kwargs)
 
+    @staticmethod
+    def orient_colliders(
+        skeleton,
+        separating_sets,
+        temporal_ordering: dict | None = None,
+    ):
+        """
+        Orient colliders in the skeleton using the separating sets and temporal ordering.
+
+        Parameters
+        ----------
+        skeleton: UndirectedGraph
+            The undirected graph skeleton of the BN underlying the data.
+        separating_sets: dict
+            A dict containing for each pair of not directly connected nodes a
+            separating set of variables that makes them conditionally
+            independent.
+        temporal_ordering: dict, optional
+            A dict containing for each node its temporal order.
+
+        Returns
+        -------
+        pgmpy.base.PDAG
+            The partially directed graph with v-structure orientations applied.
+        """
+        from pgmpy.causal_discovery import PC as _PC
+
+        est = _PC()
+        est.skeleton_ = skeleton
+        est.separating_sets_ = separating_sets
+        return est._orient_colliders(temporal_ordering=temporal_ordering or {})
+
     def estimate(
         self,
         ci_test="chi_square",
@@ -14,7 +46,7 @@ class FCI(BaseConstraintEstimator):
         max_cond_vars=5,
         variant="stable",
         **kwargs,
-    ):
+    ) -> PAG | None:
 
         ci_test = citests.ci_registry.get_test(ci_test, data=self.data)
 
@@ -30,32 +62,24 @@ class FCI(BaseConstraintEstimator):
         # 2. Initialize self with o–o edges
         pag = PAG()
         for u, v in skeleton.edges():
-            pag.add_edge(u, v, "o", "o")
+            pag.add_edge(u, v, "oo")
 
         # 2. Orient colliders
-        directed_edges, _ = self.orient_colliders(
-            skeleton, separating_sets, temporal_ordering={}
-        )
-        directed_edges = set(directed_edges)
+        pdag = self.orient_colliders(skeleton, separating_sets, temporal_ordering={})
 
-        for u, v in skeleton.edges():
-            uv = (u, v) in directed_edges
-            vu = (v, u) in directed_edges
-
-            if uv and not vu:
+        for u, v, edge_type in pdag.get_edges(data=True):
+            if edge_type == "->":
                 pag.modify_edge(u, v, "-", ">")
-            elif vu and not uv:
+            elif edge_type == "<-":
                 pag.modify_edge(u, v, ">", "-")
-            elif uv and vu:
-                pag.modify_edge(u, v, "o", "o")
+            elif edge_type == "--":
+                # leave undirected edges as circle-circle in the PAG representation
+                pass
         # 3. Apply orientation rules iteratively
         while True:
-            pag_new = pag.apply_orientation_rules(
-                inplace=False, separating_sets=separating_sets
-            )
-
-            if pag_new == pag:
-                break
+            pag_new = pag.apply_orientation_rules(inplace=False, separating_sets=separating_sets)
+            if pag_new is None or pag_new == pag:
+                return pag
             pag = pag_new
 
-        return pag_new
+        return pag
